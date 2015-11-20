98b936939f18ac55f0dd3702be563b108546aedf
Matthew Batchelder <borkweb@gmail.com>
2015-11-20 13:27:58 -0500

    Show which comming the debugging comes from

--- src/Tribe/Debug.php
+++ src/Tribe/Debug.php
@@ -33,9 +33,10 @@ class Tribe__Debug {
 	public static function render( $title, $data = false, $format = 'log' ) {
 		$format = ucfirst( $format );
 		if ( Tribe__Settings_Manager::instance()->get_option( 'debugEvents' ) ) {
-			error_log( "$format: $title" );
+			$plugin = basename( dirname( Tribe__Main::instance()->plugin_path ) );
+			error_log( "$plugin/common -  $format: $title" );
 			if ( $data && $data != '' ) {
-				error_log( "$format: " . print_r( $data, true ) );
+				error_log( "$plugin/common - $format: " . print_r( $data, true ) );
 			}
 		}
 	}
