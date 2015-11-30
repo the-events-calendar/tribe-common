9bbbcb42577fdb9fe93ad3dd963655f857f514ac
Luca Tumedei <luca@theaveragedev.com>
2015-11-30 13:47:42 +0100

    added wpunit suite to tests
    and `codecpept build`

--- tests/_bootstrap.dist.php
+++ tests/_bootstrap.dist.php
@@ -5,3 +5,4 @@
 
 Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
+require_once codecept_root_dir( 'tribe-autoload.php' );
