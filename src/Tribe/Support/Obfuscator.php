4e4ee5729f40a658186fb3ba948375c766dc65d9
Luca Tumedei <luca@theaveragedev.com>
2016-01-08 16:55:18 +0100

    else if replace

--- src/Tribe/Support/Obfuscator.php
+++ src/Tribe/Support/Obfuscator.php
@@ -53,13 +53,13 @@ class Tribe__Support__Obfuscator {
 		$length = strlen( $string_value );
 		if ( $length <= 3 ) {
 			return preg_replace( "/./", "#", $string_value );
-		} else if ( $length > 3 && $length <= 5 ) {
+		} elseif ( $length > 3 && $length <= 5 ) {
 			return preg_replace( '/^(.{1}).*$/', '$1' . str_repeat( '#', $length - 1 ) . '$2', $string_value );
-		} else if ( $length > 5 && $length <= 9 ) {
+		} elseif ( $length > 5 && $length <= 9 ) {
 			return preg_replace( '/^(.{1}).*(.{1})$/', '$1' . str_repeat( '#', $length - 2 ) . '$2', $string_value );
-		} else if ( $length > 9 && $length <= 19 ) {
+		} elseif ( $length > 9 && $length <= 19 ) {
 			return preg_replace( '/^(.{2}).*(.{2})$/', '$1' . str_repeat( '#', $length - 4 ) . '$2', $string_value );
-		} else if ( $length > 19 && $length <= 31 ) {
+		} elseif ( $length > 19 && $length <= 31 ) {
 			return preg_replace( '/^(.{3}).*(.{3})$/', '$1' . str_repeat( '#', $length - 6 ) . '$2', $string_value );
 		}
 
