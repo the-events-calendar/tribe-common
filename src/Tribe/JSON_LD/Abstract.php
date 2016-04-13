f642cd9a2279963f5ad35aca94557907c8acd36d
bordoni <bordoni.dev@gmail.com>
2016-04-13 14:32:00 -0300

    Better Filters

--- src/Tribe/JSON_LD/Abstract.php
+++ src/Tribe/JSON_LD/Abstract.php
@@ -44,8 +44,8 @@ abstract class Tribe__JSON_LD__Abstract {
 	 * A way for users to fetch the filter in which this was added
 	 * @return string
 	 */
-	public function get_filter() {
-		return 'tribe_json_ld_' . strtolower( esc_attr( $this->type ) ) . '_data';
+	public function get_filter( $suffix = '' ) {
+		return 'tribe_json_ld_' . strtolower( esc_attr( $this->type ) ) . '_data' . ( ! empty( $suffix ) ? '_' . $suffix : '' );
 	}
 
 	/**
@@ -72,7 +72,20 @@ abstract class Tribe__JSON_LD__Abstract {
 		if ( has_post_thumbnail( $post ) ) {
 			$data->image = wp_get_attachment_url( get_post_thumbnail_id( $post ) );
 		}
+
 		$data->url = esc_url_raw( get_permalink( $post ) );
+
+		/**
+		 * Allows the event data to be modifed by themes and other plugins.
+		 *
+		 * @example tribe_json_ld_thing_data_object
+		 * @example tribe_json_ld_event_data_object
+		 *
+		 * @param object $data objects representing the Google Markup for each event.
+		 * @param array $args the arguments used to get data
+		 * @param WP_Post $post the arguments used to get data
+		 */
+		$data = apply_filters( $this->get_filter( 'object' ), $data, $args, $post );
 
 		// Index by ID: this will allow filter code to identify the actual event being referred to
 		// without injecting an additional property
@@ -89,10 +102,13 @@ abstract class Tribe__JSON_LD__Abstract {
 		/**
 		 * Allows the event data to be modifed by themes and other plugins.
 		 *
+		 * @example tribe_json_ld_thing_data_array
+		 * @example tribe_json_ld_event_data_array
+		 *
 		 * @param array $data objects representing the Google Markup for each event.
 		 * @param array $args the arguments used to get data
 		 */
-		$data = apply_filters( $this->get_filter(), $data, $args );
+		$data = apply_filters( $this->get_filter( 'array' ), $data, $args );
 
 		// Strip the post ID indexing before returning
 		$data = array_values( $data );
