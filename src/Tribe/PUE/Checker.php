c312613d0fed37ce012539ccf902182660367d76
Trisha Salas <trisha@trishasalas.com>
2016-07-14 11:06:10 -0500

    change escaping on url
    use `esc_url` instead of `esc_html_e`

--- src/Tribe/PUE/Checker.php
+++ src/Tribe/PUE/Checker.php
@@ -494,6 +494,7 @@ if ( ! class_exists( 'Tribe__PUE__Checker' ) ) {
 		 */
 		public function display_license_error_message() {
 			$plugin_info  = $this->json_error;
+			var_dump($plugin_info);
 
 			if ( ! current_user_can( 'administrator' ) ) {
 				return;
@@ -503,7 +504,7 @@ if ( ! class_exists( 'Tribe__PUE__Checker' ) ) {
 			?>
 			<div class="notice notice-warning is-dismissible" id="pu-dashboard-message">
 				<div class="tribe-message">
-					<img class="spirit-animal" src="<?php esc_html_e( plugins_url( '../../src/resources/images/tec-panda.png', dirname( __FILE__ ) ) );?>">
+					<img class="spirit-animal" src="<?php echo esc_url( plugins_url( '../../src/resources/images/tec-panda.png', dirname( __FILE__ ) ) );?>">
 
 					<p><?php echo wp_kses( $expired_license_message, 'post' ); ?></p>
 					<p><a href="https://theeventscalendar.com/my-account/"><?php esc_html_e( 'Renew your license', 'tribe-common' ); ?></a> <?php esc_html_e( 'to get access to the latest versions including bug fixes, security updates, and new features.', 'tribe-common' ); ?></p>
