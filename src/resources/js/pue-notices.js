73de705c8856d785a5b4ca01d1be7d387d8fd747
Trisha Salas <trisha@trishasalas.com>
2016-07-15 00:43:57 -0500

    add js file to inject html onto plugins page
    I pretty much understand everything that’s happening in isolation here

---
+++ src/resources/js/pue-notices.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function ($) {
+	// alert( tribe_plugin_notices.slug );
+	$('tr.active[data-slug="the-events-calendar-pro"] .column-description').append( '<p>hello</p>' );
+});
