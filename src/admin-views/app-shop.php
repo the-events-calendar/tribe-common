56cad99c6d1771814e6ee2401623ac3ec03fbab8
Zach <zbtirrell@gmail.com>
2016-07-29 10:38:37 -0400

    making App Shop local (#144)
    Instead of callign out to TEC.com, load the App Shop from the local site.
    
    Also, optimized the images in the resources/images folder
    
    See: https://central.tri.be/issues/64286

--- src/admin-views/app-shop.php
+++ src/admin-views/app-shop.php
@@ -2,70 +2,41 @@
 
 	<div class="header">
 		<h1><?php esc_html_e( 'Tribe Event Add-Ons', 'tribe-common' ); ?></h1>
+		<a href="https://theeventscalendar.com/?utm_campaign=in-app&utm_source=addonspage&utm_medium=top-banner" target="_blank"><img src="<?php echo esc_url( tribe_resource_url( 'images/app-shop-banner.jpg', false, 'common' ) ); ?>" /></a>
 	</div>
 
 	<div class="content-wrapper">
-		<?php
-
-		if ( ! empty( $banner ) ) {
-			$banner_markup = '';
-			if ( property_exists( $banner, 'top_banner_url' ) && ! empty( $banner->top_banner_url ) ) {
-				$banner_markup = sprintf( "<img src='%s'/>", esc_url( $banner->top_banner_url ) );
-			}
-			if ( property_exists( $banner, 'top_banner_link' ) && ! empty( $banner->top_banner_link ) ) {
-				$banner_markup = sprintf( "<a href='%s' target='_blank'>%s</a>", esc_url( $banner->top_banner_link ), $banner_markup );
-			}
-			echo $banner_markup;
-		}
-
-		if ( empty( $products ) ) {
-			?>
-			<a class="button button-primary" href="https://theeventscalendar.com/products/?utm_campaign=in-app&utm_source=addonspage&utm_medium=noload"><?php _e( 'Get Add-Ons', 'tribe-common' ); ?></a>
+		<div class="addon-grid">
 			<?php
-		}
-		else {
-			$category = null;
 			$i = 0;
-			foreach ( (array) $products as $product ) {
-				if ( $product->category != $category ) {
-					if ( $category !== null ) { ?>
-						</div>
-					<?php } ?>
-
-					<div class="addon-grid">
-
-					<?php
-					$category = $product->category;
-				}
+			foreach ( $products as $product ) {
 				?>
 				<div class="tribe-addon<?php echo ( $i % 4 == 0 ) ? ' first tribe-clearfix' : '';?>">
 					<div class="thumb">
-						<a href="<?php echo esc_url( $product->permalink ); ?>"><img src="<?php echo esc_attr( $product->featured_image_url ); ?>" /></a>
+						<a href="<?php echo esc_url( $product->link ); ?>"><img src="<?php echo esc_url( tribe_resource_url( $product->image, false, 'common' ) ); ?>" /></a>
 					</div>
 					<div class="caption">
-						<h4><a href="<?php echo esc_url( $product->permalink ); ?>"><?php echo $product->title; ?></a></h4>
+						<h4><a href="<?php echo esc_url( $product->link ); ?>"><?php echo esc_html( $product->title ); ?></a></h4>
 
 						<div class="description">
 							<p><?php echo $product->description; ?></p>
-						</div>
-						<div class="meta">
 							<?php
-							if ( $product->version ) {
-								echo sprintf( '<strong>%s</strong>: %s<br/>', esc_html__( 'Version', 'tribe-common' ), esc_html( $product->version ) );
-							}
-							if ( $product->last_update ) {
-								echo sprintf( '<strong>%s</strong>: %s<br/>', esc_html__( 'Last Update', 'tribe-common' ), esc_html( $product->last_update ) );
+							if ( isset( $product->requires ) ) {
+								?>
+								<p><strong><?php esc_html_e( 'Requires:', 'tribe-common' );?></strong> <?php echo esc_html( $product->requires );?></p>
+								<?php
 							}
 							?>
 						</div>
-						<a class="button button-primary" href="<?php echo esc_url( $product->permalink ); ?>">Get This Add-on</a>
+
+						<a class="button button-primary" href="<?php echo esc_url( $product->link ); ?>">Get This Add-on</a>
 					</div>
 				</div>
 
 				<?php
 				$i++;
 			}
-		}
-		?>
+			?>
+		</div>
 	</div>
 </div>
