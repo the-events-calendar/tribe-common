9f958b3598c4418b48492796f4b679954dc7c179
Luca Tumedei <luca@theaveragedev.com>
2016-10-11 18:24:02 +0200

    scaffold `Tabbed_View` test

---
+++ tests/_data/classes/Tabbed_View_Extension.php
@@ -0,0 +1,6 @@
+<?php
+
+
+class Tabbed_View_Extension {
+
+}
---
+++ tests/wpunit/Tribe/Tabbed_ViewTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tribe;
+
+require_once codecept_data_dir( 'classes/Tabbed_View_Extension.php' );
+
+use Tabbed_View_Extension as Tabbed_View;
+
+class Tabbed_ViewTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp() {
+		// before
+		parent::setUp();
+
+		// your set up methods here
+	}
+
+	public function tearDown() {
+		// your tear down methods here
+
+		// then
+		parent::tearDown();
+
+	}
+
+	/**
+	 * @test
+	 * it should be instantiatable
+	 */
+	public function it_should_be_instantiatable() {
+		$sut = $this->make_instance();
+
+		$this->assertInstanceOf( Tabbed_View::class, $sut );
+	}
+
+	/**
+	 * @return Tabbed_View
+	 */
+	private function make_instance() {
+		return new Tabbed_View();
+	}
+
+}
