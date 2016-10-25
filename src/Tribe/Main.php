9f773f10845946afae93b50474d5c26f5f98fa8a
bordoni <bordoni.dev@gmail.com>
2016-10-25 01:21:18 -0200

    Include TimePicker CSS

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -158,6 +158,7 @@ class Tribe__Main {
 				array( 'tribe-bumpdown', 'bumpdown.js', array( 'jquery', 'underscore', 'hoverIntent' ) ),
 				array( 'tribe-bumpdown-css', 'bumpdown.css' ),
 				array( 'tribe-jquery-timepicker', 'vendor/jquery-timepicker/jquery.timepicker.js', array( 'jquery' ) ),
+				array( 'tribe-jquery-timepicker-css', 'vendor/jquery-timepicker/jquery.timepicker.css' ),
 			)
 		);
 
