8e07f44ad233ba821cfdf0ae4b5833ea1a2fa435
barryhughes <barry@codingkills.me>
2016-11-17 09:11:08 -0800

    Update tribe_get_request_var() to use Tribe__Utils__Array::get() | #68051

--- src/functions/utils.php
+++ src/functions/utils.php
@@ -132,18 +132,28 @@ if ( ! function_exists( 'tribe_get_request_var' ) ) {
 	 * Post data takes priority over fields passed in the URL query. If the field is not
 	 * set then $default (null unless a different value is specified) will be returned.
 	 *
-	 * @param string $var
-	 * @param mixed  $default
+	 * The variable being tested for can be an array if you wish to find a nested value.
+	 *
+	 * @see Tribe__Utils__Array::get()
+	 *
+	 * @param string|array $var
+	 * @param mixed        $default
 	 *
 	 * @return mixed
 	 */
 	function tribe_get_request_var( $var, $default = null ) {
-		if ( isset( $_POST[ $var ] ) ) {
-			return $_POST[ $var ];
-		} elseif ( isset( $_GET[ $var ] ) ) {
-			return $_GET[ $var ];
-		} else {
-			return $default;
+		$post_var = Tribe__Utils__Array::get( $_POST, $var );
+
+		if ( null !== $post_var ) {
+			return $post_var;
 		}
+
+		$query_var = Tribe__Utils__Array::get( $_GET, $var );
+
+		if ( null !== $query_var ) {
+			return $query_var;
+		}
+
+		return $default;
 	}
 }
