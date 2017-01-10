0d3853982ead4eb9eda67393aebf31f6ddeccafc
Luca Tumedei <luca@theaveragedev.com>
2017-01-10 15:39:48 +0100

    fix wording

--- tests/_support/ea-server-mocker/src/Notices.php
+++ tests/_support/ea-server-mocker/src/Notices.php
@@ -6,7 +6,7 @@ class Tribe__Events__Aggregator_Mocker__Notices {
 	public function render() {
 		?>
 		<div class="notice notice-warning">
-			<p>Event Aggregator responses are being mocked!</p>
+			<p>Event Aggregator server responses are being mocked!</p>
 			<p><a href="<?php echo admin_url( 'admin.php?page=ea-mocker' ); ?>">Want to disable? Go here!</a></p>
 		</div>
 		<?php
