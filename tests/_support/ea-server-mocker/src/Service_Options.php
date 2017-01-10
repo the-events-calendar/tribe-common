b5d46e2a7c35b9f9a6a151bb490a0392fcd892a9
Luca Tumedei <luca@theaveragedev.com>
2017-01-10 15:06:25 +0100

    added more example and more robust formatting:

--- tests/_support/ea-server-mocker/src/Service_Options.php
+++ tests/_support/ea-server-mocker/src/Service_Options.php
@@ -1,8 +1,100 @@
 <?php
 
 
-class Tribe__Events__Aggregator_Mocker__Service_Options
-	implements Tribe__Events__Aggregator_Mocker__Option_Provider_Interface {
+class Tribe__Events__Aggregator_Mocker__Service_Options implements Tribe__Events__Aggregator_Mocker__Option_Provider_Interface {
+
+	protected $examples
+		= array(
+			'all_active'   => array(
+				'origin' => array(
+					0 => array(
+						'id'       => 'csv',
+						'name'     => 'csv file',
+						'disabled' => false,
+					),
+					1 => array(
+						'id'       => 'facebook',
+						'name'     => 'facebook',
+						'disabled' => false,
+					),
+					2 => array(
+						'id'       => 'gcal',
+						'name'     => 'google calendar',
+						'disabled' => false,
+					),
+					3 => array(
+						'id'       => 'ical',
+						'name'     => 'icalendar',
+						'disabled' => false,
+					),
+					4 => array(
+						'id'       => 'ics',
+						'name'     => 'ics file',
+						'disabled' => false,
+					),
+					5 => array(
+						'id'       => 'meetup',
+						'name'     => 'meetup',
+						'disabled' => false,
+					),
+					6 => array(
+						'id'       => 'url',
+						'name'     => 'other url',
+						'disabled' => false,
+					),
+				),
+			),
+			'all_inactive' => array(
+				'origin' => array(
+					0 => array(
+						'id'       => 'csv',
+						'name'     => 'csv file',
+						'disabled' => true,
+					),
+					1 => array(
+						'id'       => 'facebook',
+						'name'     => 'facebook',
+						'disabled' => true,
+					),
+					2 => array(
+						'id'       => 'gcal',
+						'name'     => 'google calendar',
+						'disabled' => true,
+					),
+					3 => array(
+						'id'       => 'ical',
+						'name'     => 'icalendar',
+						'disabled' => true,
+					),
+					4 => array(
+						'id'       => 'ics',
+						'name'     => 'ics file',
+						'disabled' => true,
+					),
+					5 => array(
+						'id'       => 'meetup',
+						'name'     => 'meetup',
+						'disabled' => true,
+					),
+					6 => array(
+						'id'       => 'url',
+						'name'     => 'other url',
+						'disabled' => true,
+					),
+				),
+			),
+			'one_active'   => array(
+				array(
+					'origin' => array(
+						0 => array(
+							'id'       => 'some-source',
+							'name'     => 'Some Source',
+							'disabled' => false,
+						),
+					),
+				)
+			)
+		);
 
 	/**
 	 * Returns an array of options the class uses.
@@ -38,6 +130,12 @@ class Tribe__Events__Aggregator_Mocker__Service_Options
 						  cols="30"
 						  rows="10"
 				><?php echo get_option( 'ea_mocker-origins-mock_response' ); ?></textarea>
+				<button class="button-secondary insert-default" data-slug="all_active">All active</button>
+				<div class="default" data-slug="all_active"><?php echo json_encode( $this->examples['all_active'] ); ?></div>
+				<button class="button-secondary insert-default" data-slug="all_inactive">One active</button>
+				<div class="default" data-slug="all_inactive"><?php echo json_encode( $this->examples['all_inactive'] ); ?></div>
+				<button class="button-secondary insert-default" data-slug="one_active">One active</button>
+				<div class="default" data-slug="one_active"><?php echo json_encode( $this->examples['one_active'] ); ?></div>
 			</td>
 		</tr>
 		<?php
