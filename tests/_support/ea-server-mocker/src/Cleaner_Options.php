dad16edaced3a714a6c897fb36466d5b4cb2e895
Luca Tumedei <luca@theaveragedev.com>
2017-01-11 16:47:01 +0100

    make a fieldset of service options

--- tests/_support/ea-server-mocker/src/Cleaner_Options.php
+++ tests/_support/ea-server-mocker/src/Cleaner_Options.php
@@ -12,27 +12,26 @@ class Tribe__Events__Aggregator_Mocker__Cleaner_Options {
 		<tr valign="top">
 			<th scope="row">House Cleaning</th>
 			<td>
-				<label>
-					<input type="checkbox" name="ea_mocker-clean-events" value="tribe_events">
-					All Events (posts of type <code>tribe_events</code> and meta)
-				</label>
-				<label>
-					<input type="checkbox" name="ea_mocker-clean-venues" value="tribe_venue">
-					All Venues (posts of type <code>tribe_venue</code> and meta)
-				</label>
-				<label>
-					<input type="checkbox" name="ea_mocker-clean-organizers" value="tribe_organizer">
-					All Organizers (posts of type <code>tribe_organizer</code> and meta)
-				</label>
-				<label>
-					<input type="checkbox" name="ea_mocker-clean-ea-records" value="tribe-ea-record">
-					All EA Records (posts of type <code>tribe-ea-record</code> and meta)
-				</label>
-				<input type="submit" class="button-primary" value="Clean">
-				<input type="submit" class="button-primary" value="Clean all" name="ea_mocker-clean-all">
-
-				<?php do_action( 'ea_mocker-cleaner-messages' ); ?>
-
+				<fieldset>
+					<label>
+						<input type="checkbox" name="ea_mocker-clean-events" value="tribe_events">
+						All Events (posts of type <code>tribe_events</code> and meta)
+					</label>
+					<label>
+						<input type="checkbox" name="ea_mocker-clean-venues" value="tribe_venue">
+						All Venues (posts of type <code>tribe_venue</code> and meta)
+					</label>
+					<label>
+						<input type="checkbox" name="ea_mocker-clean-organizers" value="tribe_organizer">
+						All Organizers (posts of type <code>tribe_organizer</code> and meta)
+					</label>
+					<label>
+						<input type="checkbox" name="ea_mocker-clean-ea-records" value="tribe-ea-record">
+						All EA Records (posts of type <code>tribe-ea-record</code> and meta)
+					</label>
+					<input type="submit" class="button-primary" value="Clean">
+					<input type="submit" class="button-primary" value="Clean all" name="ea_mocker-clean-all">
+				</fieldset>
 			</td>
 		</tr>
 		<?php
