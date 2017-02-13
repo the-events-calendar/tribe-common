1a27b1eb7ce91007d82cc34d7926649f6781da51
Luca Tumedei <luca@theaveragedev.com>
2017-02-13 16:13:35 +0100

    added q system

---
+++ src/Tribe/Queue.php
@@ -0,0 +1,177 @@
+<?php
+
+class Tribe__Queue {
+	const WORKS_OPTION = 'tribe_q_works';
+
+	/**
+	 * Tells the factory to work on all the available works.
+	 *
+	 * This is the method that should be hooked to actions and crons.
+	 *
+	 * @return bool
+	 */
+	public static function work() {
+		$instance = new self();
+
+		$list = array_keys( $instance->get_work_list() );
+
+		if ( empty( $list ) ) {
+			return true;
+		}
+
+		return $instance->work_on( reset( $list ) );
+	}
+
+	/**
+	 * Tells the factory to work on a particular work immediately.
+	 *
+	 * @param string $work_id
+	 *
+	 * @return bool `true` if the work was done successfully, `false` otherwise.
+	 *              Successfully means that something was done, it does not mean complete.
+	 */
+	public function work_on( $work_id ) {
+		$work = $this->get_work( $work_id );
+
+		if ( empty( $work ) ) {
+			return false;
+		}
+
+		$work->work();
+
+		if ( $work->get_status() === Tribe__Queue__Worker::DONE ) {
+			$this->remove_work_from_list( $work );
+		} else {
+			$this->update_work_status( $work );
+		}
+
+		return true;
+	}
+
+	protected function remove_work_from_list( Tribe__Queue__Worker $work ) {
+		$list = $this->get_work_list();
+
+		unset( $list[ $work->get_id() ] );
+
+		update_option( self::WORKS_OPTION, $list );
+	}
+
+	/**
+	 * @param array                 $targets
+	 * @param       callable| array $callback  Either a callable object or array or a container reference in the
+	 *                                         ['tribe', <alias>, <method>] format.
+	 *                                         The callback will receive three arguments: the current target, the
+	 *                                         target index in the complete list of targets and the data for this work.
+	 * @param mixed                 $data      Some additional data that will be passed to the work callback.
+	 *
+	 * @return Tribe__Queue__Worker
+	 */
+	public function queue_work( array $targets, $callback, $data = null ) {
+		// let the Tribe__Queue__Work class make its own verifications
+		$work = new Tribe__Queue__Worker( $targets, $targets, $callback, $data, Tribe__Queue__Worker::QUEUED );
+
+		$this->update_work_status( $work );
+
+		return $work;
+	}
+
+	/**
+	 * Updates the status of a work in the Factory managed option.
+	 *
+	 * @param $work
+	 */
+	protected function update_work_status( Tribe__Queue__Worker $work ) {
+		$list = $this->get_work_list();
+
+		$list[ $work->get_id() ] = $work->get_status();
+
+		update_option( self::WORKS_OPTION, $list );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function get_work_list() {
+		$list = get_option( self::WORKS_OPTION );
+
+		if ( empty( $list ) ) {
+			$list = array();
+		}
+
+		$working_stati = array( Tribe__Queue__Worker::WORKING, Tribe__Queue__Worker::QUEUED );
+
+		$filtered = array();
+		foreach ( $list as $work_id => $work_status ) {
+			$work = $this->get_work( $work_id );
+			if ( empty( $work ) || ! in_array( $work_status, $working_stati ) ) {
+				continue;
+			}
+			$filtered[ $work_id ] = $work_status;
+		}
+
+		return $filtered;
+	}
+
+	/**
+	 * Returns the status of a work.
+	 *
+	 * @param string $work_id
+	 *
+	 * @see Tribe__Queue__Worker for possible stati.
+	 *
+	 * @return string
+	 */
+	public function get_work_status( $work_id ) {
+		$work = $this->get_work( $work_id );
+
+		return false !== $work ? $work->get_status() : Tribe__Queue__Worker::NOT_FOUND;
+	}
+
+	/**
+	 * Builds and returns a Tribe__Queue__Work object from its id.
+	 *
+	 * @param string $work_id
+	 *
+	 * @return Tribe__Queue__Worker|false Either the built Work object or `false` if the work object could not be found.
+	 */
+	public function get_work( $work_id ) {
+		$work_data = get_transient( $this->build_transient_name( $work_id ) );
+
+		if ( empty( $work_data ) || false === $decoded = json_decode( $work_data ) ) {
+			// either invalid or expired
+			return false;
+		}
+
+		return $this->build_from_data( $decoded );
+	}
+
+	/**
+	 * Builds the name of the transient storing a work information from its work id.
+	 *
+	 * @param string $work_id
+	 *
+	 * @return string The complete transient name.
+	 */
+	protected function build_transient_name( $work_id ) {
+		$transient = Tribe__Queue__Worker::TRANSIENT_PREFIX . $work_id;
+
+		return $transient;
+	}
+
+	/**
+	 * Builds a Work object from its JSON representation.
+	 *
+	 * @param stdClass $work_data
+	 *
+	 * @return Tribe__Queue__Worker
+	 */
+	protected function build_from_data( stdClass $work_data ) {
+		$work = new Tribe__Queue__Worker( $work_data->targets, $work_data->remaining, $work_data->callback, $work_data->data, $work_data->status );
+
+		if ( isset( $work_data->batch_size ) ) {
+			$work->set_batch_size( $work_data->batch_size );
+		}
+
+		return $work;
+	}
+}
---
+++ tests/wpunit/Tribe/Queue/WorkerTest.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Tribe\Queue;
+
+use Tribe__Queue__Worker as Worker;
+use function GuzzleHttp\json_encode;
+
+class WorkerTest extends \Codeception\TestCase\WPTestCase {
+
+	/**
+	 * @test
+	 * it should throw if adding non callable callback
+	 */
+	public function it_should_throw_if_adding_non_callable_callback() {
+		$this->expectException( \InvalidArgumentException::class );
+
+		new Worker( [], [], 'foo' );
+	}
+
+	/**
+	 * @test
+	 * it should throw if passing non well formed tribe container callback
+	 */
+	public function it_should_throw_if_passing_non_well_formed_tribe_container_callback() {
+		$this->expectException( \InvalidArgumentException::class );
+
+		new Worker( [], [], [ 'tribe', 'foo' ] );
+	}
+
+	/**
+	 * @test
+	 * it should save its state in a transient
+	 */
+	public function it_should_save_its_state_in_a_transient() {
+		$worker = new Worker( [ 'a', 'b', 'c' ], [ 'a', 'b' ], 'trailingslashit', [ 'foo' => 'bar' ], Worker::DONE );
+
+		$worker->save();
+
+		$expected = json_decode( json_encode( [
+			'targets'    => [ 'a', 'b', 'c' ],
+			'remaining'  => [ 'a', 'b' ],
+			'callback'   => 'trailingslashit',
+			'data'       => [ 'foo' => 'bar' ],
+			'status'     => Worker::DONE,
+			'batch_size' => 10,
+		] ) );
+		$this->assertEquals( $expected, $worker->read() );
+	}
+
+	/**
+	 * @test
+	 * it should work on all targets if batch size is bigger then the count
+	 */
+	public function it_should_work_on_all_targets_if_batch_size_is_bigger_then_the_count() {
+		$worker = new Worker( [ 'a', 'b', 'c', 'd' ], [ 'a', 'b', 'c', 'd' ], 'trailingslashit' );
+		$worker->set_batch_size( 10 );
+		$worker->work();
+
+		$expected = [
+			'targets'    => [ 'a', 'b', 'c', 'd' ],
+			'remaining'  => [],
+			'callback'   => 'trailingslashit',
+			'data'       => '',
+			'status'     => Worker::DONE,
+			'batch_size' => 10,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+	}
+
+	/**
+	 * @test
+	 * it should work on a subset of targets if count is larger than batch size
+	 */
+	public function it_should_work_on_a_subset_of_targets_if_count_is_larger_than_batch_size() {
+		$worker = new Worker( [ 'a', 'b', 'c', 'd' ], [ 'a', 'b', 'c', 'd' ], 'trailingslashit' );
+		$worker->set_batch_size( 2 );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => [ 'a', 'b', 'c', 'd' ],
+			'remaining'  => [ 'c', 'd' ],
+			'callback'   => 'trailingslashit',
+			'data'       => '',
+			'status'     => Worker::WORKING,
+			'batch_size' => 2,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => [ 'a', 'b', 'c', 'd' ],
+			'remaining'  => [],
+			'callback'   => 'trailingslashit',
+			'data'       => '',
+			'status'     => Worker::DONE,
+			'batch_size' => 2,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+	}
+
+	/**
+	 * @test
+	 * it should move failed at the end of the queue
+	 */
+	public function it_should_move_failed_at_the_end_of_the_queue() {
+		$targets = [ 'a', 'b', 'c', 'd', 'e' ];
+		$worker = new Worker( $targets, $targets, array( __CLASS__, 'callback_one' ) );
+		$worker->set_batch_size( 3 );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => $targets,
+			'remaining'  => [ 'd', 'e', 'c' ],
+			'callback'   => array( __CLASS__, 'callback_one' ),
+			'data'       => '',
+			'status'     => Worker::WORKING,
+			'batch_size' => 3,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => $targets,
+			'remaining'  => [ 'c' ],
+			'callback'   => array( __CLASS__, 'callback_one' ),
+			'data'       => '',
+			'status'     => Worker::WORKING,
+			'batch_size' => 3,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+	}
+
+	/**
+	 * @test
+	 * it should move exception raising at the end of the queue
+	 */
+	public function it_should_move_exception_raising_at_the_end_of_the_queue() {
+		$targets = [ 'a', 'b', 'c', 'd', 'e' ];
+		$worker = new Worker( $targets, $targets, array( __CLASS__, 'callback_two' ) );
+		$worker->set_batch_size( 3 );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => $targets,
+			'remaining'  => [ 'd', 'e', 'c' ],
+			'callback'   => array( __CLASS__, 'callback_two' ),
+			'data'       => '',
+			'status'     => Worker::WORKING,
+			'batch_size' => 3,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+
+		$worker->work();
+
+		$expected = [
+			'targets'    => $targets,
+			'remaining'  => [ 'c' ],
+			'callback'   => array( __CLASS__, 'callback_two' ),
+			'data'       => '',
+			'status'     => Worker::WORKING,
+			'batch_size' => 3,
+		];
+		$this->assertEquals( $expected, $worker->to_array() );
+	}
+
+	public static function callback_one( $_, $index ) {
+		return 2 === $index ? false : true;
+	}
+
+	public static function callback_two( $_, $index ) {
+		if ( 2 === $index ) {
+			throw new \RuntimeException();
+		}
+
+		return true;
+	}
+}
