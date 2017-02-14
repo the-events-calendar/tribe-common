a56f1986aefe7a4db7cfedaf869e2d89914ff3cb
Luca Tumedei <luca@theaveragedev.com>
2017-02-14 16:45:38 +0100

    code style

--- src/Tribe/Cron.php
+++ src/Tribe/Cron.php
@@ -10,7 +10,7 @@ class Tribe__Cron {
 	 * @return array
 	 */
 	public function filter_cron_schedules( array $schedules ) {
-		$schedules[ 'thirty_seconds' ] = array(
+		$schedules['thirty_seconds'] = array(
 			'interval' => 30,
 			'display'  => esc_html__( 'Every Thirty Seconds' ),
 		);
