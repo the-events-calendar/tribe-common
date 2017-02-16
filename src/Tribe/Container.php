6bb488e880bd5df4b5b911794e667c65d17eac1c
Luca Tumedei <luca@theaveragedev.com>
2017-02-16 13:53:49 +0100

    wrap doc

--- src/Tribe/Container.php
+++ src/Tribe/Container.php
@@ -228,7 +228,8 @@ if ( ! function_exists( 'tribe_register_provider' ) ) {
 	/**
 	 * Registers a service provider in the container.
 	 *
-	 * Service providers must implement the `tad_DI52_ServiceProviderInterface` interface or extend the `tad_DI52_ServiceProvider` class.
+	 * Service providers must implement the `tad_DI52_ServiceProviderInterface` interface or extend
+	 * the `tad_DI52_ServiceProvider` class.
 	 *
 	 * @see tad_DI52_ServiceProvider
 	 * @see tad_DI52_ServiceProviderInterface
