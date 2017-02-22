f878369d575f3f7de605b4a917a26565362c126f
Luca Tumedei <luca@theaveragedev.com>
2017-02-22 16:38:05 +0100

    base code for change authority

--- src/Tribe/Change_Authority/Base.php
+++ src/Tribe/Change_Authority/Base.php
@@ -1,5 +1,69 @@
 <?php
 
-class Tribe__Change_Authority__Base implements Tribe__Change_Authority__Interface {
+abstract class Tribe__Change_Authority__Base implements Tribe__Change_Authority__Interface {
+	/**
+	 * An array defining the name of the fields that should be propagated.
+	 * @var array
+	 */
+	protected $fields = array();
 
+	/**
+	 * Sets the fields the change authority should apply a propagation policy to.
+	 *
+	 * @param array $fields
+	 *
+	 * @return mixed
+	 */
+	public function set_fields( array $fields ) {
+		$this->fields = $fields;
+	}
+
+	/**
+	 * Propagates the changes from the source to the destination.
+	 *
+	 * @param mixed $from The source object or data.
+	 * @param mixed $to   The destination object or data.
+	 *
+	 * @return array An associative array in the format [ <field> => <propagated> ]
+	 */
+	public function propagate( $from, $to ) {
+		if ( empty( $this->fields ) ) {
+			return array();
+		}
+
+		if ( empty( $from ) || empty( $to ) ) {
+			return array_combine( $this->fields, array_fill( 0, count( $this->fields ), false ) );
+		}
+
+		foreach ( $this->fields as $field ) {
+			if ( ! $this->should_propagate( $from, $to, $field ) ) {
+				$resul[ $field ] = false;
+			}
+			$result[ $field ] = $this->propagate_field( $from, $to, $field );
+		}
+	}
+
+	/**
+	 * Whether a field should be propagated from the source to the destination.
+	 *
+	 * @param mixed  $from The source object or data.
+	 * @param mixed  $to   The destination object or data.
+	 * @param string $type The name of the field that's to be evaluated for propagation.
+	 *
+	 * @return bool Whether the field should be propagated or not.
+	 */
+	public function should_propagate( $from, $to, $field ) {
+		return in_array( $field, $this->fields );
+	}
+
+	/**
+	 * Propagates a field from the source to the destination.
+	 *
+	 * @param mixed  $from  The source object or data.
+	 * @param mixed  $to    The destination object or data.
+	 * @param string $field The name of the field that's to be evaluated for propagation.
+	 *
+	 * @return bool Whether the field was propagated or not.
+	 */
+	abstract public function propagate_field( $from, $to, $field );
 }
---
+++ src/Tribe/Change_Authority/Post_Base.php
@@ -0,0 +1,140 @@
+<?php
+
+abstract class Tribe__Change_Authority__Post_Base extends Tribe__Change_Authority__Base {
+	/**
+	 * @var array
+	 */
+	protected $post_fields = array(
+		'ID',
+		'post_author',
+		'post_date',
+		'post_date_gmt',
+		'post_content',
+		'post_title',
+		'post_excerpt',
+		'post_status',
+		'comment_status',
+		'ping_status',
+		'post_password',
+		'post_name',
+		'to_ping',
+		'pinged',
+		'post_modified',
+		'post_modified_gmt',
+		'post_content_filtered',
+		'post_parent',
+		'guid',
+		'menu_order',
+		'post_type',
+		'post_mime_type',
+		'comment_count',
+	);
+
+	/**
+	 * Whether a field should be propagated from the source to the destination.
+	 *
+	 * @param mixed  $from  The source object or data.
+	 * @param mixed  $to    The destination object or data.
+	 * @param string $field The name of the field that's to be evaluated for propagation.
+	 *
+	 * @return bool Whether the field should be propagated or not.
+	 */
+	public function should_propagate( $from, $to, $field ) {
+		return true && parent::should_propagate( $from, $to, $field );
+	}
+
+	/**
+	 * Propagates a field from the source to the destination.
+	 *
+	 * @param mixed  $from  The source object or data.
+	 * @param mixed  $to    The destination object or data.
+	 * @param string $field The name of the field that's to be evaluated for propagation.
+	 *
+	 * @return bool Whether the field was propagated or not.
+	 */
+	public function propagate_field( $from, $to, $field ) {
+		list( $from, $to ) = $this->cast_to_post( $from, $to );
+
+		if ( ! $this->is_post_field( $field ) ) {
+			return $this->propagate_meta_field( $from, $to, $field );
+		}
+
+		return $this->propagate_post_field( $from, $to, $field );
+	}
+
+	/**
+	 * @param $from
+	 * @param $to
+	 *
+	 * @return array
+	 */
+	protected function cast_to_post( $from, $to ) {
+		$from = get_post( $from );
+		$to = get_post( $to );
+
+		return array( $from, $to );
+	}
+
+	/**
+	 * Whether the specified field is a post field from the `posts` table or not.
+	 *
+	 * @param string $field The field name
+	 *
+	 * @return bool
+	 */
+	protected function is_post_field( $field ) {
+		return in_array( $field, $this->post_fields );
+	}
+
+	/**
+	 * Propagates the changes from the source to the destination.
+	 *
+	 * @param mixed $from The source object or data.
+	 * @param mixed $to   The destination object or data.
+	 *
+	 * @return array An associative array in the format [ <field> => <propagated> ]
+	 */
+	public function propagate( $from, $to ) {
+		list( $from, $to ) = $this->cast_to_post( $from, $to );
+
+		return parent::propagate( $from, $to );
+	}
+
+	/**
+	 * Propagates all the values of a post meta field from the source to the destination.
+	 *
+	 * @param WP_Post        $from
+	 * @param WP_Post        $to
+	 * @param         string $field
+	 *
+	 * @return bool Whether the custom field was propagated or not.
+	 */
+	protected function propagate_meta_field( WP_Post $from, WP_Post $to, $field ) {
+		$from_meta = get_post_meta( $from->ID, $field );
+		delete_post_meta( $to->ID, $field );
+
+		if ( empty( $from_meta ) ) {
+			return true;
+		}
+
+		$done = true;
+		foreach ( $from_meta as $entry ) {
+			$done &= (bool) update_post_meta( $to->ID, $field, $entry );
+		}
+
+		return $done;
+	}
+
+	/**
+	 * Propagates a post field from the source to the destination.
+	 *
+	 * @param WP_Post        $from
+	 * @param WP_Post        $to
+	 * @param         string $field
+	 *
+	 * @return bool Whether the field was propagated or not.
+	 */
+	protected function propagate_post_field( WP_Post $from, WP_Post $to, $field ) {
+		return (bool) wp_update_post( array( 'ID' => $to->ID, $field => $from->{$field} ) );
+	}
+}
