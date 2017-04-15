457d1b8ba1c7fa27037db0f5f52da37748cea170
bordoni <bordoni.dev@gmail.com>
2017-04-15 18:58:23 -0300

    Include Dropdown AJAX class to handle Select2 Search, for now only Search of terms

---
+++ src/Tribe/Ajax/Dropdown.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Class Tribe__Ajax__Dropdown
+ *
+ * Handles common AJAX operations.
+ */
+class Tribe__Ajax__Dropdown {
+
+	public function hook() {
+		add_action( 'wp_ajax_tribe_dropdown', array( $this, 'route' ) );
+		add_action( 'wp_ajax_nopriv_tribe_dropdown', array( $this, 'route' ) );
+	}
+
+	public function search_terms( $search, $page, $arguments, $source ) {
+		$data = array();
+
+		$data['args'] = $arguments;
+
+		return $data;
+	}
+
+	public function parse_params( $params ) {
+		$defaults = array(
+			'page'   => 0,
+			'source' => null,
+			'args'   => array(),
+			'search' => null,
+		);
+
+		$arguments = wp_parse_args( $params, $defaults );
+
+		// Return Object just for the sake of making it simpler to read
+		return (object) $arguments;
+	}
+
+	public function route() {
+		// Push all POST params into a Default set of data
+		$args = $this->parse_params( $_POST );
+
+		if ( empty( $args->source ) ) {
+			$this->error( esc_attr__( 'Missing data source for this dropdown', 'tribe-common' ) );
+		}
+
+		// Define a Filter to allow external calls to our Select2 Dropboxes
+		$filter = sanitize_key( 'tribe_dropdown_' . $args->source );
+		if ( has_filter( $filter ) ) {
+			$data = apply_filters( $filter, array(), $args->search, $args->page, $args->args, $args->source );
+		} else {
+			$data = call_user_func_array( array( $this, $args->source ), (array) $args );
+		}
+
+		// if we got a empty dataset we return an error
+		if ( empty( $data ) ) {
+			$this->error( esc_attr__( 'Empty data set for this dropdown', 'tribe-common' ) );
+		} else {
+			$this->success( $data );
+		}
+	}
+
+	private function success( $data ) {
+		// We need a Results item for Select2 Work
+		if ( ! isset( $data['results'] ) ) {
+			$data['results'] = array();
+		}
+
+		wp_send_json_success( $data );
+	}
+
+	private function error( $message ) {
+		$data = array(
+			'message' => $message,
+			'results' => array(),
+		);
+		wp_send_json_error( $data );
+	}
+
+	public function __call( $name, $arguments ) {
+		return array();
+	}
+}
