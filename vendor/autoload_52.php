58271f7264dcf9ef69a9953b522a117c059af601
Luca Tumedei <luca@theaveragedev.com>
2017-04-18 13:16:23 +0200

    composer update --no-dev

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitc67f620f586078f6b66ad887458220d3::getLoader();
+return ComposerAutoloaderInit7374570b101953a5329ce313965cbed0::getLoader();
