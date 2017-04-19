7dcaad4ba4324f230123ca5615f175fce298d8ae
bordoni <bordoni.dev@gmail.com>
2017-04-18 23:54:02 -0300

    Remove the old code for Select2 trials

--- src/Tribe/Ajax/Dropdown.php
+++ src/Tribe/Ajax/Dropdown.php
@@ -33,18 +33,6 @@ class Tribe__Ajax__Dropdown {
 			$terms = get_terms( $args );
 		}
 
-		// var_dump( $terms );
-
-		// $results = array();
-		// foreach ( $terms as $key => $term ) {
-		// 	$ancestor = get_ancestors( $term->term_id, $term->taxonomy );
-		// 	$term->id = $term->term_id;
-		// 	$term->text = $term->name;
-		// 	$term->pad = max( 0, count( $ancestor ) );
-
-		// 	$results[] = $term;
-		// }
-
 		$results = array();
 
 		if ( empty( $args['search'] ) ) {
@@ -74,98 +62,6 @@ class Tribe__Ajax__Dropdown {
 		$data['taxonomies'] = get_taxonomies();
 
 		return $data;
-	}
-
-
-	private function _get_terms( $search, $page, $args, $source ) {
-		$data = array();
-
-		if ( empty( $args['taxonomy'] ) ) {
-			$this->error( esc_attr__( 'Cannot look for Terms without a taxonomy', 'tribe-common' ) );
-		}
-		// We always want all the fields so we overwrite it
-		$args['fields'] = 'all';
-		$args['get'] = 'all';
-		$args['page'] = $page;
-		$args['number'] = 20;
-
-		// On versions older than 4.5 taxonomy goes as an Param
-		if ( version_compare( $GLOBALS['wp_version'], '4.5', '<' ) ) {
-			$terms = get_terms( $args['taxonomy'], $args );
-		} else {
-			$terms = get_terms( $args );
-		}
-
-		// Set variable because $args['number'] can be subsequently overridden.
-		$number = $args['number'];
-
-		$args['offset'] = $offset = ( $page - 1 ) * $number;
-
-		// Convert it to table rows.
-		$count = 0;
-
-		if ( is_taxonomy_hierarchical( $args['taxonomy'] ) && ! isset( $args['orderby'] ) ) {
-			// We'll need the full set of terms then.
-			$args['number'] = $args['offset'] = 0;
-		}
-
-		if ( is_taxonomy_hierarchical( $taxonomy ) && ! isset( $args['orderby'] ) ) {
-			if ( ! empty( $args['search'] ) ) {// Ignore children on searches.
-				$children = array();
-			} else {
-				$children = _get_term_hierarchy( $taxonomy );
-			}
-			// Some funky recursion to get the job done( Paging & parents mainly ) is contained within, Skip it for non-hierarchical taxonomies for performance sake
-			$this->_rows( $taxonomy, $terms, $children, $offset, $number, $count );
-		}
-	}
-
-	private function _rows( $taxonomy, $terms, &$children, $start, $per_page, &$count, $parent = 0, $level = 0 ) {
-
-		$end = $start + $per_page;
-
-		foreach ( $terms as $key => $term ) {
-
-			if ( $count >= $end )
-				break;
-
-			if ( $term->parent != $parent && empty( $_REQUEST['s'] ) )
-				continue;
-
-			// If the page starts in a subtree, print the parents.
-			if ( $count == $start && $term->parent > 0 && empty( $_REQUEST['s'] ) ) {
-				$my_parents = $parent_ids = array();
-				$p = $term->parent;
-				while ( $p ) {
-					$my_parent = get_term( $p, $taxonomy );
-					$my_parents[] = $my_parent;
-					$p = $my_parent->parent;
-					if ( in_array( $p, $parent_ids ) ) // Prevent parent loops.
-						break;
-					$parent_ids[] = $p;
-				}
-				unset( $parent_ids );
-
-				$num_parents = count( $my_parents );
-				while ( $my_parent = array_pop( $my_parents ) ) {
-					echo "\t";
-					$this->single_row( $my_parent, $level - $num_parents );
-					$num_parents--;
-				}
-			}
-
-			if ( $count >= $start ) {
-				echo "\t";
-				$this->single_row( $term, $level );
-			}
-
-			++$count;
-
-			unset( $terms[$key] );
-
-			if ( isset( $children[$term->term_id] ) && empty( $_REQUEST['s'] ) )
-				$this->_rows( $taxonomy, $terms, $children, $start, $per_page, $count, $term->term_id, $level + 1 );
-		}
 	}
 
 	public function sort_terms_hierarchicaly( &$terms, &$into, $parent = 0 ) {
