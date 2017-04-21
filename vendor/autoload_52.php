4fe01ad62c89a6e0e5fd03be716a528172d0ae37
Luca Tumedei <luca@theaveragedev.com>
2017-04-21 13:51:48 +0200

    pick changes from develop based branch

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit44f558c0290dd82b94f75a5f47a68f19::getLoader();
+return ComposerAutoloaderInit980f46fb819ff34bf79ac7e63fd07032::getLoader();
