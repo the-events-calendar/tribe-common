6663be8e2a57169a17f2ceb754590309e6887495
Luca Tumedei <luca@theaveragedev.com>
2017-05-23 14:39:49 +0200

    specialized GET and POST interfaces for endpoints

---
+++ src/Tribe/REST/Endpoints/POST_Endpoint_Interface.php
@@ -0,0 +1,21 @@
+<?php
+
+
+interface Tribe__REST__Endpoints__POST_Endpoint_Interface {
+	/**
+	 * Handles POST requests on the endpoint.
+	 *
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_Error|WP_REST_Response An array containing the data on success or a WP_Error instance on failure.
+	 */
+	public function post( WP_REST_Request $request );
+
+	/**
+	 * Returns the content of the `args` array that should be used to register the endpoint
+	 * with the `register_rest_route` function.
+	 *
+	 * @return array
+	 */
+	public function POST_args();
+}
