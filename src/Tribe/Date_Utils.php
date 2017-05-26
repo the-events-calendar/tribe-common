1143baea710c0ec39ee488f73582b12abd277e48
Luca Tumedei <luca@theaveragedev.com>
2017-05-26 15:10:41 +0200

    fix path of test file and add support for timestamps in `reformat` method

--- src/Tribe/Date_Utils.php
+++ src/Tribe/Date_Utils.php
@@ -375,7 +375,7 @@ if ( ! class_exists( 'Tribe__Date_Utils' ) ) {
 		 * @return string
 		 */
 		public static function reformat( $dt_string, $new_format ) {
-			$timestamp = strtotime( $dt_string );
+			$timestamp = self::is_timestamp( $dt_string ) ? $dt_string : strtotime( $dt_string );
 			$revised   = date( $new_format, $timestamp );
 
 			return $revised ? $revised : '';
