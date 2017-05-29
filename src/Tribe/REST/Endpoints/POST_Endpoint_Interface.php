a3d3750522a91075ecd0fc2e6f3684fe5c3f6c7d
Luca Tumedei <luca@theaveragedev.com>
2017-05-29 18:23:29 +0200

    Update interface description

--- src/Tribe/REST/Endpoints/POST_Endpoint_Interface.php
+++ src/Tribe/REST/Endpoints/POST_Endpoint_Interface.php
@@ -6,10 +6,11 @@ interface Tribe__REST__Endpoints__POST_Endpoint_Interface {
 	 * Handles POST requests on the endpoint.
 	 *
 	 * @param WP_REST_Request $request
+	 * @param bool            $return_id Whether the created post ID should be returned or the full response object.
 	 *
-	 * @return WP_Error|WP_REST_Response An array containing the data on success or a WP_Error instance on failure.
+	 * @return WP_Error|WP_REST_Response|int An array containing the data on success or a WP_Error instance on failure.
 	 */
-	public function post( WP_REST_Request $request );
+	public function post( WP_REST_Request $request, $return_id = false );
 
 	/**
 	 * Returns the content of the `args` array that should be used to register the endpoint
