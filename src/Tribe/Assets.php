c1200b57e9a284d6d0e4a70a3751f17ac3f880c0
Stephen Page <stephenjpage@gmail.com>
2017-06-22 14:00:53 -0400

    Whitespace - missed one

--- src/Tribe/Assets.php
+++ src/Tribe/Assets.php
@@ -350,7 +350,7 @@ class Tribe__Assets {
 		// If you are passing localize, you need `name` and `data`
 		if ( ! empty( $asset->localize ) && ( is_array( $asset->localize ) || is_object( $asset->localize ) ) ) {
 			if ( is_array( $asset->localize ) && empty( $asset->localize['name'] )  ) {
-				foreach( $asset->localize as $index => $local ) {
+				foreach ( $asset->localize as $index => $local ) {
 					$asset->localize[ $index ] = (object) $local;
 				}
 			} else {
