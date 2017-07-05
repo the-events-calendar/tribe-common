cac4cc543561bcb6dc1ad963c2b082487170e98e
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-05 16:06:32 +0200

    restore ea-service-mocker options pag

--- tests/_support/ea-server-mocker/src/Options_Page.php
+++ tests/_support/ea-server-mocker/src/Options_Page.php
@@ -10,11 +10,11 @@ class Tribe__Events__Aggregator_Mocker__Options_Page {
 
 	public function register_menu() {
 		add_menu_page(
-			'Event Aggregator Server Mocker',
-			'EA Mocker',
-			'administrator',
-			'ea-mocker',
-			array( $this, 'render' )
+				'Event Aggregator Server Mocker',
+				'EA Mocker',
+				'administrator',
+				'ea-mocker',
+				array( $this, 'render' )
 		);
 	}
 
@@ -37,35 +37,29 @@ class Tribe__Events__Aggregator_Mocker__Options_Page {
 
 	public function render() {
 		?>
-        <div class="wrap">
-            <h1>Event Aggregator Server Mocker Settings</h1>
+		<div class="wrap">
+			<h1>Event Aggregator Server Mocker Settings</h1>
 
-            <p>This tool <strong>will not</strong> delete existing transients and options: that's on purpose to allow
-                you to set up complex fixtures.
-            </p>
-            <p>If you do not know what you are doing (exactly) but want to experiment: if things go downhill deactivate
-                and re-activate the plugin to
-                clear all its data.</p>
-            <p>Just in case: deactivating the plugin <strong>will wipe all its data.</strong></p>
-            <p>Want to make this even more awesome? Learn to use <a href="http://www.openvim.com/"
-                                                                    target="_blank">vim</a> and then install <a
-                        href="http://appsweets.net/wasavi/" target="_blank">wasavi</a>.</p>
+			<p>This tool <strong>will not</strong> delete existing transients and options: that's on purpose to allow you to set up complex fixtures.</p>
+			<p>If you do not know what you are doing (exactly) but want to experiment: if things go downhill deactivate and re-activate the plugin to clear all its data.</p>
+			<p>Just in case: deactivating the plugin <strong>will wipe all its data.</strong></p>
+			<p>Want to make this even more awesome? Learn to use <a href="http://www.openvim.com/" target="_blank">vim</a> and then install <a href="http://appsweets.net/wasavi/" target="_blank">wasavi</a>.</p>
 
-            <form method="post" action="options.php" id="ea-mocker">
+			<form method="post" action="options.php" id="ea-mocker">
 				<?php settings_fields( 'ea_mocker' ); ?>
 				<?php do_settings_sections( 'ea_mocker' ); ?>
-                <table class="form-table">
+				<table class="form-table">
 
-                    <tr valign="top">
-                        <th scope="row">Enable mocking</th>
-                        <td>
-                            <label>
-                                <input type="checkbox" value="yes" name="ea_mocker-enable" <?php checked( 'yes',
+					<tr valign="top">
+						<th scope="row">Enable mocking</th>
+						<td>
+							<label>
+								<input type="checkbox" value="yes" name="ea_mocker-enable" <?php checked( 'yes',
 									get_option( 'ea_mocker-enable' ) ); ?>>
-                                Enable server mocking
-                            </label>
-                        </td>
-                    </tr>
+								Enable server mocking
+							</label>
+						</td>
+					</tr>
 
 					<?php
 					/**
@@ -73,9 +67,9 @@ class Tribe__Events__Aggregator_Mocker__Options_Page {
 					 */
 					do_action( 'ea_mocker-options_form' );
 					?>
-                </table>
+				</table>
 				<?php submit_button(); ?>
-            </form>
-        </div>
+			</form>
+		</div>
 	<?php }
 }
