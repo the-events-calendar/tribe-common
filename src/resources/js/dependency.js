7034a284d60159edf29e3f4afbfe822b355c4f3b
Stephen Page <stephenjpage@gmail.com>
2017-07-05 23:41:44 -0400

    Need to do some extra checking for radios that are grouped with dependant radios

--- src/resources/js/dependency.js
+++ src/resources/js/dependency.js
@@ -32,6 +32,20 @@
 					return;
 				}
 
+				/**
+				 * If we're hooking to a radio, we need to amke sure changing
+				 * any similarly named ones triggers verify on all of them
+				 * @since TBD
+				 */
+				if ( $field.is( ':radio' ) ) {
+					var $radios = $( "[name='" + $field.attr('name') + "']").not( $field );
+					$radios.each( function() {
+							$(this).on( 'change', function() {
+								$radios.trigger( 'verify.dependency' );
+							});
+					} );
+				}
+
 				// Fetch dependent elements
 				var $dependents = $document.find( '[data-depends="' + selector + '"]' ).not( '.select2-container' );
 
@@ -57,7 +71,7 @@
 						return ! $.isNumeric( val );
 					},
 					'is_checked': function ( _, __, $field ) {
-						return ( $field.is( ':checkbox' ) || $field.is( ':radio' ) ) ? $field.is( ':checked' ) : false;
+						return ( $field.is( ':checkbox' ) || $field.is( ':radio' ) ) ? $field.prop( 'checked' ) : false;
 					}
 				};
 
@@ -112,6 +126,18 @@
 							// ideally the class should be enough, but just in case...
 							if ( $dependent.is( ':hidden' ) ) {
 								$dependent.show();
+							}
+
+							/**
+							 * If we're hooking to a radio, we need to make sure changing
+							 * any similarly named ones triggers verify on all of them
+							 * @since TBD
+							 */
+							if ( $field.is( ':radio' ) ) {
+								var $radios = $( "[name='" + $field.attr('name') + "']").not( $field );
+								$radios.each( function() {
+											$radios.trigger( 'verify.dependency' );
+								} );
 							}
 						}
 
