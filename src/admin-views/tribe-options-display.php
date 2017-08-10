64de05fd2ed0fa27ad8494bb2be73195344cba72
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-08-11 01:15:09 +0300

    Use "https" for codex URL
    Replacing:
    
    `http://codex.wordpress.org/Formatting_Date_and_Time`
    
    With:
    
    `https://codex.wordpress.org/Formatting_Date_and_Time`

--- src/admin-views/tribe-options-display.php
+++ src/admin-views/tribe-options-display.php
@@ -26,7 +26,7 @@ $displayTab = array(
 				'html' => '<p>' . sprintf(
 					__( 'The following three fields accept the date format options available to the PHP %1$s function. <a href="%2$s" target="_blank">Learn how to make your own date format here</a>.', 'tribe-common' ),
 					'<code>date()</code>',
-					'http://codex.wordpress.org/Formatting_Date_and_Time'
+					'https://codex.wordpress.org/Formatting_Date_and_Time'
 				). '</p>',
 			),
 			'datepickerFormat'                   => array(
