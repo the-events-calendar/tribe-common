8026f49e62d036aab8e860384356b3823f9f8c01
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-20 11:58:04 +0200

    composer update --no-dev and dump-autoload --optimize

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitecf19c24af77e866fd11bccced6a4584::getLoader();
+return ComposerAutoloaderInit6745cea09669b6890bae6273c15acd4c::getLoader();
