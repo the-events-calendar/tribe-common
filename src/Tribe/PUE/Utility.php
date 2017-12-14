ad3d2b157bddc9d0394af724908ab98f7718f804
Scott Kingsley Clark <lol@scottkclark.com>
2017-12-14 01:30:36 -0600

    Move custom update handling

--- src/Tribe/PUE/Utility.php
+++ src/Tribe/PUE/Utility.php
@@ -75,6 +75,7 @@ if ( ! class_exists( 'Tribe__PUE__Utility' ) ) {
 				'api_invalid',
 				'api_invalid_message',
 				'api_inline_invalid_message',
+				'custom_update',
 			);
 
 			foreach ( $copyFields as $field ) {
@@ -83,19 +84,6 @@ if ( ! class_exists( 'Tribe__PUE__Utility' ) ) {
 				}
 
 				$update->$field = $info->$field;
-			}
-
-			// Support custom $update properties coming straight from PUE
-			if ( ! empty( $info->custom_update ) ) {
-				$custom_update = get_object_vars( (object) $info->custom_update );
-
-				foreach ( $custom_update as $field => $custom_value ) {
-					if ( is_object( $custom_value ) ) {
-						$custom_value = get_object_vars( $custom_value );
-					}
-
-					$update->$field = $custom_value;
-				}
 			}
 
 			return $update;
@@ -117,6 +105,19 @@ if ( ! class_exists( 'Tribe__PUE__Utility' ) ) {
 			$update->package     = $this->download_url;
 			if ( ! empty( $this->upgrade_notice ) ) {
 				$update->upgrade_notice = $this->upgrade_notice;
+			}
+
+			// Support custom $update properties coming straight from PUE
+			if ( ! empty( $this->custom_update ) ) {
+				$custom_update = get_object_vars( (object) $this->custom_update );
+
+				foreach ( $custom_update as $field => $custom_value ) {
+					if ( is_object( $custom_value ) ) {
+						$custom_value = get_object_vars( $custom_value );
+					}
+
+					$update->$field = $custom_value;
+				}
 			}
 
 			return $update;
