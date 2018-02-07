2f05a0ae17412a403018b30c4f27e5caa93d79fe
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-07 19:14:58 +0100

    no-dev...

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit081290d3904a54c3e791304db42b4d14::getLoader();
+return ComposerAutoloaderInit556e8d0b8d552d5875f3af231e54ac39::getLoader();
