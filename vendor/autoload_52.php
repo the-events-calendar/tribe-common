7ece2e2e2b801dd8a599249eb598eb27a51b03f2
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-12 18:29:24 +0100

    udpate autoloader and tests

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1cdbfcbe09e0cd5f56b2e5c150580001::getLoader();
+return ComposerAutoloaderInit7e395a791733d109ee2a381eefb921dc::getLoader();
