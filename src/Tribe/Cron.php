4a75d9695bb0b76fa3b51bea665ca111b9123deb
Scott Kingsley Clark <lol@scottkclark.com>
2018-02-22 02:02:15 -0600

    Update translation domain

--- src/Tribe/Cron.php
+++ src/Tribe/Cron.php
@@ -19,7 +19,7 @@ class Tribe__Cron {
 	public function filter_cron_schedules( array $schedules ) {
 		$schedules['thirty_seconds'] = array(
 			'interval' => 30,
-			'display'  => esc_html__( 'Every Thirty Seconds' ),
+			'display'  => esc_html__( 'Every Thirty Seconds', 'tribe-common' ),
 		);
 
 		return $schedules;
