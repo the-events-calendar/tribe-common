1b9fe378621de97ec3b815be9daeb1f9fc1cda2f
Scott Kingsley Clark <lol@scottkclark.com>
2018-05-22 09:11:20 -0500

    Add more zero tests for is_string

--- tests/wpunit/Tribe/Validator/BaseTest.php
+++ tests/wpunit/Tribe/Validator/BaseTest.php
@@ -30,6 +30,8 @@ class BaseTest extends \Codeception\TestCase\WPTestCase {
 			[ new \StdClass(), false ],
 			[ 'f', true ],
 			[ 'foo bar', true ],
+			[ '0', true ],
+			[ 0, false ],
 		];
 	}
 
@@ -52,6 +54,8 @@ class BaseTest extends \Codeception\TestCase\WPTestCase {
 			[ new \StdClass(), false ],
 			[ 'f', true ],
 			[ 'foo bar', true ],
+			[ '0', false ],
+			[ 0, false ],
 		];
 	}
 
