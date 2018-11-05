f0e1cdb1dc0331f67ab6d44b9d5bfe4b2cb4eefa
George Gecewicz <gecewicz.george@gmail.com>
2018-11-04 21:26:26 -0500

    creating gutneberg release notice branch

--- src/Tribe/Admin/Notice/Marketing.php
+++ src/Tribe/Admin/Notice/Marketing.php
@@ -6,13 +6,24 @@
  */
 class Tribe__Admin__Notice__Marketing {
 
+	public $tec_is_active;
+
+	public $et_is_active;
+
+	public function __construct() {
+		$tribe_dependency    = Tribe__Dependency::instance();
+		$this->tec_is_active = $tribe_dependency->is_plugin_active( 'Tribe__Events__Main' );
+		$this->et_is_active  = $tribe_dependency->is_plugin_active( 'Tribe__Tickets__Main' );
+	}
+
 	/**
 	 * Register the various Marketing notices.
 	 *
-	 * @since
+	 * @since TBD
 	 */
 	public function hook() {
 		$this->bf_2018_hook_notice();
+		$this->gutenberg_release_notice();
 	}
 
 	/**
@@ -32,23 +43,6 @@ class Tribe__Admin__Notice__Marketing {
 			),
 			array( $this, 'bf_2018_should_display' )
 		);
-	}
-
-	/**
-	 * Whether the Black Friday 2018 notice should display.
-	 *
-	 * Unix times for Nov 20 2018 @ 6am UTC and Nov 26 2018 @ 6am UTC.
-	 * 6am UTC is midnight for TheEventsCalendar.com, which uses the America/Los_Angeles time zone.
-	 *
-	 * @since TBD
-	 *
-	 * @return boolean
-	 */
-	public function bf_2018_should_display() {
-		$bf_sale_start = $this->get_bf_2018_start_time();
-		$bf_sale_end   = $this->get_bf_2018_end_time();
-
-		return $bf_sale_start <= time() && time() < $bf_sale_end;
 	}
 
 	/**
@@ -86,6 +80,22 @@ class Tribe__Admin__Notice__Marketing {
 		 */
 		return apply_filters( 'tribe_bf_2018_end_time', 1543212000 );
 	}
+	/**
+	 * Whether the Black Friday 2018 notice should display.
+	 *
+	 * Unix times for Nov 20 2018 @ 6am UTC and Nov 26 2018 @ 6am UTC.
+	 * 6am UTC is midnight for TheEventsCalendar.com, which uses the America/Los_Angeles time zone.
+	 *
+	 * @since TBD
+	 *
+	 * @return boolean
+	 */
+	public function bf_2018_should_display() {
+		$bf_sale_start = $this->get_bf_2018_start_time();
+		$bf_sale_end   = $this->get_bf_2018_end_time();
+
+		return $bf_sale_start <= time() && time() < $bf_sale_end;
+	}
 
 	/**
 	 * HTML for the Black Friday 2018 notice.
@@ -98,18 +108,89 @@ class Tribe__Admin__Notice__Marketing {
 
 		Tribe__Assets::instance()->enqueue( array( 'tribe-common-admin' ) );
 
-		$tribe_dependency = Tribe__Dependency::instance();
-		$tec_is_active    = $tribe_dependency->is_plugin_active( 'Tribe__Events__Main' );
-		$et_is_active     = $tribe_dependency->is_plugin_active( 'Tribe__Tickets__Main' );
-
 		$mascot_url = Tribe__Main::instance()->plugin_url . 'src/resources/images/mascot.png';
 		$end_time   = $this->get_bf_2018_end_time();
 
 		ob_start();
 
-		if ( $tec_is_active && ! $et_is_active ) {
+		if ( $this->tec_is_active && ! $this->et_is_active ) {
 			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-tec.php';
-		} elseif ( $et_is_active && ! $tec_is_active ) {
+		} elseif ( $this->et_is_active && ! $this->tec_is_active ) {
+			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-et.php';
+		} else {
+			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-general.php';
+		}
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Register the Gutenberg Release notice (November 2018).
+	 *
+	 * @since TBD
+	 */
+	public function gutenberg_release_notice() {
+
+		tribe_notice(
+			'gutenberg-release-2018',
+			array( $this, 'gutenberg_release_display_notice' ),
+			array(
+				'type'    => 'warning',
+				'dismiss' => 1,
+				'wrap'    => false,
+			),
+			array( $this, 'gutenberg_release_should_display' )
+		);
+	}
+
+	/**
+	 * Gets the end time for the Gutenberg release notice.
+	 *
+	 * @since TBD
+	 *
+	 * @return int
+	 */
+	public function get_gutenberg_release_end_time() {
+
+		/**
+		 * Allows filtering of the default Gutenberg Release Notice's end time, mainly for testing purposes.
+		 *
+		 * @since TBD
+		 *
+		 * @param int $gutenberg_release_end_time Defaults to Nov 17 2018 @ midnight, California time.
+		 */
+		return time() < apply_filters( 'tribe_gutenberg_release_notice_end_time', 1542434400 );
+	}
+
+	/**
+	 * Whether the Gutenberg Release notice should display.
+	 *
+	 * @since TBD
+	 *
+	 * @return boolean
+	 */
+	public function gutenberg_release_should_display() {
+		return time() < $this->get_gutenberg_release_end_time();
+	}
+
+	/**
+	 * HTML for the Gutenberg Release notice (November 2018).
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public function gutenberg_release_display_notice() {
+
+		Tribe__Assets::instance()->enqueue( array( 'tribe-common-admin' ) );
+
+		$end_time = $this->get_gutenberg_release_end_time();
+
+		ob_start();
+
+		if ( $this->tec_is_active && ! $this->et_is_active ) {
+			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-tec.php';
+		} elseif ( $this->et_is_active && ! $this->tec_is_active ) {
 			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-et.php';
 		} else {
 			include Tribe__Main::instance()->plugin_path . 'src/admin-views/notices/tribe-bf-2018-general.php';
---
+++ src/admin-views/notices/tribe-gutenberg-release.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The Gutenberg Release admin notice for when TEC is active, or TEC *and* ET.
+ *
+ * @since TBD
+ *
+ * @var int $mascot_url The local URL for the notice's mascot image.
+ * @var int $end_time The Unix timestamp for the release notice's end time.
+ */
+?>
+<div class="tribe-marketing-notice">
+	<div class="tribe-notice-icon">
+		<img src="<?php echo esc_url( $mascot_url ); ?>" />
+	</div>
+	<div class="tribe-notice-content">
+		<h3>Gutneberg!</h3>
+	</div>
+</div>
