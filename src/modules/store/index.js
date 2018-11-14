dee9bf34611f4854997b1e4abcf72437f0f91457
Neal Fennimore <hi@neal.codes>
2018-11-13 21:04:50 -0500

    Fix middlewares export

--- src/modules/store/index.js
+++ src/modules/store/index.js
@@ -7,5 +7,7 @@ import 'regenerator-runtime/runtime';
  * Internal dependencies
  */
 import configureStore from './configure-store';
+import * as middlewares from './middlewares';
 
 export const store = configureStore();
+export { middlewares };
