f27355e352dcd4e069a29007f3dd9220d00e5452
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-12-07 09:52:49 +0100

    remove not required modifications, add some comments

--- src/Tribe/Process/Tester.php
+++ src/Tribe/Process/Tester.php
@@ -61,11 +61,8 @@ class Tribe__Process__Tester extends Tribe__Process__Handler {
 	public function maybe_handle() {
 		// Don't lock up other requests while processing
 		session_write_close();
-		$nonce = tribe_get_request_var( 'nonce' );
 
-		if ( ! wp_verify_nonce( $nonce, $this->identifier ) ) {
-			return false;
-		}
+		check_ajax_referer( $this->identifier, 'nonce' );
 
 		$this->handle();
 
