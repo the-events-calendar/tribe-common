5b5b21d3c84be14a570a5e72744304a1fcc07c75
Scott Kingsley Clark <lol@scottkclark.com>
2018-12-21 17:07:15 -0600

    Use tribe_callback
    Per @bordoni's feedback

--- src/Tribe/Service_Providers/Promoter_Connector.php
+++ src/Tribe/Service_Providers/Promoter_Connector.php
@@ -13,11 +13,12 @@ class Tribe__Service_Providers__Promoter_Connector extends tad_DI52_ServiceProvi
 	 * Binds and sets up implementations.
 	 */
 	public function register() {
-		tribe_singleton( 'common.promoter.auth', 'Tribe__Promoter__Auth' );
-		tribe_singleton( 'common.promoter.connector', 'Tribe__Promoter__Connector' );
-		tribe_singleton( 'common.promoter.pue', 'Tribe__Promoter__PUE', array( 'load' ) );
-		tribe_singleton( 'common.promoter.rewrite', 'Tribe__Promoter__Rewrite' );
-		tribe_singleton( 'common.promoter.view', 'Tribe__Promoter__View' );
+		tribe_singleton( 'promoter.auth', 'Tribe__Promoter__Auth' );
+		tribe_singleton( 'promoter.connector', 'Tribe__Promoter__Connector' );
+		tribe_singleton( 'promoter.pue', 'Tribe__Promoter__PUE', array( 'load' ) );
+		tribe_singleton( 'promoter.rewrite', 'Tribe__Promoter__Rewrite' );
+		tribe_singleton( 'promoter.view', 'Tribe__Promoter__View' );
+
 		$this->hook();
 	}
 
@@ -25,17 +26,13 @@ class Tribe__Service_Providers__Promoter_Connector extends tad_DI52_ServiceProvi
 	 * Setup hooks for classes.
 	 */
 	private function hook() {
-		$view      = tribe( 'common.promoter.view' );
-		$rewrite   = tribe( 'common.promoter.rewrite' );
-		$connector = tribe( 'common.promoter.connector' );
+		add_action( 'template_redirect', tribe_callback( 'promoter.view', 'display_auth_check_view' ), 10, 0 );
+		add_action( 'tribe_common_promoter_pre_rewrite', tribe_callback( 'promoter.rewrite', 'generate_core_rules' ) );
+		add_action( 'init', tribe_callback( 'promoter.rewrite', 'add_rewrite_tags' ) );
+		add_filter( 'generate_rewrite_rules', tribe_callback( 'promoter.rewrite', 'filter_generate' ) );
+		add_filter( 'rewrite_rules_array', tribe_callback( 'promoter.rewrite', 'remove_percent_placeholders' ), 25 );
+		add_action( 'save_post', tribe_callback( 'promoter.connector', 'notify_promoter_of_changes' ), 10, 1 );
 
-		add_action( 'template_redirect', array( $view, 'display_auth_check_view' ), 10, 0 );
-		add_action( 'tribe_common_promoter_pre_rewrite', array( $rewrite, 'generate_core_rules' ) );
-		add_action( 'init', array( $rewrite, 'add_rewrite_tags' ) );
-		add_filter( 'generate_rewrite_rules', array( $rewrite, 'filter_generate' ) );
-		add_filter( 'rewrite_rules_array', array( $rewrite, 'remove_percent_placeholders' ), 25 );
-		add_action( 'save_post', array( $connector, 'notify_promoter_of_changes' ), 10, 1 );
-
-		tribe( 'common.promoter.pue' );
+		tribe( 'promoter.pue' );
 	}
 }
