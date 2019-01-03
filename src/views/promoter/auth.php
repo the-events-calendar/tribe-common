7b8f85ed2e91e941650837e7ff59a0a747a9b74f
Scott Kingsley Clark <lol@scottkclark.com>
2019-01-03 11:59:26 -0600

    i18n escaping tweaks

--- src/views/promoter/auth.php
+++ src/views/promoter/auth.php
@@ -7,33 +7,33 @@
 	}
 </style>
 <?php if ( $authorized ) : ?>
-	<p><?php _e( 'You may now close this window.', 'tribe-common' ); ?>/p>
+	<p><?php esc_html_e( 'You may now close this window.', 'tribe-common' ); ?>/p>
 <?php else: ?>
 	<div class="site-wrap">
 		<main id="page-content" class="page page--push page--auth">
 			<div class="row row--reduced">
 				<div class="promoter-logo">
-					<span class="a11y-visual-hide"><?php _e( 'Promoter', 'tribe-common' ); ?>/span>
+					<span class="a11y-visual-hide"><?php esc_html_e( 'Promoter', 'tribe-common' ); ?>/span>
 				</div>
 
-				<h1 class="headline__large"><?php _e( 'Promoter would like to sync with your site', 'tribe-common' ); ?></h1>
-				<p><?php _e( 'Please enter your website\'s credentials to continue onboarding', 'tribe-common' ); ?></p>
+				<h1 class="headline__large"><?php esc_html_e( 'Promoter would like to sync with your site', 'tribe-common' ); ?></h1>
+				<p><?php esc_html_e( 'Please enter your website\'s credentials to continue onboarding', 'tribe-common' ); ?></p>
 
 				<form method="post">
 					<?php if ( ! $logged_in ) : ?>
 						<div class="form-control-group">
-							<label for="email" class="form-control-label form-control-custom-label"><?php _e( 'Username/Email', 'tribe-common' ); ?></label>
+							<label for="email" class="form-control-label form-control-custom-label"><?php esc_html_e( 'Username/Email', 'tribe-common' ); ?></label>
 							<input type="text" name="username" placeholder="" class="form-control form-control-custom-style--border-bottom" />
 						</div>
 						<div class="form-control-group">
-							<label for="email" class="form-control-label form-control-custom-label"><?php _e( 'Password', 'tribe-common' ); ?></label>
+							<label for="email" class="form-control-label form-control-custom-label"><?php esc_html_e( 'Password', 'tribe-common' ); ?></label>
 							<input type="password" name="password" class="form-control form-control-custom-style--border-bottom" />
 						</div>
 					<?php endif; ?>
-					<input type="hidden" value="<?php esc_attr_e( $promoter_key ); ?>" name="promoter_key"/>
-					<input type="hidden" value="<?php esc_attr_e( $license_key ); ?>" name="license_key"/>
+					<input type="hidden" value="<?php esc_attr( $promoter_key ); ?>" name="promoter_key"/>
+					<input type="hidden" value="<?php esc_attr( $license_key ); ?>" name="license_key"/>
 					<input type="hidden" value="1" name="promoter_authenticate"/>
-					<button class="btn btn--blue" type="submit"><?php _e( 'Authorize Promoter', 'tribe-common' ); ?></button>
+					<button class="btn btn--blue" type="submit"><?php esc_html_e( 'Authorize Promoter', 'tribe-common' ); ?></button>
 				</form>
 			</div>
 		</main>
@@ -41,9 +41,9 @@
 		<footer class="site-footer site-footer--locked">
 			<div class="row">
 				<div class="site-footer__logo">
-					<span class="a11y-visual-hide"><?php _e( 'Promoter', 'tribe-common' ); ?></span>
+					<span class="a11y-visual-hide"><?php esc_html_e( 'Promoter', 'tribe-common' ); ?></span>
 				</div>
-				<p class="site-footer__meta"><?php _e( '©2018 Promoter All rights reserved.', 'tribe-common' ); ?> <a href="https://promoter.theeventscalendar.com/privacy"><?php _e( 'Privacy', 'tribe-common' ); ?></a> <?php _e( 'and', 'tribe-common' ); ?> <a href="https://promoter.theeventscalendar.com/terms"><?php _e( 'Terms', 'tribe-common' ); ?></a>.
+				<p class="site-footer__meta">&copy;2018 <?php esc_html_e( 'Promoter All rights reserved.', 'tribe-common' ); ?> <a href="https://promoter.theeventscalendar.com/privacy"><?php esc_html_e( 'Privacy', 'tribe-common' ); ?></a> <?php esc_html_e( 'and', 'tribe-common' ); ?> <a href="https://promoter.theeventscalendar.com/terms"><?php esc_html_e( 'Terms', 'tribe-common' ); ?></a>.
 				</p>
 			</div>
 		</footer>
