494a8073e74d3c3ad9202e025e7f56309f71995f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-01-17 12:26:50 +0100

    regenerate vendor folder, no dev

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit7927142c97fdd9a483f80792a97cdf96::getLoader();
+return ComposerAutoloaderInit51da8c0da3d55517ede646d548efe684::getLoader();
