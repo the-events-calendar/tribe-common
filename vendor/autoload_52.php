c6883ce20cac178e44ffe81aea5d8b42e6e2fd1e
Scott Kingsley Clark <lol@scottkclark.com>
2019-04-04 17:39:21 -0500

    Update composer build

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit97f1ac9ce3c13802e00973bec54a10a2::getLoader();
+return ComposerAutoloaderInit892bd03f8af022fc8efd625b72e07bd6::getLoader();
