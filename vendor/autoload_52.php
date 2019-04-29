f14e2f2a0dcc23e572a085ae1761897944092410
Scott Kingsley Clark <lol@scottkclark.com>
2019-04-29 10:31:56 -0500

    Composer install

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,8 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-<<<<<<< HEAD
-return ComposerAutoloaderInitf9ea8b79d9865e2ae089824bb52e137f::getLoader();
-=======
-return ComposerAutoloaderInit8418c8624fb2214654dd7beadbf36e67::getLoader();
->>>>>>> master
+return ComposerAutoloaderInitdfa186765119b050ecb64f00cbe2b6bb::getLoader();
