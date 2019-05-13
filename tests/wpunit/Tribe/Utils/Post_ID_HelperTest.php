b36df006092ebdb4993aee0d530c156675d6588b
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-05-13 10:36:44 +0200

    fix(Main::post_id_helper): refactor code and make it a bit more robust
    Following Cliff's suggestion this commit tries to slim down the code leveraging the number of checks
    WordPress `get_post` function already does and, by extension, covers the case where the function is
    passed a valid integer number that is, but, not an actual post ID. In this last case the could
    would, previously, return the integer number making it so that, if the `Tribe__Main::post_id_helper`
    function is used in filtering/validating loops, the post would be flagged as valid; the correct
    behavior should be to filter out the post as invalid.
    
    re https://central.tri.be/issues/118994

--- tests/wpunit/Tribe/Utils/Post_ID_HelperTest.php
+++ tests/wpunit/Tribe/Utils/Post_ID_HelperTest.php
@@ -51,16 +51,16 @@ class Post_ID_HelperTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 	/**
-	 * @test         When passing positive integers, return the int as-is.
+	 * @test It should return false when passed non existing post ID
 	 *
 	 * @param int $post_id
 	 *
 	 * @dataProvider integers
 	 */
-	public function it_should_return_int_when_passed_positive_int( int $post_id ) {
+	public function it_should_return_false_when_passed_non_existing_post_id( int $post_id ) {
 		$actual = Main::post_id_helper( $post_id );
 
-		$this->assertEquals( $post_id, $actual );
+		$this->assertFalse( $actual );
 	}
 
 	/**
@@ -76,15 +76,15 @@ class Post_ID_HelperTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 	/**
-	 * It should return int when passed a numeric string.
+	 * It should return false when passed a numeric string for non existing post IDs.
 	 *
 	 * @test
 	 * @dataProvider numeric_string_inputs
 	 */
-	public function it_should_return_int_when_passed_numeric_strings( string $numeric_string ) {
+	public function it_should_return_int_when_passed_numeric_strings_for_non_existing_post_ids( string $numeric_string ) {
 		$actual = Main::post_id_helper( $numeric_string );
 
-		$this->assertEquals( (int) $numeric_string, $actual );
+		$this->assertFalse( $actual );
 	}
 
 	/**
@@ -128,5 +128,36 @@ class Post_ID_HelperTest extends \Codeception\TestCase\WPTestCase {
 			':-]'       => [ ':-]' ],
 			'π'         => [ 'π' ],
 		];
+	}
+
+	/**
+	 * It should return false when passed zero
+	 *
+	 * @test
+	 */
+	public function should_return_false_when_passed_zero() {
+		$this->assertFalse( Main::post_id_helper( 0 ) );
+	}
+
+	/**
+	 * It should return the post ID when passed numeric string of existing post ID
+	 *
+	 * @test
+	 */
+	public function should_return_the_post_id_when_passed_numeric_string_of_existing_post_id() {
+		$post_id = static::factory()->post->create();
+
+		$this->assertEquals( $post_id, Main::post_id_helper( (string) $post_id ) );
+	}
+
+	/**
+	 * It should return false if global post is set to non post object
+	 *
+	 * @test
+	 */
+	public function should_return_false_if_global_post_is_set_to_non_post_object() {
+		$GLOBALS['post'] = 'not-a-post-object';
+
+		$this->assertFalse( Main::post_id_helper() );
 	}
 }
