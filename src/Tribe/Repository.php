08702905ae48926105f8f5bcef794db2037032d6
mitogh <hello@crisoforo.com>
2019-06-13 17:15:42 -0500

    Restore code that is used and fix tests

--- src/Tribe/Repository.php
+++ src/Tribe/Repository.php
@@ -1344,16 +1344,25 @@ abstract class Tribe__Repository
 			$query = $this->get_query();
 
 			// The request property will be set during the `get_posts` method and empty before it.
-			if ( ! empty( $query->request ) ) {
-				return array_map( static function ( $post ) {
+			if ( empty( $query->request ) ) {
+				$query->set( 'fields', 'ids' );
+
+				// Tests: 941, Assertions: 1592, Errors: 2, Failures: 44.
+				return $query->get_posts();
+			}
+
+			return array_map(
+				static function ( $post ) {
 					if ( is_int( $post ) ) {
 						return $post;
 					}
 					$post_arr = (array) $post;
 
 					return Arr::get( $post_arr, 'ID', Arr::get( $post_arr, 'id', 0 ) );
-				}, $query->posts );
-			}
+				},
+				$query->posts
+			);
+
 		} catch ( Tribe__Repository__Void_Query_Exception $e ) {
 			/*
 			 * Extending classes might use this method to run sub-queries
