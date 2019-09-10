9ef258c3100d88a599cf7157d87beea1629cf03d
Stephen Page <stephen@tri.be>
2019-09-09 23:36:29 -0400

    Add some helper functions for forms

--- src/functions/template-tags/html.php
+++ src/functions/template-tags/html.php
@@ -66,12 +66,34 @@ function tribe_required( $required, $echo = true ) {
  */
 function tribe_required_label( $required, $echo = true ) {
 	if ( $required ) {
-		$required_string = '<span class="screen-reader-text">' . esc_html_x( '(required)', 'The associated field is required.', 'tribe-common' ) . '</span>';
+		$required_string = '<span class="screen-reader-text">' . esc_html_x( '(required)', 'The associated field is required.', 'tribe-common' ) . '</span><span class="tribe-required">*</span>';
 
 		if ( ! $echo ) {
 			return $required_string;
 		} else {
 			echo $required_string;
+		}
+	}
+}
+
+/**
+ * Get attributes for disabled fields.
+ *
+ * @since TBD
+ *
+ * @param boolean $disabled If the field is disabled.
+ * @param boolean $echo     Whether to echo the string or return it.
+ *
+ * @return string|void If echo is false, returns $disabled_string.
+ */
+function tribe_disabled( $disabled, $echo = true ) {
+	if ( $disabled ) {
+		$disabled_string = 'disabled aria-disabled="true"';
+
+		if ( ! $echo ) {
+			return $disabled_string;
+		} else {
+			echo $disabled_string;
 		}
 	}
 }
