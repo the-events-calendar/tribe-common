034bffa0534d601e7f18f66175fa0de168ff2512
Clifford Paulick <tko@tourkick.com>
2019-09-13 10:00:14 -0500

    dataProvider needs to be public

--- tests/unit/functions/utilsTest.php
+++ tests/unit/functions/utilsTest.php
@@ -12,24 +12,24 @@ class utilsTest extends Unit {
 	}
 
 	/**
-	 * @dataProvider provider_get_unsuccessful_class_instance
-	 */
-	public function test_unsuccessfully_getting_a_class_instance( $class ) {
-		$this->assertNull( tribe_get_class_instance( $class ) );
-	}
-
-	/**
 	 * Different ways to successfully get a class instance.
 	 *
 	 * @see \tribe_get_class_instance()
 	 *
 	 * @return Generator
 	 */
-	private function provider_get_successful_class_instance() {
+	public function provider_get_successful_class_instance() {
 		yield 'class slug registered with tribe()' => [ 'assets' ];
 		yield 'class string not registered with tribe()' => [ 'Tribe__App_Shop' ];
 		yield 'class string that has instance()' => [ new Tribe__App_Shop() ];
 		yield 'class string that has get_instance()' => [ 'WP_Post' ];
+	}
+
+	/**
+	 * @dataProvider provider_get_unsuccessful_class_instance
+	 */
+	public function test_unsuccessfully_getting_a_class_instance( $class ) {
+		$this->assertNull( tribe_get_class_instance( $class ) );
 	}
 
 	/**
@@ -39,10 +39,10 @@ class utilsTest extends Unit {
 	 *
 	 * @return Generator
 	 */
-	private function provider_get_unsuccessful_class_instance() {
+	public function provider_get_unsuccessful_class_instance() {
 		yield 'empty string' => [ '' ];
-		yield 'class not found' => [ 'ABC_123_XYZ' ];
-		yield 'not an object or a string' => [ [] ];
+		yield 'string that is neither an existing class name nor slug' => [ 'ABC_123_XYZ' ];
+		yield 'neither an object nor a string' => [ [] ];
 	}
 
 }
