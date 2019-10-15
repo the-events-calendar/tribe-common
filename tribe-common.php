1423eba42e35ee39b55c2bdad4b921aa89f51092
Scott Kingsley Clark <lol@scottkclark.com>
2019-10-15 14:25:54 -0500

    Update version

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tribe Common
 Description: An event settings framework for managing shared options
-Version: 4.9.19
+Version: 4.9.21
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/1x
 Text Domain: tribe-common
