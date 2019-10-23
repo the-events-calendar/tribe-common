be17b0cc1137228be13c0ba9b5dbf85287cf1775
Juanfra Aldasoro <juanfraa@gmail.com>
2019-10-23 08:43:07 -0300

    Add alt to the post thumbnail object.

--- src/Tribe/Utils/Post_Thumbnail.php
+++ src/Tribe/Utils/Post_Thumbnail.php
@@ -188,6 +188,9 @@ class Post_Thumbnail implements \ArrayAccess, \Serializable {
 		$srcset                   = wp_get_attachment_image_srcset( $thumbnail_id );
 		$thumbnail_data['srcset'] = ! empty( $srcset ) ? $srcset : false;
 
+		$alt                   = get_the_title( $thumbnail_id );
+		$thumbnail_data['alt'] = ! empty( $alt ) ? $alt : false;
+
 		/**
 		 * Filters the post thumbnail data and information that will be returned for a specific post.
 		 *
