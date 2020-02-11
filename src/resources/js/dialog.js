90e9920d0a2833952e21bf888be46fa65f803f6b
Juanfra Aldasoro <juanfraa@gmail.com>
2020-02-11 10:44:12 +0100

    Improve spacing.

--- src/resources/js/dialog.js
+++ src/resources/js/dialog.js
@@ -3,15 +3,15 @@ tribe.dialogs = tribe.dialogs || {};
 tribe.dialogs.dialogs = tribe.dialogs.dialogs || [];
 tribe.dialogs.events = tribe.dialogs.events || {};
 
-	( function ( obj ) {
+	( function( obj ) {
 		'use strict';
 
 		document.addEventListener(
 			'DOMContentLoaded',
-			function () {
-				tribe.dialogs.dialogs.forEach(function(dialog) {
+			function() {
+				tribe.dialogs.dialogs.forEach( function( dialog ) {
 					var objName     = 'dialog_obj_' + dialog.id;
-					window[objName] = new window.A11yDialog({
+					window[objName] = new window.A11yDialog( {
 						appendTarget: dialog.appendTarget,
 						bodyLock: dialog.bodyLock,
 						closeButtonAriaLabel: dialog.closeButtonAriaLabel,
@@ -24,26 +24,26 @@ tribe.dialogs.events = tribe.dialogs.events || {};
 						overlayClickCloses: dialog.overlayClickCloses,
 						trigger: dialog.trigger,
 						wrapperClasses: dialog.wrapperClasses
-					});
+					} );
 
-					window[objName].on('show', function (dialogEl, event) {
+					window[ objName ].on( 'show', function( dialogEl, event ) {
 						if ( event ) {
 							event.preventDefault();
 							event.stopPropagation();
 						}
 
-						jQuery( tribe.dialogs.events ).trigger( dialog.showEvent, [dialogEl, event] );
+						jQuery( tribe.dialogs.events ).trigger( dialog.showEvent, [ dialogEl, event ] );
 					});
 
-					window[objName].on('hide', function (dialogEl, event) {
+					window[ objName ].on( 'hide', function ( dialogEl, event ) {
 						if ( event ) {
 							event.preventDefault();
 							event.stopPropagation();
 						}
 
-						jQuery( tribe.dialogs.events ).trigger( dialog.closeEvent, [dialogEl, event] );
+						jQuery( tribe.dialogs.events ).trigger( dialog.closeEvent, [ dialogEl, event ] );
 					});
 				});
 			}
 		)
-	})(tribe.dialog);
+	})( tribe.dialog );
