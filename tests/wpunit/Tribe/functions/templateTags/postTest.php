4ecaa226c61c78b8ba81e170963fd06c929132b9
jesseeproductions <info@jesseeproductions.com>
2020-03-05 10:49:02 -0500

    ECP-345 - revise initial test and add another

--- tests/wpunit/Tribe/functions/templateTags/postTest.php
+++ tests/wpunit/Tribe/functions/templateTags/postTest.php
@@ -1,21 +1,8 @@
 <?php
+
 namespace Tribe\functions\templateTags;
 
 class postTest extends \Codeception\TestCase\WPTestCase {
-
-	public function setUp() {
-		// before
-		parent::setUp();
-
-		// your set up methods here
-	}
-
-	public function tearDown() {
-		// your tear down methods here
-
-		// then
-		parent::tearDown();
-	}
 
 	/**
 	 * @test
@@ -23,17 +10,36 @@ class postTest extends \Codeception\TestCase\WPTestCase {
 	public function it_does_not_run_apply_filters_when_doing_filter_and_create_nested_level_error() {
 		// Remove autop to make it easier to match content.
 		remove_filter( 'the_content', 'wpautop' );
-		$content = 'Test Content';
-		$post_id = $this->factory->post->create();
-		$expected = $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
+
+		// Set Test Content, Create an Event, and Expected Content
+		$content  = 'Test Content';
+		$post_id  = $this->factory->post->create( [ 'post_content' => 'Event Content' ] );
+		$expected = $content . 'Event ContentFILTERING';
 
 		// Run tribe_get_the_content inside of the_content filter call.
 		add_filter( 'the_content', static function ( $content ) use ( $post_id ) {
 
-				return $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
-			} );
+			return $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
+		} );
 
 		$content = apply_filters( 'the_content', $content );
+
+		$this->assertEquals( $expected, $content );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_runs_apply_filters() {
+		// Remove autop to make it easier to match content.
+		remove_filter( 'the_content', 'wpautop' );
+
+		// Set Test Content, Create an Event, and Expected Content
+		$content  = 'Test Content';
+		$post_id  = $this->factory->post->create( [ 'post_content' => 'Event Content' ] );
+		$expected = $content . 'Event ContentFILTERING';
+
+		$content = $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
 
 		$this->assertEquals( $expected, $content );
 	}
