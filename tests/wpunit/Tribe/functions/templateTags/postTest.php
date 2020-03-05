21d6f9b7f8f2c1803a3932810bb08374c60668ea
jesseeproductions <info@jesseeproductions.com>
2020-03-04 21:01:30 -0500

    ECP-345 - commenting fixes

--- tests/wpunit/Tribe/functions/templateTags/postTest.php
+++ tests/wpunit/Tribe/functions/templateTags/postTest.php
@@ -21,13 +21,13 @@ class postTest extends \Codeception\TestCase\WPTestCase {
 	 * @test
 	 */
 	public function it_does_not_run_apply_filters_when_doing_filter_and_create_nested_level_error() {
-		// Remove autop as to make it easier to match content.
+		// Remove autop to make it easier to match content.
 		remove_filter( 'the_content', 'wpautop' );
 		$content = 'Test Content';
 		$post_id = $this->factory->post->create();
 		$expected = $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
 
-		// Use lambda function to run tribe_get_the_content inside of the_content filter call.
+		// Run tribe_get_the_content inside of the_content filter call.
 		add_filter( 'the_content', static function ( $content ) use ( $post_id ) {
 
 				return $content . tribe_get_the_content( null, false, $post_id ) . 'FILTERING';
