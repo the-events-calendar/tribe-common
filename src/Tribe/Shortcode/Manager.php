70f82079446e82b93a168e2aa782f83c48447624
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-04-14 22:20:09 -0400

    Pull the Shortcode Management classes into Common

---
+++ src/Tribe/Shortcode/Manager.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Shortcodes manager for Tribe plugins.
+ *
+ * @package Tribe\Shortcode
+ * @since   TBD
+ */
+namespace Tribe\Shortcode;
+
+/**
+ * Class Shortcode Manager.
+ *
+ * @since  TBD
+ *
+ * @package Tribe\Shortcode
+ */
+class Manager {
+	/**
+	 * Get the list of shortcodes available for handling.
+	 *
+	 * @since  TBD
+	 *
+	 * @return array An associative array of shortcodes in the shape `[ <slug> => <class> ]`
+	 */
+	public function get_registered_shortcodes() {
+		$shortcodes = [];
+
+		/**
+		 * Allow the registering of shortcodes into the our Tribe plugins.
+		 *
+		 * @since  TBD
+		 *
+		 * @var array An associative array of shortcodes in the shape `[ <slug> => <class> ]`
+		 */
+		$shortcodes = apply_filters( 'tribe_shortcodes', $shortcodes );
+
+		return $shortcodes;
+	}
+
+	/**
+	 * Verifies if a given shortcode slug is registered for handling.
+	 *
+	 * @since  TBD
+	 *
+	 * @param  string $slug Which slug we are checking if is registered.
+	 *
+	 * @return bool
+	 */
+	public function is_shortcode_registered( $slug ) {
+		$registered_shortcodes = $this->get_registered_shortcodes();
+		return isset( $registered_shortcodes[ $slug ] );
+	}
+
+	/**
+	 * Verifies if a given shortcode class name is registered for handling.
+	 *
+	 * @since  TBD
+	 *
+	 * @param  string $class_name Which class name we are checking if is registered.
+	 *
+	 * @return bool
+	 */
+	public function is_shortcode_registered_by_class( $class_name ) {
+		$registered_shortcodes = $this->get_registered_shortcodes();
+		return in_array( $class_name, $registered_shortcodes );
+	}
+
+	/**
+	 * Add new shortcodes handler to catch the correct strings.
+	 *
+	 * @since  TBD
+	 *
+	 * @return void
+	 */
+	public function add_shortcodes() {
+		$registered_shortcodes = $this->get_registered_shortcodes();
+
+		// Add to WordPress all of the registered Shortcodes
+		foreach ( $registered_shortcodes as $shortcode => $class_name ) {
+			add_shortcode( $shortcode, [ $this, 'handle' ] );
+		}
+	}
+
+	/**
+	 * Makes sure we are correctly handling the Shortcodes we manage.
+	 *
+	 * @since  TBD
+	 *
+	 * @param array  $arguments Set of arguments passed to the Shortcode at hand.
+	 * @param string $content   Contents passed to the shortcode, inside of the open and close brackets.
+	 * @param string $shortcode Which shortcode tag are we handling here.
+	 *
+	 * @return string
+	 */
+	public function handle( $arguments, $content, $shortcode ) {
+		$registered_shortcodes = $this->get_registered_shortcodes();
+
+		// Bail when we try to handle an unregistered shortcode (shouldn't happen)
+		if ( ! $this->is_shortcode_registered( $shortcode ) ) {
+			return false;
+		}
+
+		$instance = new $registered_shortcodes[ $shortcode ];
+		$instance->setup( $arguments, $content );
+
+		return $instance->get_html();
+	}
+}
---
+++ src/Tribe/Shortcode/Shortcode_Interface.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * The interface all shortcodes should implement.
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Shortcode
+ */
+namespace Tribe\Shortcode;
+
+/**
+ * Interface Shortcode_Interface
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Shortcode
+ */
+interface Shortcode_Interface {
+
+	/**
+	 * Returns the shortcode slug.
+	 *
+	 * The slug should be the one that will allow the shortcode to be built by the shortcode class by slug.
+	 *
+	 * @since  TBD
+	 *
+	 * @return string The shortcode slug.
+	 */
+	public function get_registration_slug();
+
+	/**
+	 * Configures the base variables for an instance of shortcode.
+	 *
+	 * @since  TBD
+	 *
+	 * @param array  $arguments Set of arguments passed to the Shortcode at hand.
+	 * @param string $content   Contents passed to the shortcode, inside of the open and close brackets.
+	 *
+	 * @return void
+	 */
+	public function setup( $arguments, $content );
+
+	/**
+	 * Returns the arguments for the shortcode parsed correctly with defaults applied.
+	 *
+	 * @since  TBD
+	 *
+	 * @param array  $arguments Set of arguments passed to the Shortcode at hand.
+	 *
+	 * @return array
+	 */
+	public function parse_arguments( $arguments );
+
+	/**
+	 * Returns the array of arguments for this shortcode after applying the validation callbacks.
+	 *
+	 * @since  TBD
+	 *
+	 * @param array  $arguments Set of arguments passed to the Shortcode at hand.
+	 *
+	 * @return array
+	 */
+	public function validate_arguments( $arguments );
+
+	/**
+	 * Returns the array of callbacks for this shortcode's arguments.
+	 *
+	 * @since  TBD
+	 *
+	 * @return array
+	 */
+	public function get_validate_arguments_map();
+
+	/**
+	 * Returns a shortcode default arguments.
+	 *
+	 * @since  TBD
+	 *
+	 * @return array
+	 */
+	public function get_default_arguments();
+
+	/**
+	 * Returns a shortcode arguments after been parsed.
+	 *
+	 * @since  TBD
+	 *
+	 * @return array
+	 */
+	public function get_arguments();
+
+	/**
+	 * Returns a shortcode argument after been parsed.
+	 *
+	 * @uses  Tribe__Utils__Array::get For index fetching and Default.
+	 *
+	 * @since  TBD
+	 *
+	 * @param array  $index   Which index we indent to fetch from the arguments.
+	 * @param array  $default Default value if it doesnt exist.
+	 *
+	 * @return array
+	 */
+	public function get_argument( $index, $default = null );
+
+	/**
+	 * Returns a shortcode HTML code.
+	 *
+	 * @since  TBD
+	 *
+	 * @return string
+	 */
+	public function get_html();
+}
