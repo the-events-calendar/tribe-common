462610b92e0177b89004f8297533bc0ecf0e8c9e
Scott Kingsley Clark <lol@scottkclark.com>
2020-05-06 20:49:40 -0500

    Update src/Tribe/Utils/Strings.php

--- src/Tribe/Utils/Strings.php
+++ src/Tribe/Utils/Strings.php
@@ -51,7 +51,7 @@ class Strings {
 	 * @param string $replace The replacement string.
 	 * @param string $subject The string to do the search and replace from.
 	 *
-	 * @return string
+	 * @return string The string with the last occurrence of a given value replaced.
 	 */
 	public static function replace_last( $search, $replace, $subject ) {
 		$position = strrpos( $subject, $search );
