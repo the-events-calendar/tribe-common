fa84a56bb7071e5bca48c32918114a72d9abb809
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-05-19 23:44:05 -0400

    Apply suggestions from code review
    Co-authored-by: Scott Kingsley Clark <lol@scottkclark.com>

--- src/functions/template-tags/post.php
+++ src/functions/template-tags/post.php
@@ -32,9 +32,6 @@ function tribe_get_the_content( $more_link_text = null, $strip_teaser = false, $
 
 	$has_blocks = function_exists( 'has_blocks' ) && has_blocks( $content );
 	// If blocks are present we need to run the content filter.
-	if ( function_exists( 'has_blocks' ) ) {
-		$has_blocks = has_blocks( $content );
-	}
 
 	if ( $has_blocks || ! doing_filter( 'the_content' ) ) {
 		/**
