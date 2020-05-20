8b4ddbe24fa989c600ab5f8ecf8ec973d3115b96
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-05-19 23:43:29 -0400

    Update src/functions/template-tags/post.php
    Co-authored-by: theAverageDev (Luca Tumedei) <luca@theaveragedev.com>

--- src/functions/template-tags/post.php
+++ src/functions/template-tags/post.php
@@ -31,7 +31,7 @@ function tribe_get_the_content( $more_link_text = null, $strip_teaser = false, $
 	}
 
 	$has_blocks = false;
-	// If blocks are present we need to for sure pass the content filter.
+	// If blocks are present we need to run the content filter.
 	if ( function_exists( 'has_blocks' ) ) {
 		$has_blocks = has_blocks( $content );
 	}
