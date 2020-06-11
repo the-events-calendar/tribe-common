0d1293205bf8c9c9f8c255841eac60cd5eb84bc1
Brian <jesseeproductions@users.noreply.github.com>
2020-06-11 10:33:17 -0400

    formatting change

--- src/Tribe/Rewrite.php
+++ src/Tribe/Rewrite.php
@@ -872,7 +872,7 @@ class Tribe__Rewrite {
 		$perma_query_vars     = [];
 		$url_components = parse_url($url);
 		$url_path = Arr::get( $url_components, 'path', '/' );
-		$site_path = parse_url(home_url(),PHP_URL_PATH);
+		$site_path = parse_url( home_url(), PHP_URL_PATH );
 		if ( ! ( empty( $site_path ) && '/' !== $site_path ) ) {
 			// The current site is in a sub-directory: the site path should be dropped from the request path.
 			$url_path = str_replace( $site_path, '', $url_path );
