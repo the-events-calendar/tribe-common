c3959de44df3d29ccbcf67ca90c054e88a62e7e3
Stephen Page <stephenjpage@gmail.com>
2020-07-02 17:07:50 -0400

    Create Service Provider singleton

--- src/Tribe/Service_Providers/Body_Classes.php
+++ src/Tribe/Service_Providers/Body_Classes.php
@@ -20,6 +20,7 @@ class Body_Classes extends \tad_DI52_ServiceProvider {
 	 */
 	public function register() {
 		tribe_singleton( Body_Class_Object::class, Body_Class_Object::class );
+		tribe_singleton( 'tribe_body_classes_provider', $this );
 
 		/**
 		 * Allows plugins to hook into the register action to register views, etc.
