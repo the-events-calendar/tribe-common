03193a17ace99407a0042f549408cf3ca1e50850
Stephen Page <stephen@tri.be>
2020-07-09 08:56:49 -0400

    Update src/Tribe/Service_Providers/Body_Classes.php
    Co-authored-by: Gustavo Bordoni <bordoni.dev@gmail.com>

--- src/Tribe/Service_Providers/Body_Classes.php
+++ src/Tribe/Service_Providers/Body_Classes.php
@@ -20,7 +20,7 @@ class Body_Classes extends \tad_DI52_ServiceProvider {
 	 */
 	public function register() {
 		tribe_singleton( Body_Class_Object::class, Body_Class_Object::class );
-		tribe_singleton( 'common.body_classes_provider', $this );
+		tribe_singleton( 'common.service_providers.body_classes', $this );
 
 		/**
 		 * Allows plugins to hook into the register action to register views, etc.
