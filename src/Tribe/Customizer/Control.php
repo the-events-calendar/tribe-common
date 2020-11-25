35dd2f9742d143ae5f86a054ddb9332d71006c3c
Luca Tumedei <luca@theaveragedev.com>
2020-11-25 12:18:32 +0100

    fix(Customizer/Controls) udpate the logic and setting for the Heading control

--- src/Tribe/Customizer/Control.php
+++ src/Tribe/Customizer/Control.php
@@ -1,34 +1,20 @@
 <?php
+/**
+ * The base class for any custom Control.
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Customizer
+ */
 
 namespace Tribe\Customizer;
 
+/**
+ * Class Control
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Customizer
+ */
 abstract class Control extends \WP_Customize_Control {
-	// public $type = 'customtext';
-	
-	/**
-	 * Custom heading which displays above the control
-	 *
-	 * @since TBD
-	 *
-	 * @access public
-	 * @var string
-	 */
-	// public $extra = '';
-	
-	/**
-	 * Render the control's content
-	 *
-	 * @since TBD
-	 */
-	/* abstract public function render_content() {
-		?>
-		<label>
-				<?php if ( isset( $this->extra ) ) { ?>
-					<h4><?php echo esc_html( $this->extra ); ?></h4>
-				<?php } ?>
-				<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
-				<input type="text" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> />
-		</label>
-		<?php
-	} */
 }
--- src/Tribe/Customizer/Controls/Heading.php
+++ src/Tribe/Customizer/Controls/Heading.php
@@ -1,10 +1,42 @@
 <?php
+/**
+ * Models a Customizer heading, a Control just in name, it does not control any setting.
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Customizer\Controls
+ */
 
 namespace Tribe\Customizer\Controls;
 
 use Tribe\Customizer\Control;
 
+/**
+ * Class Heading
+ *
+ * @since   TBD
+ *
+ * @package Tribe\Customizer\Controls
+ */
 class Heading extends Control {
+	/**
+	 * Anyone able to set theme options will be able to see the header.
+	 *
+	 * @since TBD
+	 *
+	 * @var string
+	 */
+	public $capability = 'edit_theme_options';
+
+	/**
+	 * The heading does not control any setting.
+	 *
+	 * @since TBD
+	 *
+	 * @var array<string,mixed>
+	 */
+	public $settings = [];
+
 	/**
 	 * Render the control's content
 	 *
@@ -12,7 +44,7 @@ class Heading extends Control {
 	 */
 	public function render_content() {
 		?>
-			<h4><?php echo esc_html( $this->label ); ?></h4>
+		<h4><?php echo esc_html( $this->label ); ?></h4>
 		<?php
 	}
 }
