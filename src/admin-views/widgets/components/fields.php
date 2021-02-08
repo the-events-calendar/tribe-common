ec418d565f08b330fb1bf2683095e0bd09a95722
Gustavo Bordoni <bordoni.dev@gmail.com>
2021-02-07 23:40:04 -0500

    Modfied code to move fields into common

---
+++ src/admin-views/widgets/components/fields.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Admin View: Widget Component Fields Container
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/admin-views/widgets/components/fields.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link    http://evnt.is/1aiy
+ *
+ * @var \Tribe__Template     $this                      Instance of the template including this file.
+ * @var array<mixed>         $fields_container_classes  (optional) HTML classes used for the form element
+ * @var Widget_Abstract      $widget_obj                An instance of the widget abstract.
+ * @var array<array,mixed>   $fields                    An array of admin fields to display in the widget form.
+ *
+ * @version TBD
+ */
+
+use  Tribe\Widget\Widget_Abstract;
+
+$default_classes = [
+	'tribe-widget-fields'
+];
+
+$classes = array_merge( $default_classes, $this->get( 'fields_container_classes', [] ) );
+?>
+
+<div
+	<?php tribe_classes( $classes ); ?>
+>
+	<?php
+	foreach ( $fields as $field ) {
+		// Try to load the component for this field type.
+		$this->template( "widgets/components/fields/{$field['type']}", $field );
+
+		// Sets the current field for possible usage inside of the entry point.
+		$this->set_values( [ 'field' => $field ] );
+
+		// Allow fields that were not registered as components to have something loaded.
+		$this->do_entry_point( "field-{$field['type']}" );
+	}
+	?>
+</div>
