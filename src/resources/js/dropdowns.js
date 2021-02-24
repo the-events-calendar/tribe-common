8edbf8b5d53c96015efc35f5908270cee16426e2
Gustavo Bordoni <bordoni.dev@gmail.com>
2021-02-24 13:28:11 -0500

    Move things back into .ready

--- src/resources/js/dropdowns.js
+++ src/resources/js/dropdowns.js
@@ -536,7 +536,7 @@ var tribe_dropdowns = window.tribe_dropdowns || {};
 		return $elements;
 	};
 
-	$( window ).on( 'load', function() {
+	$( function() {
 		$( obj.selector.dropdown ).tribe_dropdowns();
 	} );
 
