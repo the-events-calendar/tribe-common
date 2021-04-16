2ac1699b7cfe952cbc2ff013a80c7918794222cf
Juanfra Aldasoro <juanfraa@gmail.com>
2021-04-16 17:36:45 +0200

    Initial implementation of Onboarding: Tours & Hints.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -639,6 +639,7 @@ class Tribe__Main {
 		tribe_register_provider( Tribe\Log\Service_Provider::class );
 		tribe_register_provider( Tribe\Service_Providers\Crons::class );
 		tribe_register_provider( Tribe\Service_Providers\Widgets::class );
+		tribe_register_provider( Tribe\Service_Providers\Onboarding::class );
 	}
 
 	/**
---
+++ src/Tribe/Service_Providers/Onboarding.php
@@ -0,0 +1,131 @@
+<?php
+namespace Tribe\Service_Providers;
+
+/**
+ * Class Onboarding
+ *
+ * @since TBD
+ *
+ * Handles the registration and creation of our async process handlers.
+ */
+class Onboarding extends \tad_DI52_ServiceProvider {
+
+	/**
+	 * Binds and sets up implementations.
+	 *
+	 * @since TBD
+	 */
+	public function register() {
+		tribe_singleton( 'onboarding.tour', '\Tribe\Onboarding\Tour' );
+		tribe_singleton( 'onboarding.hints', '\Tribe\Onboarding\Hints' );
+
+		$this->hooks();
+	}
+
+	/**
+	 * Set up hooks for classes.
+	 *
+	 * @since TBD
+	 */
+	private function hooks() {
+		add_action( 'tribe_common_loaded', [ $this, 'register_assets' ] );
+
+		add_action( 'admin_enqueue_scripts', tribe_callback( 'onboarding.tour', 'localize' ) );
+		add_action( 'admin_enqueue_scripts', tribe_callback( 'onboarding.hints', 'localize' ) );
+	}
+
+	/**
+	 * Register assets associated with onboarding.
+	 *
+	 * @since TBD
+	 */
+	public function register_assets() {
+		$main = \Tribe__Main::instance();
+
+		tribe_asset(
+			$main,
+			'intro-js',
+			'node_modules/intro.js/intro.js',
+			[],
+			[ 'admin_enqueue_scripts' ],
+			[
+				'groups'       => 'tribe-onboarding',
+				'conditionals' => [ $this, 'should_enqueue_assets' ],
+			]
+		);
+
+		tribe_asset(
+			$main,
+			'intro-styles',
+			'node_modules/intro.js/introjs.css',
+			[],
+			[ 'admin_enqueue_scripts' ],
+			[
+				'groups'       => 'tribe-onboarding',
+				'conditionals' => [ $this, 'should_enqueue_assets' ],
+			]
+		);
+
+		tribe_asset(
+			$main,
+			'tribe-onboarding-styles',
+			'onboarding.css',
+			[ 'intro-styles' ],
+			[ 'admin_enqueue_scripts' ],
+			[
+				'groups'       => 'tribe-onboarding',
+				'conditionals' => [ $this, 'should_enqueue_assets' ],
+			]
+		);
+
+		tribe_asset(
+			$main,
+			'tribe-onboarding-js',
+			'onboarding.js',
+			[
+				'tribe-common',
+				'intro-js'
+			],
+			[ 'admin_enqueue_scripts' ],
+			[
+				'groups'       => 'tribe-onboarding',
+				'in_footer'    => false,
+				'localize'     => [
+					'name' => 'TribeOnboarding',
+					'data' => [
+						'hintButtonLabel' => __( 'Got it', 'tribe-common' ),
+					],
+				],
+				'conditionals' => [ $this, 'should_enqueue_assets' ],
+			]
+		);
+	}
+
+	public function should_enqueue_assets() {
+		$is_enabled = $this->is_enabled();
+		$tour_data  = tribe( 'onboarding.tour' )->data();
+		$hints_data = tribe( 'onboarding.hints' )->data();
+
+		return $is_enabled && ( ! empty( $tour_data ) || ! empty( $hints_data ) );
+	}
+
+	/**
+	 * Check if the onboarding is enabled or not.
+	 *
+	 * @since TBD
+	 *
+	 * @return bool
+	 */
+	public function is_enabled() {
+		/**
+		 * Filter to disable tribe onboarding
+		 *
+		 * @since TBD
+		 *
+		 * @param bool $disabled If we want to disable the onboarding.
+		 */
+		$is_disabled = (bool) apply_filters( 'tribe_onboarding_disable', false );
+
+		return is_admin() && ! $is_disabled;
+	}
+}
