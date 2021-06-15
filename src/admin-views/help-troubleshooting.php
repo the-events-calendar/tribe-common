a9cc65b8d6306bd90f184a856b1412b3554b1ce3
eugenekyale <eugene.kyale@gmail.com>
2021-06-15 17:16:14 +0300

    initial sys info mockup

--- src/admin-views/help-troubleshooting.php
+++ src/admin-views/help-troubleshooting.php
@@ -112,6 +112,31 @@
 		<?php endforeach; ?>
 	</div>
     
+	<?php // sys info?>
+	<div class="tribe-events-admin__system-information">
+		<div class="tribe-events-admin__system-information-content">
+			<h3 class="tribe-events-admin__system-information-title">
+				<?php esc_html_e('System Information', 'tribe-common'); ?>
+			</h3>	
+			<p class="tribe-events-admin__system-information-description">
+				<?php esc_html_e('Please opt-in below to automatically share your system information with our support team. This will allow us to assist you faster if you post in our help desk.', 'tribe-common'); ?>	
+			</p>
+			<div class="tribe-events-admin__system-information-select">
+				<input type="checkbox" name="userToggleSystemInformation" value="1">
+				<label>
+					<?php esc_html_e('Yes, automatically share my system information with The Events Calendar support team*', 'tribe-common'); ?>
+				</label>
+			</div>
+			<small>
+				<?php esc_html_e('* Your system information will only be used by The Events Calendar support team. All information is stored securely. We do not share this information with any third parties.', 'tribe-common'); ?>
+			</small>
+		</div>
+
+		<div class="tribe-events-admin__system-information-widget">
+			&nbsp;
+		</div>
+	</div>
+
     <?php // cta section?>
 	<div class="tribe-events-admin-cta">
 		<img
