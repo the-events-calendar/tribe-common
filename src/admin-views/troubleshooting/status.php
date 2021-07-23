d8c3d6693e6d2282c9cfd8598a5f1a1cc354696d
eugenekyale <eugene.kyale@gmail.com>
2021-07-23 21:11:28 +0300

    EA server connection

--- src/admin-views/troubleshooting/status.php
+++ src/admin-views/troubleshooting/status.php
@@ -53,7 +53,7 @@ $show_third_party_accounts = ! is_network_admin();
         <td>
             <img
                 src="<?php echo esc_url(tribe_resource_url($status_icons[ $icon ], false, null, $main)); ?>"
-                alt="<?php esc_attr_e('success-icon', 'tribe-common'); ?>"
+                alt=""
             />
             <?php echo esc_html( $message ); ?>
         </td>
@@ -97,7 +97,7 @@ $show_third_party_accounts = ! is_network_admin();
         <td>
             <img
                 src="<?php echo esc_url(tribe_resource_url($status_icons[ $icon ], false, null, $main)); ?>"
-                alt="<?php esc_attr_e('success-icon', 'tribe-common'); ?>"
+                alt=""
             />
             <?php echo esc_html( $message ); ?>
         </td>
@@ -115,18 +115,49 @@ $show_third_party_accounts = ! is_network_admin();
         </th>
     </tr>
 
+    <?php
+		$icon = 'success';
+		$notes     = '&nbsp;';
+
+		$ea_server = tribe( 'events-aggregator.service' )->api()->domain;
+		$up        = tribe( 'events-aggregator.service' )->get( 'status/up' );
+
+		if ( ! $up || is_wp_error( $up ) ) {
+			$icon = 'error';
+			/* translators: %s: Event Aggregator Server URL */
+			$message  = sprintf( __( 'Not connected to %s', 'the-events-calendar' ), $ea_server );
+			$notes = esc_html__( 'The server is not currently responding', 'the-events-calendar' );
+		} elseif ( is_object( $up ) && is_object( $up->data ) && isset( $up->data->status ) && 400 <= $up->data->status ) {
+			// this is a rare condition that should never happen
+			// An example case: the route is not defined on the EA server
+			$icon = 'warning';
+
+			/* translators: %s: Event Aggregator Server URL */
+			$message = sprintf( __( 'Not connected to %s', 'the-events-calendar' ), $ea_server );
+
+			$notes  = __( 'The server is responding with an error:', 'the-events-calendar' );
+			$notes .= '<pre>';
+			$notes .= esc_html( $up->message );
+			$notes .= '</pre>';
+		} else {
+			/* translators: %s: Event Aggregator Server URL */
+			$message = sprintf( __( 'Connected to %s', 'the-events-calendar' ), $ea_server );
+		}
+    ?>
+
     <tr class="tribe-events-admin__ea-status-table-dark">
         <td>
             <?php esc_html_e('Server Connection', 'tribe-common'); ?>
         </td>
         <td>
             <img
-                src="<?php echo esc_url(tribe_resource_url('images/help/error-icon.svg', false, null, $main)); ?>"
-                alt="<?php esc_attr_e('error-icon', 'tribe-common'); ?>"
+                src="<?php echo esc_url(tribe_resource_url($status_icons[ $icon ], false, null, $main)); ?>"
+                alt=""
             />
-            <?php esc_html_e('Connected to https://ea.theeventscalendar.com', 'tribe-common'); ?></td>
-        </tr>
-
+            <?php echo esc_html( $message ); ?>
+        </td>
+        <td><?php echo $notes;  // Escaping handled above. ?></td>
+    </tr>
     <tr>
         <td>
             <?php esc_html_e( 'Scheduler Status', 'tribe-common' ); ?>
