629bba289fb9263155a98e8ca6bf6007787f47f5
eugenekyale <eugene.kyale@gmail.com>
2021-07-23 22:00:48 +0300

    break ea status files into manageable chunks

---
+++ src/admin-views/troubleshooting/ea-status/scheduler-status.php
@@ -0,0 +1,26 @@
+<?php
+    $icon = 'success';
+    $notes = '&nbsp;';
+
+    if ( defined( 'DISABLE_WP_CRON' ) && true === DISABLE_WP_CRON ) {
+        $icon = 'warning';
+        $message      = __( 'WP Cron not enabled', 'tribe-common' );
+        $notes     = esc_html__( 'Scheduled imports may not run reliably', 'tribe-common' );
+    } else {
+        $message = __( 'WP Cron enabled', 'tribe-common' );
+    }
+?>
+
+<tr>
+    <td>
+        <?php esc_html_e( 'Scheduler Status', 'tribe-common' ); ?>
+    </td>
+    <td>
+        <img
+            src="<?php echo esc_url(tribe_resource_url($status_icons[ $icon ], false, null, $main)); ?>"
+            alt=""
+        />
+        <?php echo esc_html( $message ); ?>
+    </td>
+    <td><?php echo $notes;  // Escaping handled above. ?></td>
+</tr>
