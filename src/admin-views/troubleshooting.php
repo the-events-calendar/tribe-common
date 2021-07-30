36d54e4b1be4a64998369d38a6e84da48c47fc05
eugenekyale <eugene.kyale@gmail.com>
2021-07-30 18:26:38 +0300

    format jQuery

--- src/admin-views/troubleshooting.php
+++ src/admin-views/troubleshooting.php
@@ -25,28 +25,31 @@
 
 <?php // this is inline jQuery / javascript for extra simplicity */ ?>
 <script>
-	if ( jQuery( '.tribe-events-admin__issues-found-card .tribe-events-admin__issues-found-title' )
-	.hasClass( 'active' ) ) {
+	if ( 
+		jQuery( '.tribe-events-admin__issues-found-card .tribe-events-admin__issues-found-title' )
+			.hasClass( 'active' ) 
+	) {
 		jQuery( '.tribe-events-admin__issues-found-card .tribe-events-admin__issues-found-card-title.active' )
-		.closest( '.tribe-events-admin__issues-found-card' )
-		.find( '.tribe-events-admin__issues-found-description' )
-		.show();
+			.closest( '.tribe-events-admin__issues-found-card' )
+			.find( '.tribe-events-admin__issues-found-description' )
+			.show();
 	}
 	jQuery( '.tribe-events-admin__issues-found-card .tribe-events-admin__issues-found-card-title' )
-	.on( 'click', function () {
-		if ( jQuery( this ).hasClass( 'active' ) ) {
-			jQuery( this )
-			.removeClass( 'active' )
-			.closest( '.tribe-events-admin__issues-found-card' )
-			.find( '.tribe-events-admin__issues-found-card-description' )
-			.slideUp( 200 );
-		}
-		else {
-			jQuery( this )
-			.addClass( 'active' )
-			.closest( '.tribe-events-admin__issues-found-card' )
-			.find( '.tribe-events-admin__issues-found-card-description' )
-			.slideDown( 200 );
-		}
-	});
+		.on( 'click', function () {
+			var $this = jQuery( this );
+
+			if ( jQuery( this ).hasClass( 'active' ) ) {
+				$this
+					.removeClass( 'active' )
+					.closest( '.tribe-events-admin__issues-found-card' )
+					.find( '.tribe-events-admin__issues-found-card-description' )
+					.slideUp( 200 );
+			} else {
+				$this
+					.addClass( 'active' )
+					.closest( '.tribe-events-admin__issues-found-card' )
+					.find( '.tribe-events-admin__issues-found-card-description' )
+					.slideDown( 200 );
+			}
+		} );
 </script>
