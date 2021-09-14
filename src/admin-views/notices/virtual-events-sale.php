1a0c3993d65a9a8355cf4df61868e269555b1077
Mike Cotton <codingmusician@gmail.com>
2021-09-14 13:24:07 -0500

    convert spaces to tabs

--- src/admin-views/notices/virtual-events-sale.php
+++ src/admin-views/notices/virtual-events-sale.php
@@ -8,27 +8,27 @@
  */
 ?>
 <div class="tribe-marketing-notice-virtual_events_sale">
-    <div class="tribe-marketing-notice-virtual_events_sale-banner_ad-wrap">
-        <div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title-wrap">
-            <div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title">
-                <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-red">S</span>
-                <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-blue">A</span>
-                <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-yellow">L</span>
-                <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-green">E</span>
-            </div>
-        </div>
-        <div class="tribe-marketing-notice-virtual_events_sale-banner_ad-copy">
-            Get 20% off the Virtual Events Add-On with coupon code 
-            <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-code">virtual-2021</span>
-            <span class="tribe-marketing-notice-virtual_events_sale-banner_ad-limit"> for a limited time only!</span>
-        </div>
-        <div class="tribe-marketing-notice-virtual_events_sale-banner_ad-button">
-            <a 
-                target="_blank" 
-                href="<?php echo esc_url( $cta_url ); ?>" 
-                class="tribe-marketing-notice-virtual_events_sale-banner_ad-link">
-                    Order Now
-            </a>
-        </div>
-    </div>
+	<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-wrap">
+		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title-wrap">
+			<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title">
+				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-red">S</span>
+				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-blue">A</span>
+				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-yellow">L</span>
+				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-green">E</span>
+			</div>
+		</div>
+		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-copy">
+			Get 20% off the Virtual Events Add-On with coupon code 
+			<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-code">virtual-2021</span>
+			<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-limit"> for a limited time only!</span>
+		</div>
+		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-button">
+			<a 
+				target="_blank" 
+				href="<?php echo esc_url( $cta_url ); ?>" 
+				class="tribe-marketing-notice-virtual_events_sale-banner_ad-link">
+					Order Now
+			</a>
+		</div>
+	</div>
 </div>
