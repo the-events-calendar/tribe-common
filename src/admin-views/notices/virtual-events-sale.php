7bf96f162dc2625023375cad3fb6586438073366
Mike Cotton <codingmusician@gmail.com>
2021-09-14 13:47:43 -0500

    use correct BEM class names

--- src/admin-views/notices/virtual-events-sale.php
+++ src/admin-views/notices/virtual-events-sale.php
@@ -7,27 +7,27 @@
  * @var string $cta_url The short URL for black friday.
  */
 ?>
-<div class="tribe-marketing-notice-virtual_events_sale">
-	<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-wrap">
-		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title-wrap">
-			<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-title">
-				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-red">S</span>
-				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-blue">A</span>
-				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-yellow">L</span>
-				<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-green">E</span>
+<div class="tribe-marketing-notice__ve-sale">
+	<div class="tribe-marketing-notice__ve-sale__wrap">
+		<div class="tribe-marketing-notice__ve-sale__title-wrap">
+			<div class="tribe-marketing-notice__ve-sale__ad-title">
+				<span class="tribe-marketing-notice__ve-sale__red">S</span>
+				<span class="tribe-marketing-notice__ve-sale__blue">A</span>
+				<span class="tribe-marketing-notice__ve-sale__yellow">L</span>
+				<span class="tribe-marketing-notice__ve-sale__green">E</span>
 			</div>
 		</div>
-		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-copy">
+		<div class="tribe-marketing-notice__ve-sale__copy">
 			Get 20% off the Virtual Events Add-On with coupon code 
-			<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-code">virtual-2021</span>
-			<span class="tribe-marketing-notice-virtual_events_sale-banner_ad-limit"> for a limited time only!</span>
+			<span class="tribe-marketing-notice__ve-sale__code">virtual-2021</span>
+			<span class="tribe-marketing-notice__ve-sale__limit"> for a limited time only!</span>
 		</div>
-		<div class="tribe-marketing-notice-virtual_events_sale-banner_ad-button">
+		<div class="tribe-marketing-notice__ve-sale__button">
 			<a 
 				target="_blank" 
 				rel="noopener noreferrer"
 				href="<?php echo esc_url( $cta_url ); ?>" 
-				class="tribe-marketing-notice-virtual_events_sale-banner_ad-link">
+				class="tribe-marketing-notice__ve-sale__link">
 					Order Now
 			</a>
 		</div>
