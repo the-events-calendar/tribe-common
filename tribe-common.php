6981998d5c078fc2c5505828ba4466230b6f0459
Luca Tumedei <luca@theaveragedev.com>
2021-10-27 15:34:11 +0200

    set version to 6.0.0-RBE-Beta1

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tribe Common
 Description: An event settings framework for managing shared options
-Version: 4.14.6
+Version: 6.0.0-RBE-Beta1
 Author: The Events Calendar
 Author URI: http://evnt.is/1x
 Text Domain: tribe-common
