64e6c0ad880a7eafa85e53c7c16f4d2a3a290466
Edwin Daniels <stratease@gmail.com>
2021-11-19 11:20:42 -0800

    updated paths in docs to reflect the correct path for overwriting templates

--- src/views/v2/components/icons/list.php
+++ src/views/v2/components/icons/list.php
@@ -3,7 +3,7 @@
  * View: List Icon
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/v2/components/icons/list.php
+ * [your-theme]/tribe/events/v2/components/icons/list.php
  *
  * See more documentation about our views templating system.
  *
