ec0a16caba98107a41cd08ead0b223d3b22192b9
Ricardo Moraleida <moraleida@gmail.com>
2021-11-30 00:42:14 -0300

    add clarification comment

--- src/Tribe/Values/Price.php
+++ src/Tribe/Values/Price.php
@@ -2,6 +2,10 @@
 
 namespace Tribe\Values;
 
+/**
+ * This is what a Price class should look like when implemented in a plugin. A base implementation will be available
+ * with an empty `set_up_currency_details()` method.
+ */
 class Price extends Abstract_Currency {
 
 	public function set_up_currency_details() {
