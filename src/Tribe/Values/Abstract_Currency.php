feae7364f138eaec4d80d9c3d49b0f39d1d28619
Ricardo Moraleida <moraleida@gmail.com>
2021-12-01 22:35:44 -0300

    move methods down the inheritance to leaf classes

--- src/Tribe/Values/Abstract_Currency.php
+++ src/Tribe/Values/Abstract_Currency.php
@@ -4,50 +4,18 @@ namespace Tribe\Values;
 
 abstract class Abstract_Currency extends Abstract_Value implements Currency_Interface {
 
-	private $currency = '';
+	private $currency;
 
 	private $decimal;
 
 	private $string;
 
-	private $currency_code = 'USD';
-
-	private $currency_separator_decimal = '.';
-
-	private $currency_separator_thousands = ',';
-
-	private $currency_symbol = '$';
-
-	private $currency_symbol_position = 'prefix';
-
-	private $use_tec_currency_locale = true;
-
 	use ValueFormatting;
 
 	public function __construct( $amount = 0 ) {
-		parent::__construct( $amount );
-
 		$this->set_up_currency_details();
-	}
 
-	public function get_currency_code() {
-		return $this->currency_code;
-	}
-
-	public function get_currency_symbol() {
-		return $this->currency_symbol;
-	}
-
-	public function get_currency_symbol_position() {
-		return $this->currency_symbol_position;
-	}
-
-	public function get_currency_separator_decimal() {
-		return $this->currency_separator_decimal;
-	}
-
-	public function get_currency_separator_thousands() {
-		return $this->currency_separator_thousands;
+		parent::__construct( $amount );
 	}
 
 	public function get_currency() {
@@ -66,11 +34,11 @@ abstract class Abstract_Currency extends Abstract_Value implements Currency_Inte
 		$this->currency = $this->to_currency( $this->get_normalized_value() );
 	}
 
-	private function set_string_value() {
-		$this->string = $this->to_string( $this->get_normalized_value() );
-	}
-
 	private function set_decimal_value() {
 		$this->decimal = $this->to_decimal( $this->get_normalized_value() );
+	}
+
+	private function set_string_value() {
+		$this->string = $this->to_string( $this->get_normalized_value() );
 	}
 }
--- src/Tribe/Values/Currency_Interface.php
+++ src/Tribe/Values/Currency_Interface.php
@@ -8,11 +8,17 @@ interface Currency_Interface {
 
 	public function get_decimal();
 
+	public function get_string();
+
 	public function get_currency_code();
 
 	public function get_currency_symbol();
 
 	public function get_currency_symbol_position();
+
+	public function get_currency_separator_decimal();
+
+	public function get_currency_separator_thousands();
 
 	public function set_up_currency_details();
 
