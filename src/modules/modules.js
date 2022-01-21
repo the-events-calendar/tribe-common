eeaeedcdc0600d5a582eadb97a603d6686f3bb79
Paul <kim.paul.ms@gmail.com>
2022-01-21 19:38:38 +0800

    add entry point for main and modules

---
+++ src/modules/modules.js
@@ -0,0 +1,11 @@
+import * as redux from 'redux';
+import * as reduxSaga from 'redux-saga';
+import * as reactRedux from 'react-redux';
+import propTypes from 'prop-types';
+
+export {
+	redux,
+	reduxSaga,
+	reactRedux,
+	propTypes,
+};
