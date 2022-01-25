91e3a276ef88c8be01c70ee2c3c9136acdef1c85
Camwyn <stephen@theeventscalendar.com>
2022-01-25 10:40:00 -0500

    A bit of cleanup

--- src/Tribe/Editor/Compatibility/Divi.php
+++ src/Tribe/Editor/Compatibility/Divi.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Tribe\Editor\Compatibility;
+
+use WP_Post;
+
 /**
  * Editor Compatibility with Divi theme's builder option.
  *
@@ -30,17 +33,45 @@ class Divi {
 	 * @since TBD
 	 */
 	public function hooks() {
-		global $current_screen;
-		$screen = empty( $current_screen ) ? $current_screen : get_current_screen();
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX  ) {
+			return;
+		}
 
-		$good_screens = [
-			'post-new.php',
-			'post.php',
-		];
+		if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
+			return;
+		}
 
-		//if ( in_array( $current_screen, $good_screens ) ) {
-			add_filter( 'tribe_editor_should_load_blocks', [ $this, 'filter_tribe_editor_should_load_blocks' ], 20 );
-		//}
+		// This happens too early for most plugin/theme tests. Let's try and bail when we're not needed.
+		$path = basename( parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) );
+		$new = false !== stripos( $path, 'post-new' );
+
+		if ( $new ) {
+			// Are we creating a new event?
+			$post_type = filter_input( INPUT_GET, 'post_type', FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE );
+			if ( \Tribe__Events__Main::POSTTYPE !== $post_type ) {
+				return;
+			}
+		} else {
+			// Are we editing an event?
+			if ( false === stripos( $path, 'post' ) ) {
+				return;
+			}
+
+			$action = filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE );
+			if ( 'edit' !== $action ) {
+				return;
+			}
+
+			$post_id = filter_input( INPUT_GET, 'post', FILTER_SANITIZE_STRING, FILTER_NULL_ON_FAILURE );
+			$event = get_post( $post_id );
+
+			if (! $event instanceof WP_Post || \Tribe__Events__Main::POSTTYPE !== $event->post_type ) {
+				return;
+			}
+		}
+
+		$foo = '';
+		add_filter( 'tribe_editor_should_load_blocks', [ $this, 'filter_tribe_editor_should_load_blocks' ], 20 );
 	}
 
 	public static function is_divi_active() {
@@ -58,15 +89,13 @@ class Divi {
 	 * @return boolean Whether we should force blocks or classic.
 	 */
 	public function filter_tribe_editor_should_load_blocks( $should_load_blocks ) {
-		$boo = apply_filters( 'et_builder_enable_classic_editor', $should_load_blocks );
+		// et_enable_classic_editor
+		$divi_option = get_option( 'et_divi', [] );
 
-		if ( function_exists( 'et_builder_enable_classic_editor' ) ) {
-			$baz = (
-				function_exists('et_get_option' )
-				&& static::$classic_on === et_get_option( static::$classic_key, static::$classic_off )
-			);
-
-			return ! $baz;
+		if ( empty( $divi_option[ static::$classic_key ] ) ) {
+			return $should_load_blocks;
+		} else if ( static::$classic_on === $divi_option[ static::$classic_key ] ) {
+			return false;
 		}
 
 		return $should_load_blocks;
