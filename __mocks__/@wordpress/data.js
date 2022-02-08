e81d66157657173a49cdd0c8d59e2dfc90c377e8
Luca Tumedei <luca@theaveragedev.com>
2022-02-08 13:49:22 +0100

    test(__mocks__) remove unused mocks

--- __mocks__/@wordpress/data.js
+++ __mocks__/@wordpress/data.js
@@ -4,5 +4,3 @@
 import { noop } from 'lodash';
 
 export const select = noop;
-export const withSelect = () => ( component ) => component;
-export const withDispatch = () => ( component ) => component;
