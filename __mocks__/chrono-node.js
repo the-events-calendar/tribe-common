9f98e46189d0478003cd8d0ea27692fca175f6cd
Paul <kim.paul.ms@gmail.com>
2022-02-17 21:58:08 +0800

    fix broken test for chrono-node

--- __mocks__/chrono-node.js
+++ __mocks__/chrono-node.js
@@ -1,15 +1,13 @@
 import moment from 'moment';
 
-export default {
-	parse: ( label ) => {
-		const start = label ? moment( label ) : null;
-		const results = [];
-		if ( start && start.isValid() ) {
-			const date = {
-				date: () => start
-			}
-			results.push( { start: date, end: date } );
+export const parse = ( label ) => {
+	const start = label ? moment( label ) : null;
+	const results = [];
+	if ( start && start.isValid() ) {
+		const date = {
+			date: () => start
 		}
-		return results;
+		results.push( { start: date, end: date } );
 	}
+	return results;
 };
