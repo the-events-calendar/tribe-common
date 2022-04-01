04eb3bdf553bd1204e6d9b8704fc531575c8b482
Camwyn <stephen@theeventscalendar.com>
2022-04-01 15:12:19 -0400

    version update

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -20,7 +20,7 @@ class Tribe__Main {
 	const OPTIONNAME          = 'tribe_events_calendar_options';
 	const OPTIONNAMENETWORK   = 'tribe_events_calendar_network_options';
 
-	const VERSION             = '4.14.16';
+	const VERSION             = '4.14.18';
 
 	const FEED_URL            = 'https://theeventscalendar.com/feed/';
 
