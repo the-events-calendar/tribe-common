fb53544d69cd97f21209f5644ef48ce155da3785
jesseeproductions <info@jesseeproductions.com>
2022-04-04 11:55:42 -0400

    EA-123 - Added EA status row showing if it is enabled or disabled in the Event Aggregator system status.

---
+++ src/admin-views/troubleshooting/ea-status/current-status.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * View: Troubleshooting - EA Status Table Current Status.
+ *
+ * @since TBD
+ *
+ *
+ * @param array<string|string> $status_icons An array of icons for the EA Status table.
+ * @param  $main An array of icons for the EA Status table.
+ *
+ */
+
+$icon     = 'success';
+$notes    = '&nbsp;';
+$message  = esc_html_x( 'Imports Enabled in Settings', '', 'tribe-common' );
+$notes    = '&nbsp;';
+$disabled = tribe_get_option( 'tribe_aggregator_disable', false );
+
+if ( $disabled ) {
+	$icon         = 'error';
+	$message      = _x( 'Imports disabled in Settings', '', 'tribe-common' );
+	$settings_url = Tribe__Settings::instance()->get_url( array( 'tab' => 'imports' ) );
+	$notes 		  = sprintf(
+		'<a href="%1$s">%2$s</a>',
+		$settings_url,
+		_x( 'Edit Import Settings', '','tribe-common' ),
+	);
+}
+?>
+<tr class="tribe-events-admin__ea-status-table-dark">
+	<td>
+		<?php esc_html_e( 'Enabled', 'tribe-common' ); ?>
+	</td>
+	<td>
+		<img
+			src="<?php echo esc_url( tribe_resource_url( $status_icons[ $icon ], false, null, $main ) ); ?>"
+			alt=""
+		/>
+		<?php echo esc_html( $message ); ?>
+	</td>
+	<td><?php echo $notes;  // Escaping handled above. ?></td>
+</tr>
