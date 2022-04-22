4386c81ac462eadf5417049b4480d4765d3527d2
Camwyn <stephen@theeventscalendar.com>
2022-04-22 09:54:47 -0400

    Remove debug

--- src/functions/editor.php
+++ src/functions/editor.php
@@ -9,17 +9,9 @@
 if ( ! function_exists( 'tec_is_full_site_editor' ) ) {
 	function tec_is_full_site_editor() {
 		if ( function_exists( 'wp_is_block_theme' ) ) {
-			codecept_debug([
-				'wp_is_block_theme()',
-				(int) wp_is_block_theme()
-			]);
 			return (bool) wp_is_block_theme();
 		} else if ( function_exists( 'gutenberg_is_fse_theme' ) ) {
 			// This function has returned wp_is_block_theme since 2021/12 so this is just in case someone hasn't updated.
-			codecept_debug([
-				'gutenberg_is_fse_theme()',
-				(int) gutenberg_is_fse_theme()
-			]);
 			return (bool) gutenberg_is_fse_theme();
 		}
 
