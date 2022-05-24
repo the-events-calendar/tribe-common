8d72b8cbb37c1a45f912c1250d967da9a19c817d
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-05-24 14:32:51 -0400

    Improve HTML Class handling for templating

--- src/Tribe/Utils/Element_Classes.php
+++ src/Tribe/Utils/Element_Classes.php
@@ -207,11 +207,12 @@ class Element_Classes {
 					$this->parse( $value );
 				}
 			} elseif ( is_string( $key ) ) {
-				if ( ! is_bool( $value ) ) {
-					throw new \UnexpectedValueException( 'Value for key ' . $key . ' must be of type boolean' );
+				if ( $value instanceof \Closure || is_callable( $value ) ) {
+					// function() {}
+					$value = $value( $this->results );
 				}
 
-				$this->parse_string( $key, $value );
+				$this->parse_string( $key, tribe_is_truthy( $value ) );
 			}
 		}
 	}
