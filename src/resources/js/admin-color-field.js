5fb0490cc648a689adf647cbc77b5e4a4b4f3d4c
Mike Cotton <codingmusician@gmail.com>
2022-08-01 10:01:26 -0500

    fix spacing/indents

--- src/resources/js/admin-color-field.js
+++ src/resources/js/admin-color-field.js
@@ -6,51 +6,51 @@
  * 
  * @type {PlainObject}
  */
- tribe.settings = tribe.settings || {};
- tribe.settings.fields = tribe.settings.fields || {};
+tribe.settings = tribe.settings || {};
+tribe.settings.fields = tribe.settings.fields || {};
+
+/**
+ * Configure image field for settings in global Tribe variable.
+ * 
+ * @since TBD
+ * 
+ * @type {PlainObject}
+ */
+tribe.settings.fields.color = {};
+
+/**
+ * Intializes the image field for settings.
+ * 
+ * @since TBD
+ * 
+ * @param {PlainObject} $   jQuery
+ * @param {PlainObject} obj tribe.settings.fields.image
+ */
+( function( $, obj ) {
+	'use strict';
+
+	/**
+	 * Selectors used for configuration and setup
+	 *
+	 * @since TBD
+	 *
+	 * @type {PlainObject}
+	 */
+	obj.selectors = {
+		colorFieldInput: '.tec-admin__settings-color-field-input',
+	};
+
+	/**
+	 * Handles the initialization of color fields when Document is ready.
+	 *
+	 * @since TBD
+	 *
+	 * @return {void}
+	 */
+	obj.init = function() {
+		$( obj.selectors.colorFieldInput ).wpColorPicker();
+	};
  
- /**
-  * Configure image field for settings in global Tribe variable.
-  * 
-  * @since TBD
-  * 
-  * @type {PlainObject}
-  */
- tribe.settings.fields.color = {};
- 
- /**
-  * Intializes the image field for settings.
-  * 
-  * @since TBD
-  * 
-  * @param {PlainObject} $   jQuery
-  * @param {PlainObject} obj tribe.settings.fields.image
-  */
- ( function( $, obj ) {
-     'use strict';
- 
-     /**
-      * Selectors used for configuration and setup
-      *
-      * @since TBD
-      *
-      * @type {PlainObject}
-      */
-     obj.selectors = {
-         colorFieldInput: '.tec-admin__settings-color-field-input',
-     };
- 
-     /**
-      * Handles the initialization of color fields when Document is ready.
-      *
-      * @since TBD
-      *
-      * @return {void}
-      */
-     obj.init = function() {
-        $( obj.selectors.colorFieldInput ).wpColorPicker();
-     };
- 
-     $( obj.init );
- 
- } )( jQuery, tribe.settings.fields.color );
+	$( obj.init );
+
+} )( jQuery, tribe.settings.fields.color );
