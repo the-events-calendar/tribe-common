8c8acdb259b266a4bf1d1bdc2b4f9841455a42a4
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-09-06 16:26:22 -0400

    Remove unwanted parenthesis on tests

--- tests/wpunit/Tribe/Context_LocationsTest.php
+++ tests/wpunit/Tribe/Context_LocationsTest.php
@@ -16,7 +16,7 @@ class Context_LocationsTest extends \Codeception\TestCase\WPTestCase {
 	public function is_main_query_should_not_error_on_wp_query_null() {
 		$GLOBALS['wp_query'] = null;
 
-		$context = (new \Tribe__Context());
+		$context = new \Tribe__Context();
 		$context->dangerously_repopulate_locations();
 
 		$this->assertFalse( $context->is( 'is_main_query' ), 'When WP_Query is `null` it should be false.' );
@@ -28,7 +28,7 @@ class Context_LocationsTest extends \Codeception\TestCase\WPTestCase {
 	public function is_main_query_should_not_error_on_wp_query_is_array() {
 		$GLOBALS['wp_query'] = [ 'test' ];
 
-		$context = (new \Tribe__Context());
+		$context = new \Tribe__Context();
 		$context->dangerously_repopulate_locations();
 
 		$this->assertFalse( $context->get( 'is_main_query' ), 'When WP_Query is `array` it should be false.' );
@@ -40,7 +40,7 @@ class Context_LocationsTest extends \Codeception\TestCase\WPTestCase {
 	public function is_main_query_should_not_error_on_wp_query_not_WP_Query_instance() {
 		$GLOBALS['wp_query'] = (object) [];
 
-		$context = (new \Tribe__Context());
+		$context = new \Tribe__Context();
 		$context->dangerously_repopulate_locations();
 
 		$this->assertFalse( $context->get( 'is_main_query' ), 'When WP_Query is `stdObj` it should be false.' );
@@ -53,7 +53,7 @@ class Context_LocationsTest extends \Codeception\TestCase\WPTestCase {
 		$GLOBALS['wp_query'] = new \WP_Query();
 		$GLOBALS['wp_the_query'] = $GLOBALS['wp_query'];
 
-		$context = (new \Tribe__Context());
+		$context = new \Tribe__Context();
 		$context->dangerously_repopulate_locations();
 
 		$this->assertTrue( $context->get( 'is_main_query' ), 'When global $wp_query and $wp_the_query are the same it returns true.' );
