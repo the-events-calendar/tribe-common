a4125989ae51ca8ced7a7afbb5a80e07444866da
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-09-06 16:17:52 -0400

    Make sure tests are passign correctly

--- tests/wpunit/Tribe/Context_LocationsTest.php
+++ tests/wpunit/Tribe/Context_LocationsTest.php
@@ -13,33 +13,49 @@ class Context_LocationsTest extends \Codeception\TestCase\WPTestCase {
 	/**
 	 * @test
 	 */
-	public function should_not_fail_on_wp_query_null() {
-		global $wp_query;
+	public function is_main_query_should_not_error_on_wp_query_null() {
+		$GLOBALS['wp_query'] = null;
 
-		$wp_query = null;
+		$context = (new \Tribe__Context());
+		$context->dangerously_repopulate_locations();
 
-		$this->assertFalse( tribe_context()->is( 'is_main_query' ), 'When WP_Query is `null` it should be false.' );
+		$this->assertFalse( $context->is( 'is_main_query' ), 'When WP_Query is `null` it should be false.' );
 	}
 
 	/**
 	 * @test
 	 */
-	public function should_not_fail_on_wp_query_is_array() {
-		global $wp_query;
+	public function is_main_query_should_not_error_on_wp_query_is_array() {
+		$GLOBALS['wp_query'] = [ 'test' ];
 
-		$wp_query = [ 'test' ];
+		$context = (new \Tribe__Context());
+		$context->dangerously_repopulate_locations();
 
-		$this->assertFalse( tribe_context()->is( 'is_main_query' ), 'When WP_Query is `array` it should be false.' );
+		$this->assertFalse( $context->get( 'is_main_query' ), 'When WP_Query is `array` it should be false.' );
 	}
 
 	/**
 	 * @test
 	 */
-	public function should_not_fail_on_wp_query_not_WP_Query_instance() {
-		global $wp_query;
+	public function is_main_query_should_not_error_on_wp_query_not_WP_Query_instance() {
+		$GLOBALS['wp_query'] = (object) [];
 
-		$wp_query = (object) [];
+		$context = (new \Tribe__Context());
+		$context->dangerously_repopulate_locations();
 
-		$this->assertFalse( tribe_context()->is( 'is_main_query' ), 'When WP_Query is `stdObj` it should be false.' );
+		$this->assertFalse( $context->get( 'is_main_query' ), 'When WP_Query is `stdObj` it should be false.' );
+	}
+
+	/**
+	 * @test
+	 */
+	public function is_main_query_should_return_true_when_both_global_instances_are_the_same() {
+		$GLOBALS['wp_query'] = new \WP_Query();
+		$GLOBALS['wp_the_query'] = $GLOBALS['wp_query'];
+
+		$context = (new \Tribe__Context());
+		$context->dangerously_repopulate_locations();
+
+		$this->assertTrue( $context->get( 'is_main_query' ), 'When global $wp_query and $wp_the_query are the same it returns true.' );
 	}
 }
