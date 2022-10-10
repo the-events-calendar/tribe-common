fd952aa332f441ec4598fe1ccbb54d27c77c6b4b
Ricardo Moraleida <moraleida@gmail.com>
2022-10-10 17:41:48 -0300

    improve tests

--- tests/wpunit/Tribe/Repository/ReadRelationshipsTest.php
+++ tests/wpunit/Tribe/Repository/ReadRelationshipsTest.php
@@ -15,28 +15,21 @@ class ReadRelationshipsTest extends ReadTestBase {
 		$book_ids = $this->factory()->post->create_many( 5, [ 'post_type' => 'book' ] );
 		$review_ids = $this->factory()->post->create_many( 5, [ 'post_type' => 'review' ] );
 
-		foreach( $review_ids as $i => $id ) {
-			if ( $i === 0 ) {
-				continue;
-			}
-
-			if ( $i % 2 === 0 ) {
-				// Creates 2 reviews for the first book
-				update_post_meta( $id, 'book_id', $book_ids[0] );
-			}
-
-			// Create one review for a non-existing book
-			update_post_meta( $review_ids[0], 'book_id', rand( 1000, PHP_INT_MAX ) );
-		}
+		// Creates 2 reviews for book[4]
+		update_post_meta( $review_ids[0], 'book_id', $book_ids[4] );
+		update_post_meta( $review_ids[1], 'book_id', $book_ids[4] );
+		// Creates 1 reviews for book[3]
+		update_post_meta( $review_ids[2], 'book_id', $book_ids[3] );
+		// Creates 1 reviews for book[2]
+		update_post_meta( $review_ids[3], 'book_id', $book_ids[2] );
+		// Create 1 review for a non-existing book
+		update_post_meta( $review_ids[4], 'book_id', rand( 1000, PHP_INT_MAX ) );
+		// books 0 and 1 do not have reviews.
 
 		$repository = $this->repository();
 		$not_related_ids = $repository->by_args( [ 'post_type' => 'book' ] )->by_not_related_to( [ 'book_id' ] )->get_ids();
 
-		// Now we should have 2 reviews for book[0], one review for a non-existing book.
-		// And 4 books without reviews.
-		$this->assertEquals( 4, count( $not_related_ids ) );
-		// And the books without reviews should be the ones we created, except for book[0]
-		array_shift( $book_ids );
-		$this->assertEqualSets( $book_ids, $not_related_ids );
+		$this->assertEquals( 2, count( $not_related_ids ) );
+		$this->assertEqualSets( [ $book_ids[0], $book_ids[1] ], $not_related_ids );
 	}
 }
