019970150865cac48096b656e49d2fbecc46758c
Ricardo Moraleida <moraleida@gmail.com>
2022-10-10 16:14:27 -0300

    add testing for method by_not_related_to()

--- tests/wpunit/Tribe/Repository/ReadRelationshipsTest.php
+++ tests/wpunit/Tribe/Repository/ReadRelationshipsTest.php
@@ -29,9 +29,14 @@ class ReadRelationshipsTest extends ReadTestBase {
 			update_post_meta( $review_ids[0], 'book_id', rand( 1000, PHP_INT_MAX ) );
 		}
 
-		// Now we should have 2 reviews for book[0], one review for a non-existing book. And 4 books without reviews.
 		$repository = $this->repository();
 		$not_related_ids = $repository->by_args( [ 'post_type' => 'book' ] )->by_not_related_to( [ 'book_id' ] )->get_ids();
+
+		// Now we should have 2 reviews for book[0], one review for a non-existing book.
+		// And 4 books without reviews.
 		$this->assertEquals( 4, count( $not_related_ids ) );
+		// And the books without reviews should be the ones we created, except for book[0]
+		array_shift( $book_ids );
+		$this->assertEqualSets( $book_ids, $not_related_ids );
 	}
 }
