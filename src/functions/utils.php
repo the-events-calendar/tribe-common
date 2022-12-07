545e272c2343bbd2f75c69b0f61975b2b4cbfb34
Luca Tumedei <luca@theaveragedev.com>
2022-12-07 17:23:50 +0100

    refactor(functions/utils) better replacement for filter string

--- src/functions/utils.php
+++ src/functions/utils.php
@@ -1127,7 +1127,7 @@ if ( ! function_exists( 'tribe_sanitize_deep' ) ) {
 			return $value;
 		}
 		if ( is_string( $value ) ) {
-			$value = filter_var( $value, FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_NO_ENCODE_QUOTES );
+			$value = htmlspecialchars( stripslashes( strip_tags( $value ) ), ENT_QUOTES );
 			return $value;
 		}
 		if ( is_int( $value ) ) {
