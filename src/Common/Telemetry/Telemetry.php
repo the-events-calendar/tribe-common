c11632c01e36df51de3f1332d75cee6c2d49755a
Camwyn <stephen@theeventscalendar.com>
2023-03-01 23:10:54 -0500

    Add some filtering

--- src/Common/Telemetry/Telemetry.php
+++ src/Common/Telemetry/Telemetry.php
@@ -96,21 +96,23 @@ final class Telemetry {
 		If both, use The Events Calendar
 		*/
 
-		$this->optin_args = [
+		$optin_args = [
 			'plugin_logo'           => tribe_resource_url( 'images/logo/tec-brand.svg', false, null, \Tribe__Main::instance() ),
 			'plugin_logo_width'     => 'auto',
 			'plugin_logo_height'    => 42,
-			'plugin_logo_alt'       => 'The Events Calendar Logo',
-			'plugin_name'           => 'The Events Calendar',
+			'plugin_logo_alt'       => 'TEC Common Logo',
+			'plugin_name'           => 'TEC Common',
 			'plugin_slug'           => static::$plugin_slug,
 			'user_name'             => $user_name,
 			'permissions_url'       => '#',
 			'tos_url'               => '#',
 			'privacy_url'           => '#',
-			'opted_in_plugins_text' => __( 'See which plugins you have opted in to tracking for', 'the-events-calendar' ),
-			'heading'               => __( 'We hope you love The Events Calendar.', 'the-events-calendar' ),
-			'intro'                 => __( "Hi, {$user_name}! This is an invitation to help our StellarWP community. If you opt-in, some data about your usage of The Events Calendar and future StellarWP Products will be shared with our teams (so they can work their butts off to improve). We will also share some helpful info on WordPress, and our products from time to time. And if you skip this, that’s okay! Our products still work just fine.", 'the-events-calendar' ),
+			'opted_in_plugins_text' => __( 'See which plugins you have opted in to tracking for', 'tribe-common' ),
+			'heading'               => __( 'We hope you love TEC Common!', 'tribe-common' ),
+			'intro'                 => __( "Hi, {$user_name}! This is an invitation to help our StellarWP community. If you opt-in, some data about your usage of TEC Common and future StellarWP Products will be shared with our teams (so they can work their butts off to improve). We will also share some helpful info on WordPress, and our products from time to time. And if you skip this, that’s okay! Our products still work just fine.", 'tribe-common' ),
 		];
+
+		$this->optin_args = apply_filters( 'tec_common_telemetry_optin_args', $optin_args );
 
 		return array_merge( $args, $this->optin_args );
 	}
