ba401159d1409d9dccad606e712ce6c1f0cfd14f
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-04-22 22:45:39 -0400

    Include a base level of testing for Integration Abstract and Traits

--- tests/wpunit/_bootstrap.php
+++ tests/wpunit/_bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+use Codeception\Util\Autoload;
 
 tests_add_filter( 'tribe_common_log_to_wpcli', '__return_false' );
+
+
+Autoload::addNamespace( '\\TEC\\Common\\Integrations\\', __DIR__ . '/_data/classes/Integrations' );
