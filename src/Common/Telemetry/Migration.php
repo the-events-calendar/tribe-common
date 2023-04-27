62b6a515b89ff3e7d525e87f51302bdddc25de75
Camwyn <stephen@theeventscalendar.com>
2023-04-27 14:58:16 -0400

    docblocks and such

--- src/Common/Telemetry/Migration.php
+++ src/Common/Telemetry/Migration.php
@@ -26,29 +26,52 @@ final class Migration {
 		'event-tickets/common/vendor/freemius'
 	];
 
-	public function should_load() {
+	/**
+	 * Determine if we are opted-in to Freemius
+	 *
+	 * @since TBD
+	 *
+	 * @return boolean
+	 */
+	public function is_opted_in(): bool {
+		$fs_active_plugins = get_option( 'fs_active_plugins' );
+
+		if ( empty( $fs_active_plugins ) ) {
+			return false;
+		}
+
+		foreach( $this->our_plugins as $plugin ) {
+			if ( isset( $fs_active_plugins->plugin[$plugin] ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Whether the class should load/run.
+	 *
+	 * @since TBD
+	 *
+	 * @return boolean
+	 */
+	public function should_load(): bool {
 		// If we're not opted in to Freemius, bail.
 		if ( ! $this->is_opted_in() ) {
 			return false;
 		}
 
-		// If we're opted in to Telemetry, bail.
-		$status = Telemetry::get_status_object();
-		if ( $status->get()  ) {
-			return false;
-		}
-	}
-
-	public function is_opted_in() {
-
+		/**
+		 * Allows filtering of whether the class should load/run.
+		 */
+		return apply_filters( 'tec_telemetry_migration_should_load', true );
 	}
 
 	/**
 	 * Detect if the user has opted in to Freemius and auto-opt them in to Telemetry.
 	 *
 	 * @since TBD
-	 *
-	 * @return void
 	 */
 	public function migrate_existing_opt_in(): void {
 		// Let's reduce the amount this triggers.
@@ -60,6 +83,10 @@ final class Migration {
 			return;
 		}
 
+		if ( ! $this->should_load() ) {
+			return;
+		}
+
 		$fs_active_plugins = get_option( 'fs_active_plugins' );
 
 		// Bail if empty.
@@ -67,8 +94,52 @@ final class Migration {
 			return;
 		}
 
-		// We're going to need these...
-		$fs_accounts      = get_option( 'fs_accounts' );
+		// Clean up our list.
+		$this->remove_inactive_plugins( $fs_active_plugins );
+
+		// Bail if none of our plugins are present.
+		if ( ! count( $this->our_plugins ) ) {
+			return;
+		}
+
+		/**
+		 * Allows plugins to hook in and perform actions (like display a notice) when
+		 * the user is automatically opted in to Telemetry.
+		 *
+		 * We also use this to trigger the actual auto-opt-in at the default priority.
+		 *
+		 * @since TBD
+		 */
+		do_action( 'tec_telemetry_auto_opt_in' );
+
+		$fs_accounts = get_option( 'fs_accounts' );
+
+		// Store these for later.
+		update_option( $this->fs_accounts_slug, $fs_accounts );
+
+		// If only our plugins are present, short-cut and delete everything.
+		if ( count( $this->our_plugins ) === count( $fs_active_plugins->plugins ) ) {
+			$this->remove_all_freemius();
+
+			return;
+		}
+
+		// Remove us from fs_active_plugins.
+		$this->handle_fs_active_plugins( $fs_active_plugins );
+
+		// Remove us from fs_accounts.
+		$this->handle_fs_accounts( $fs_accounts );
+
+	}
+
+	/**
+	 * Filters our list of plugins to only the ones Freemius shows as active
+	 *
+	 * @since TBD
+	 *
+	 * @param Object $fs_active_plugins The stored list of active plugins from Freemius.
+	 */
+	private function remove_inactive_plugins( $fs_active_plugins ): void {
 		$freemius_plugins = $fs_active_plugins->plugins;
 
 		foreach( $this->our_plugins as $plugin ) {
@@ -76,53 +147,51 @@ final class Migration {
 				unset( $this->our_plugins[$plugin] );
 			}
 		}
+	}
 
-		// Bail if none of our plugins are present.
-		if ( ! count( $this->our_plugins ) ) {
-			return;
-		}
-
-		// We opted in to Freemius, opt in to Telemetry.
-		$this->auto_opt_in();
-		// Allow for plugins to do something when automatically opting in. Like display a notice.
-		// We use this to trigger the actual auto-opt-in.
-		do_action( 'tec_telemetry_auto_opt_in' );
-
-		// Store these for later.
-		update_option( $this->fs_accounts_slug, $fs_accounts );
+	private function handle_fs_active_plugins( $fs_active_plugins ): void {
 		update_option( $this->fs_plugins_slug, $fs_active_plugins );
 
-		// If only our plugins are present, short-cut and delete everything.
-		if ( count( $this->our_plugins ) === count( $freemius_plugins ) ) {
-			$removed = true;
-			delete_option( 'fs_active_plugins' );
-			delete_option( 'fs_accounts' );
-
-			return;
-		}
-
-		// Remove us from fs_active_plugins.
-		if ( empty( $removed ) ) {
-			foreach ( $this->our_plugins as $plugin ) {
-				unset( $fs_active_plugins->plugins[$plugin] );
-
-				if ( ! empty( $fs_active_plugins->newest->sdk_path ) && $fs_active_plugins->newest->sdk_path === $plugin ) {
-					unset( $fs_active_plugins->newest );
-				}
-			}
-
-			// Update the option in the database with our edits.
-			update_option( 'fs_active_plugins', $fs_active_plugins );
-		}
-
-		// Remove us from fs_accounts.
 		foreach ( $this->our_plugins as $plugin ) {
-			$plugin = str_replace( '/common/vendor/freemius', '', $plugin );
+			unset( $fs_active_plugins->plugins[$plugin] );
+
+			if ( ! empty( $fs_active_plugins->newest->sdk_path ) && $fs_active_plugins->newest->sdk_path === $plugin ) {
+				unset( $fs_active_plugins->newest );
+			}
+		}
+
+		// Update the option in the database with our edits.
+		update_option( 'fs_active_plugins', $fs_active_plugins );
+	}
+
+	/**
+	 * Removes our plugins from the fs_accounts option.
+	 *
+	 * @since TBD
+	 *
+	 * @param [type] $fs_accounts
+	 */
+	private function handle_fs_accounts( $fs_accounts ): void {
+		foreach ( $this->our_plugins as $plugin ) {
 			$fs_accounts = $this->strip_plugin_from_fs_accounts( $plugin, $fs_accounts );
 		}
 
 		// Update the option in the database with our edits.
 		update_option( 'fs_accounts', $fs_accounts );
+	}
+
+	/**
+	 * Removes all freemius options from the database.
+	 * Only used if we were the only active plugin.
+	 *
+	 * @since TBD
+	 */
+	private function remove_all_freemius(): void {
+		delete_option( 'fs_active_plugins' );
+		delete_option( 'fs_accounts' );
+		delete_option( 'fs_api_cache' );
+		delete_option( 'fs_debug_mode' );
+		delete_option( 'fs_gdpr' );
 	}
 
 	/**
@@ -132,9 +201,12 @@ final class Migration {
 	 *
 	 * @param string $plugin
 	 * @param array<string,mixed> $fs_accounts
+	 *
 	 * @return array<string,mixed>
 	 */
 	private function strip_plugin_from_fs_accounts( $plugin, $fs_accounts ): array {
+		$plugin = str_replace( '/common/vendor/freemius', '', $plugin );
+
 		foreach( $fs_accounts[ 'id_slug_type_path_map' ] as $key => $data ) {
 			if ( $data['slug'] === $plugin ) {
 				unset( $fs_accounts[ 'id_slug_type_path_map' ][$key] );
@@ -170,7 +242,12 @@ final class Migration {
 		return $fs_accounts;
 	}
 
-	private function auto_opt_in() {
+	/**
+	 * Opts the user in to Telemetry.
+	 *
+	 * @since TBD
+	 */
+	public function auto_opt_in() {
 		$Opt_In_Subscriber = Config::get_container()->get( Opt_In_Subscriber::class );
 		$Opt_In_Subscriber->opt_in( Telemetry::get_stellar_slug() );
 	}
