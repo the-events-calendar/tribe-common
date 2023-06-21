c71c5ebe935279b2eb969a993bff4870a085499f
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-06-21 13:22:31 -0400

    Modify the container get method to throw a specific Expection that extends RunTimeException but that we own.

---
+++ src/Common/Exceptions/Not_Bound_Exception.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TEC\Common\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class Not_Bound_Exception.
+ *
+ * @since 5.1.1.2
+ *
+ * @package TEC\Common\Exceptions
+ */
+class Not_Bound_Exception extends Container_Exception {
+	// Intentionally empty.
+}
