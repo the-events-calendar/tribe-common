8abbf88a0a78741ade4ffe8c28a3c3c0d1cc2bb9
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-06-21 13:47:43 -0400

    Version bump to 5.1.1.2

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Tribe Common
 Description: An event settings framework for managing shared options
-Version: 5.1.1.1
+Version: 5.1.1.2
 Author: The Events Calendar
 Author URI: http://evnt.is/1x
 Text Domain: tribe-common
