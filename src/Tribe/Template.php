9df7e3862e7b1088c5b870efd3e6f84719f7564a
Luca Tumedei <luca@theaveragedev.com>
2023-08-09 14:50:11 +0200

    feat(Template) pass context to filters

--- src/Tribe/Template.php
+++ src/Tribe/Template.php
@@ -872,13 +872,15 @@ class Tribe__Template {
 		 * Allow users to filter the HTML before rendering
 		 *
 		 * @since  4.11.0
+		 * @since TBD Added the `$context` parameter; take charge of rendering the filtered HTML.
 		 *
-		 * @param string $html     The initial HTML
-		 * @param string $file     Complete path to include the PHP File
-		 * @param array  $name     Template name
-		 * @param self   $template Current instance of the Tribe__Template
+		 * @param string              $html     The initial HTML
+		 * @param string              $file     Complete path to include the PHP File
+		 * @param array               $name     Template name
+		 * @param self                $template Current instance of the Tribe__Template
+		 * @param array<string,mixed> $context  The context data passed to the template.
 		 */
-		$html = apply_filters( 'tribe_template_pre_html', null, $file, $name, $this );
+		$html = apply_filters( 'tribe_template_pre_html', null, $file, $name, $this, $context );
 
 		/**
 		 * Allow users to filter the HTML by the name before rendering
@@ -889,13 +891,15 @@ class Tribe__Template {
 		 *    `tribe_template_pre_html:tickets/login-to-purchase`
 		 *
 		 * @since  4.11.0
+		 * @since TBD Added the `$context` parameter; take charge of rendering the filtered HTML.
 		 *
-		 * @param string $html      The initial HTML
-		 * @param string $file      Complete path to include the PHP File
-		 * @param array  $name      Template name
-		 * @param self   $template  Current instance of the Tribe__Template
+		 * @param string              $html     The initial HTML
+		 * @param string              $file     Complete path to include the PHP File
+		 * @param array               $name     Template name
+		 * @param self                $template Current instance of the Tribe__Template
+		 * @param array<string,mixed> $context  The context data passed to the template.
 		 */
-		$html = apply_filters( "tribe_template_pre_html:{$hook_name}", $html, $file, $name, $this );
+		$html = apply_filters( "tribe_template_pre_html:{$hook_name}", $html, $file, $name, $this, $context );
 
 		if ( null === $html ) {
 			// Merges the local data passed to template to the global scope
