0444b73c82a1932089b69a894a8da1b19af98d1c
Stephen Page <stephen@theeventscalendar.com>
2023-10-30 11:20:45 -0400

    Try to fix the container undefined noise.

--- src/Common/Contracts/Service_Provider.php
+++ src/Common/Contracts/Service_Provider.php
@@ -4,6 +4,14 @@ namespace TEC\Common\Contracts;
 
 use TEC\Common\lucatume\DI52\ServiceProvider as DI52_Service_Provider;
 
+/**
+ * Class ServiceProvider
+ *
+ * @package TEC\Common\Contracts
+ *
+ * @property bool $deferred
+ * @property TEC\Common\lucatume\DI52\Container $container
+ */
 abstract class Service_Provider extends DI52_Service_Provider  {
     // Intentionally empty.
 }
