b2e966f2328781056d379d35f8eb3415fcab5f59
Edwin Daniels <stratease@gmail.com>
2023-12-06 15:08:13 -0800

    Fix tests

---
+++ tests/wpunit/Tribe/AjaxDropdownTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tribe;
+
+use Tribe__Ajax__Dropdown;
+
+class AjaxDropdownTest extends \Codeception\TestCase\WPTestCase {
+
+	/**
+	 * @test
+	 */
+	public function should_default_to_only_published_posts()
+	{
+		$post1 = $this->factory->post->create( [ 'post_content' => 'Event Content', 'post_status' => 'draft' ] );
+		$post2 = $this->factory->post->create( [ 'post_content' => 'Event Content', 'post_status' => 'publish' ] );
+
+		$search = [];
+
+		$dropdown = new Tribe__Ajax__Dropdown();
+		$data     = $dropdown->search_posts( $search, 1, [] );
+		$this->assertCount( 1, $data['posts'] );
+		$this->assertEquals( $post2, $data['posts'][0]['id'] );
+	}
+
+	/**
+	 * @dataProvider parse_params_data_provider
+	 * @test
+	 */
+	public function should_clean_query_args( $dirty_params, $expected_params )
+	{
+		$dropdown     = new Tribe__Ajax__Dropdown();
+		$clean_params = $dropdown->parse_params( $dirty_params );
+		$this->assertEquals( $expected_params, (array) $clean_params );
+	}
+
+	public function parse_params_data_provider()
+	{
+		return [
+			'dirty args'    => [
+				[
+					'cat'         => 'b',
+					'copy'        => 1,
+					'post_status' => 'draft',
+					'args'        => [ 'post_status' => 'draft' ],
+					'faux'        => null,
+				],
+				[
+					'search' => null,
+					'page'   => 0,
+					'source' => null,
+					'args'   => [ 'post_status' => 'publish' ],
+				]
+			],
+			'expected args' => [
+				[
+					'args' => [ 'taxonomy' => 'abc', 'post_type' => 'post' ],
+				],
+				[
+					'search' => null,
+					'page'   => 0,
+					'source' => null,
+					'args'   => [ 'post_status' => 'publish', 'taxonomy' => 'abc', 'post_type' => 'post' ],
+				]
+			]
+		];
+	}
+}
