8079c6a8bcf615ff8a887b7fa9dcf5de21323406
Mike Cotton <codingmusician@gmail.com>
2023-12-07 13:03:57 -0600

    version bump

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -23,7 +23,7 @@ class Tribe__Main {
 	const OPTIONNAME        = 'tribe_events_calendar_options';
 	const OPTIONNAMENETWORK = 'tribe_events_calendar_network_options';
 	const FEED_URL          = 'https://theeventscalendar.com/feed/';
-	const VERSION           = '5.1.15.2';
+	const VERSION           = '5.1.16';
 
 	protected $plugin_context;
 	protected $plugin_context_class;
