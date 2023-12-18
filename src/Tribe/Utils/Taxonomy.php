e810689579080cda45881ac75b2d048b6d92ffaa
Philipp Bammes <8144115+tyrann0us@users.noreply.github.com>
2023-12-18 13:03:17 +0100

    fix: don't prime term cache when no posts are found

--- src/Tribe/Utils/Taxonomy.php
+++ src/Tribe/Utils/Taxonomy.php
@@ -141,6 +141,10 @@ class Taxonomy {
 	 * @return array<int, array>
 	 */
 	public static function prime_term_cache( array $posts = [], array $taxonomies = [ 'post_tag', \Tribe__Events__Main::TAXONOMY ], bool $prime_term_meta = false ): array {
+                if ( count( $posts ) === 0 ) {
+			return [];
+                }
+
 		$first = reset( $posts );
 		$is_numeric = ( ! $first instanceof \WP_Post );
 		if ( $is_numeric ) {
