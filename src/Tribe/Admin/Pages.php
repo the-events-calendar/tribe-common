ff409c410e53638abc7847752fd3c7cd8d0f8adc
Juan Aldasoro <juanfraa@gmail.com>
2024-01-31 17:16:34 +0100

    Add position parameter to submenu pages.

--- src/Tribe/Admin/Pages.php
+++ src/Tribe/Admin/Pages.php
@@ -127,7 +127,8 @@ class Pages {
 				$options['title'],
 				$options['capability'],
 				$options['path'],
-				$options['callback']
+				$options['callback'],
+				$options['position']
 			);
 		}
 
