e1eef8a09d4ef5157b0d7f6af1bb0b1b9e3c75ff
Luca Tumedei <luca@theaveragedev.com>
2024-05-02 10:44:30 +0200

    Set version to 5.3.0-dev

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tribe Common
  * Description: An event settings framework for managing shared options.
- * Version: 5.2.5
+ * Version: 5.3.0-dev
  * Author: The Events Calendar
  * Author URI: http://evnt.is/1x
  * Text Domain: tribe-common
