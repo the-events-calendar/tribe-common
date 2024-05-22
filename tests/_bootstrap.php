ece5d2b207b630515e8a2c880c695f20ff9ec0c1
George Pattichis <gpattihis@gmail.com>
2024-05-22 20:40:28 +0300

    Event Automator Tests Merge

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,5 +5,7 @@ use Codeception\Util\Autoload;
 
 require_once dirname( __FILE__, 2 ) . '/tribe-autoload.php';
 Autoload::addNamespace( 'Tribe\\Tests', __DIR__ . '/_support' );
+Autoload::addNamespace( 'TEC\\Event_Automator\\Tests', __DIR__ . '/_support' );
 // Silence the logger in the tests.
 $_ENV['TEC_DISABLE_LOGGING'] = 1;
+
