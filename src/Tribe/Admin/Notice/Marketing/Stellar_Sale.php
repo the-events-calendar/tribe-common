1f09d58da0922828321f4d94852a9a877ffcfd3c
Mike Cotton <codingmusician@gmail.com>
2024-06-07 09:56:56 -0500

    update url and remove todo

--- src/Tribe/Admin/Notice/Marketing/Stellar_Sale.php
+++ src/Tribe/Admin/Notice/Marketing/Stellar_Sale.php
@@ -51,9 +51,8 @@ class Stellar_Sale extends \Tribe\Admin\Notice\Date_Based {
 		\Tribe__Assets::instance()->enqueue( [ 'tribe-common-admin' ] );
 
 		// Used in the template.
-		// @todo @codingmusician - Update the URLs when they are available.
-		$cta_url     = 'https://evnt.is/1bcv';
-		$stellar_url = 'https://evnt.is/1bcu';
+		$cta_url     = 'https://evnt.is/1bdv';
+		$stellar_url = 'https://evnt.is/1bdw';
 
 		$template_args = [
 			'heading'           => __( 'Make it yours.', 'tribe-common' ),
