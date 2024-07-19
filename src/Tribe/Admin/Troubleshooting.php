b8dab480bf67f873479da1adfd3c71388cc0f3ce
Andras Guseo <andras@theeventscalendar.com>
2024-07-19 16:28:11 +0200

    Fix more info URL for timezone

--- src/Tribe/Admin/Troubleshooting.php
+++ src/Tribe/Admin/Troubleshooting.php
@@ -126,29 +126,6 @@ class Troubleshooting {
 	}
 
 	/**
-	 * Adds the troubleshooting menu to the the WP admin bar under events.
-	 *
-	 * @since 4.14.2
-	 *
-	 */
-	public function add_toolbar_item() {
-		$capability = $this->get_required_capability();
-
-		if ( ! current_user_can( $capability ) ) {
-			return;
-		}
-
-		global $wp_admin_bar;
-
-		$wp_admin_bar->add_menu( [
-			'id'     => 'tec-troubleshooting',
-			'title'  => esc_html__( 'Troubleshooting', 'tribe-common' ),
-			'href'   => Tribe__Settings::instance()->get_url( [ 'page' => static::MENU_SLUG ] ),
-			'parent' => 'tribe-events-settings-group',
-		] );
-	}
-
-	/**
 	 * Checks if the current page is the troubleshooting page.
 	 *
 	 * @since 4.14.2
@@ -181,6 +158,29 @@ class Troubleshooting {
 	}
 
 	/**
+	 * Adds the troubleshooting menu to the the WP admin bar under events.
+	 *
+	 * @since 4.14.2
+	 *
+	 */
+	public function add_toolbar_item() {
+		$capability = $this->get_required_capability();
+
+		if ( ! current_user_can( $capability ) ) {
+			return;
+		}
+
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_menu( [
+			'id'     => 'tec-troubleshooting',
+			'title'  => esc_html__( 'Troubleshooting', 'tribe-common' ),
+			'href'   => Tribe__Settings::instance()->get_url( [ 'page' => static::MENU_SLUG ] ),
+			'parent' => 'tribe-events-settings-group',
+		] );
+	}
+
+	/**
 	 * Renders the Troubleshooting page.
 	 *
 	 * @since 4.14.2
@@ -204,6 +204,101 @@ class Troubleshooting {
 		$active_issues = wp_list_pluck( $issues, 'active' );
 
 		return in_array( true, $active_issues );
+	}
+
+	/**
+	 * Displays issues found in the UI.
+	 *
+	 * @since 4.14.2
+	 *
+	 * @return array of issues which are displayed on the troubleshooting page.
+	 */
+	public function get_issues_found() {
+		$issues_found = apply_filters( 'tec_help_troubleshooting_issues_found', [
+			[
+				'title'        => __( 'Site time zone uses UTC', 'tribe-common' ),
+				'description'  => __( 'When using The Events Calendar, we highly recommend that you use a geographic timezone such as "America/Los_Angeles" and avoid using a UTC timezone offset such as “UTC+9”. Choosing a UTC timezone for your site or individual events may cause problems when importing events or with Daylight Saving Time. Go to your the General WordPress settings to adjust your site timezone.', 'tribe-common' ),
+				'more_info'    => 'http://evnt.is/1ad3',
+				'resolve_text' => __( 'Adjust your timezone', 'tribe-common' ),
+				'fix'          => '/wp-admin/options-general.php',
+				'active'       => $this->is_active_issue( 'timezone' ),
+			],
+			[
+				'title'        => __( 'Install max has been reached', 'tribe-common' ),
+				'description'  => __( 'License keys can only be used on a limited number of sites, which varies depending on your license level. You\'ll need to remove the license from one or more other site\'s in order to use it on this one.', 'tribe-common' ),
+				'more_info'    => 'https://evnt.is/1aqz',
+				'resolve_text' => __( 'Manage your licenses', 'tribe-common' ),
+				'fix'          => 'https://evnt.is/1aq-',
+				'active'       => $this->is_active_issue( 'install-max' ),
+			],
+			[
+				'title'        => __( 'Default Google Maps API key', 'tribe-common' ),
+				'description'  => __( 'The Events Calendar comes with an API key for basic maps functionality. If you’d like to use more advanced features like custom map pins, dynamic map loads, or Events Calendar Pro\'s Location Search and advanced Map View, you’ll need to get your own Google Maps API key and add it to Events > Settings > Integrations', 'tribe-common' ),
+				'more_info'    => 'https://evnt.is/1aqx',
+				'resolve_text' => __( 'Enter a custom API key', 'tribe-common' ),
+				'fix'          => '/wp-admin/edit.php?page=tec-events-settings&tab=addons&post_type=tribe_events#tribe-field-google_maps_js_api_key',
+				'active'       => $this->is_active_issue( 'geolocation' ),
+			],
+			[
+				'title'        => __( 'Plugin(s) are out of date', 'tribe-common' ),
+				'description'  => __( 'It\'s important to use the most recent versions of our plugins so that you have access to the latest features, bug fixes, and security updates. Plugin functionality can be comprimised if your site is running outdated or mis-matched versions.', 'tribe-common' ),
+				'more_info'    => 'https://evnt.is/1aqy',
+				'resolve_text' => __( 'Check for updates', 'tribe-common' ),
+				'fix'          => '/wp-admin/update-core.php',
+				'active'       => $this->is_active_issue( 'out-of-date' ),
+			],
+			[
+				'title'        => __( 'PHP version out of date', 'tribe-common' ),
+				'description'  => __( 'The PHP version your site uses has reached its end of life of life on November 28, 2022. This means it no longer receives security updates or bug fixes. Users are encouraged to upgrade to newer versions of PHP to ensure continued support and security. Reach out to your hosting provider for assistance.', 'tribe-common' ),
+				'more_info'    => 'https://evnt.is/tec-php-support',
+				'resolve_text' => false,
+				'fix'          => false,
+				'active'       => $this->is_active_issue( 'php-version' ),
+			],
+			[
+				'title'        => __( 'Default PHP timezone is other than UTC', 'tribe-common' ),
+				'description'  => __( 'The default PHP timezone of your server is set to something else than UTC. This can cause issues when displaying event times and time zones. We recommend you to set the default PHP time zone on your server to UTC. This is usually done with the `date.timezone` directive in the php.ini file.', 'tribe-common' ),
+				'more_info'    => 'https://evnt.is/1aql',
+				'resolve_text' => false,
+				'fix'          => false,
+				'active'       => $this->is_active_issue( 'php-timezone' ),
+			],
+		] );
+
+		return $issues_found;
+	}
+
+	/**
+	 * Checks if any of the issues defined are active.
+	 *
+	 * @since 4.14.2
+	 *
+	 * @param string $slug the slug of active issue.
+	 *
+	 * @return boolean returns a boolean value for each individual issue depending on whether it is active or not.
+	 */
+	public function is_active_issue( $slug ) {
+		if ( 'timezone' === $slug ) {
+			return Timezones::is_utc_offset( Timezones::wp_timezone_string() );
+		}
+		if ( 'geolocation' === $slug && class_exists( 'Tribe__Events__Google__Maps_API_Key' ) ) {
+			$key = \tribe_get_option( 'google_maps_js_api_key', false );
+
+			return empty( $key ) || Tribe__Events__Google__Maps_API_Key::$default_api_key === $key;
+		}
+		if ( 'out-of-date' === $slug ) {
+			return $this->is_any_tec_plugin_out_of_date();
+		}
+		if ( 'php-version' === $slug ) {
+			return version_compare( PHP_VERSION, '8.0.0', '<' );
+		}
+		if ( 'php-timezone' === $slug ) {
+			$php_timezone = date_default_timezone_get();
+
+			return $php_timezone != "UTC";
+		}
+
+		return false;
 	}
 
 	/**
@@ -266,100 +361,6 @@ class Troubleshooting {
 		}
 
 		return false;
-	}
-
-	/**
-	 * Checks if any of the issues defined are active.
-	 *
-	 * @since 4.14.2
-	 *
-	 * @param string $slug the slug of active issue.
-	 *
-	 * @return boolean returns a boolean value for each individual issue depending on whether it is active or not.
-	 */
-	public function is_active_issue( $slug ) {
-		if ( 'timezone' === $slug ) {
-			return Timezones::is_utc_offset( Timezones::wp_timezone_string() );
-		}
-		if ( 'geolocation' === $slug && class_exists( 'Tribe__Events__Google__Maps_API_Key' ) ) {
-			$key = \tribe_get_option( 'google_maps_js_api_key', false );
-
-			return empty( $key ) || Tribe__Events__Google__Maps_API_Key::$default_api_key === $key;
-		}
-		if ( 'out-of-date' === $slug ) {
-			return $this->is_any_tec_plugin_out_of_date();
-		}
-		if ( 'php-version' === $slug ) {
-			return version_compare( PHP_VERSION, '8.0.0', '<' );
-		}
-		if ( 'php-timezone' === $slug ) {
-			$php_timezone = date_default_timezone_get();
-			return $php_timezone != "UTC";
-		}
-
-		return false;
-	}
-
-	/**
-	 * Displays issues found in the UI.
-	 *
-	 * @since 4.14.2
-	 *
-	 * @return array of issues which are displayed on the troubleshooting page.
-	 */
-	public function get_issues_found() {
-		$issues_found = apply_filters( 'tec_help_troubleshooting_issues_found', [
-			[
-				'title'        => __( 'Site time zone uses UTC', 'tribe-common' ),
-				'description'  => __( 'When using The Events Calendar, we highly recommend that you use a geographic timezone such as "America/Los_Angeles" and avoid using a UTC timezone offset such as “UTC+9”. Choosing a UTC timezone for your site or individual events may cause problems when importing events or with Daylight Saving Time. Go to your the General WordPress settings to adjust your site timezone.', 'tribe-common' ),
-				'more_info'    => 'http://evnt.is/1ad3',
-				'resolve_text' => __( 'Adjust your timezone', 'tribe-common' ),
-				'fix'          => '/wp-admin/options-general.php',
-				'active'       => $this->is_active_issue( 'timezone' ),
-			],
-			[
-				'title'        => __( 'Install max has been reached', 'tribe-common' ),
-				'description'  => __( 'License keys can only be used on a limited number of sites, which varies depending on your license level. You\'ll need to remove the license from one or more other site\'s in order to use it on this one.', 'tribe-common' ),
-				'more_info'    => 'https://evnt.is/1aqz',
-				'resolve_text' => __( 'Manage your licenses', 'tribe-common' ),
-				'fix'          => 'https://evnt.is/1aq-',
-				'active'       => $this->is_active_issue( 'install-max' ),
-			],
-			[
-				'title'        => __( 'Default Google Maps API key', 'tribe-common' ),
-				'description'  => __( 'The Events Calendar comes with an API key for basic maps functionality. If you’d like to use more advanced features like custom map pins, dynamic map loads, or Events Calendar Pro\'s Location Search and advanced Map View, you’ll need to get your own Google Maps API key and add it to Events > Settings > Integrations', 'tribe-common' ),
-				'more_info'    => 'https://evnt.is/1aqx',
-				'resolve_text' => __( 'Enter a custom API key', 'tribe-common' ),
-				'fix'          => '/wp-admin/edit.php?page=tec-events-settings&tab=addons&post_type=tribe_events#tribe-field-google_maps_js_api_key',
-				'active'       => $this->is_active_issue( 'geolocation' ),
-			],
-			[
-				'title'        => __( 'Plugin(s) are out of date', 'tribe-common' ),
-				'description'  => __( 'It\'s important to use the most recent versions of our plugins so that you have access to the latest features, bug fixes, and security updates. Plugin functionality can be comprimised if your site is running outdated or mis-matched versions.', 'tribe-common' ),
-				'more_info'    => 'https://evnt.is/1aqy',
-				'resolve_text' => __( 'Check for updates', 'tribe-common' ),
-				'fix'          => '/wp-admin/update-core.php',
-				'active'       => $this->is_active_issue( 'out-of-date' ),
-			],
-			[
-				'title'       => __( 'PHP version out of date', 'tribe-common' ),
-				'description' => __( 'The PHP version your site uses has reached its end of life of life on November 28, 2022. This means it no longer receives security updates or bug fixes. Users are encouraged to upgrade to newer versions of PHP to ensure continued support and security. Reach out to your hosting provider for assistance.', 'tribe-common' ),
-				'more_info'   => 'https://evnt.is/tec-php-support',
-				'resolve_text' => false,
-				'fix'          => false,
-				'active'      => $this->is_active_issue( 'php-version' ),
-			],
-			[
-				'title'       => __( 'Default PHP timezone is other than UTC', 'tribe-common' ),
-				'description' => __( 'The default PHP timezone of your server is set to something else than UTC. This can cause issues when displaying event times and time zones. We recommend you to set the default PHP time zone on your server to UTC. This is usually done with the `date.timezone` directive in the php.ini file.', 'tribe-common' ),
-				'more_info'   => 'https://evnt.is/tec-php-support',
-				'resolve_text' => false,
-				'fix'          => false,
-				'active'      => $this->is_active_issue( 'php-timezone' ),
-			],
-		] );
-
-		return $issues_found;
 	}
 
 	/**
