d7f6556c48bffd4723b96950794036de75dadbad
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-12 18:55:38 -0500

    Create Settings_Section class

---
+++ src/Common/Admin/Settings_Section.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ */
+
+declare( strict_types=1 );
+
+namespace TEC\Common\Admin;
+
+use TEC\Common\Admin\Entities\Element;
+
+/**
+ * Class Settings_Section
+ *
+ * @since TBD
+ */
+class Settings_Section extends Section {
+
+	/**
+	 * Elements for the section.
+	 *
+	 * @var Element[]
+	 */
+	protected array $elements = [];
+
+	/**
+	 * Add an element to the section.
+	 *
+	 * @param Element $element The element to add.
+	 *
+	 * @return static
+	 */
+	public function add_element( Element $element ) {
+		$this->elements[] = $element;
+
+		return $this;
+	}
+
+	/**
+	 * Add multiple elements to the section.
+	 *
+	 * @param Element[] $elements The elements to add.
+	 *
+	 * @return static
+	 */
+	public function add_elements( array $elements ) {
+		foreach ( $elements as $element ) {
+			$this->add_element( $element );
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Render the section content.
+	 *
+	 * @return void
+	 */
+	public function render() {
+		?>
+		<div class="tribe-settings-section">
+			<?php $this->render_title(); ?>
+			<?php $this->render_elements(); ?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render the elements for the section.
+	 *
+	 * @return void
+	 */
+	protected function render_elements() {
+		foreach ( $this->elements as $element ) {
+			$element->render();
+		}
+	}
+}
