49316067b441f3726ef4559d51e044becce32fd5
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-16 13:03:51 -0500

    Rename variable to not be "object"

--- src/Common/Admin/Entities/Validate_Elements.php
+++ src/Common/Admin/Entities/Validate_Elements.php
@@ -35,14 +35,14 @@ trait Validate_Elements {
 	/**
 	 * Validate that an object is an instance of a class.
 	 *
-	 * @param object $object    The object to validate.
+	 * @param object $thing     The object to validate.
 	 * @param string $classname The class name to validate against.
 	 *
 	 * @return void
 	 * @throws InvalidArgumentException If the object is not an instance of the class.
 	 */
-	protected function validate_instanceof( $object, $classname ) {
-		if ( ! $object instanceof $classname ) {
+	protected function validate_instanceof( $thing, $classname ) {
+		if ( ! $thing instanceof $classname ) {
 			throw new InvalidArgumentException( esc_html__( 'Invalid class instance.', 'tribe-common' ) );
 		}
 	}
