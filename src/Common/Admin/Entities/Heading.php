71e42c1a4b6845478a45ddd58b393ea2b23716e3
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-16 12:31:43 -0500

    Add an explanation for lack of escaping in the Heading element

--- src/Common/Admin/Entities/Heading.php
+++ src/Common/Admin/Entities/Heading.php
@@ -58,9 +58,18 @@ class Heading extends Base_Entity {
 	 * @return void
 	 */
 	public function render() {
+		/*
+		 * The heading level is dynamic and should not be escaped. The reasons that the heading level is
+		 * not escaped are as follows:
+		 *
+		 * 1. The heading level is type-cast as an integer in the constructor. Non-integers will generate an error.
+		 * 2. The heading level is validated in the constructor. The validation ensures that the heading level is
+		 *    within the range of 1-6.
+		 * 3. The tag_escape() function (which should be used for escaping HTML tags) operates on strings, not integers.
+		 */
 		printf(
 			'<h%1$d class="%2$s">%3$s</h%1$d>',
-			$this->level, // phpcs:ignore StellarWP.XSS.EscapeOutput
+			$this->level, // phpcs:ignore StellarWP.XSS.EscapeOutput,WordPress.Security.EscapeOutput.OutputNotEscaped
 			esc_attr( $this->get_classes() ),
 			esc_html( $this->content )
 		);
