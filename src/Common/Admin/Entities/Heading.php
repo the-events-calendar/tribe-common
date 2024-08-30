6e9ebb41c1d0a72fe7d34ba8600369ba166e0d48
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-30 12:41:56 -0500

    Update how the Heading element is rendered

--- src/Common/Admin/Entities/Heading.php
+++ src/Common/Admin/Entities/Heading.php
@@ -21,8 +21,6 @@ use Tribe\Utils\Element_Classes as Classes;
  */
 class Heading extends Base_Entity {
 
-	use Validate_Elements;
-
 	/**
 	 * The heading content.
 	 *
@@ -73,22 +71,14 @@ class Heading extends Base_Entity {
 	 * @return void
 	 */
 	public function render() {
-		/*
-		 * The heading level is dynamic and should not be escaped. The reasons that the heading level is
-		 * not escaped are as follows:
-		 *
-		 * 1. The heading level is type-cast as an integer in the constructor. Non-integers will generate an error.
-		 * 2. The heading level is validated in the constructor. The validation ensures that the heading level is
-		 *    within the range of 1-6.
-		 * 3. The tag_escape() function (which should be used for escaping HTML tags) operates on strings, not integers.
-		 */
-		printf(
-			'<h%1$d class="%2$s" %3$s>%4$s</h%1$d>',
-			$this->level, // phpcs:ignore StellarWP.XSS.EscapeOutput,WordPress.Security.EscapeOutput.OutputNotEscaped
-			esc_attr( $this->get_classes() ),
-			$this->get_attributes(), // phpcs:ignore StellarWP.XSS.EscapeOutput,WordPress.Security.EscapeOutput
-			esc_html( $this->content )
-		);
+		?>
+		<h<?php echo absint( $this->level ); ?>
+			<?php echo $this->get_class_attribute(); // phpcs:ignore StellarWP.XSS.EscapeOutput,WordPress.Security.EscapeOutput ?>
+			<?php echo $this->get_attributes(); // phpcs:ignore StellarWP.XSS.EscapeOutput,WordPress.Security.EscapeOutput ?>
+		>
+			<?php echo esc_html( $this->content ); ?>
+		</h<?php echo absint( $this->level ); ?>>
+		<?php
 	}
 
 	/**
