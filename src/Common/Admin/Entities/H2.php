234b66b36bd732d2c5947fccccc0228183ee5e41
Jeremy Pry <jeremy.pry@gmail.com>
2024-08-30 12:42:10 -0500

    Add H2 and H3 entities

---
+++ src/Common/Admin/Entities/H2.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * H2 Entity
+ *
+ * @since TBD
+ */
+
+declare( strict_types=1 );
+
+namespace TEC\Common\Admin\Entities;
+
+use Tribe\Utils\Element_Attributes as Attributes;
+use Tribe\Utils\Element_Classes as Classes;
+
+/**
+ * Class H2
+ *
+ * @since TBD
+ */
+class H2 extends Heading {
+
+	/**
+	 * H2 constructor.
+	 *
+	 * @param string      $content    The content for the heading.
+	 * @param ?Classes    $classes    The classes for the heading.
+	 * @param ?Attributes $attributes The attributes for the heading.
+	 */
+	public function __construct( string $content, ?Classes $classes = null, ?Attributes $attributes = null ) {
+		parent::__construct( $content, 2, $classes, $attributes );
+	}
+}
