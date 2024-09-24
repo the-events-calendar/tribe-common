34a4b169c6f9d40556cdbe6acb5011af15d577a2
Eddie <stratease@gmail.com>
2024-09-24 18:26:14 +0100

    Fixed html, fixed tabbing

--- src/admin-views/help-hub.php
+++ src/admin-views/help-hub.php
@@ -14,12 +14,12 @@
 	<nav class="tec-nav__wrapper tec-settings__nav-wrapper tec-nav__wrapper--subnav-active">
 		<ul class="tec-nav">
 
-			<li class="tec-nav__tab tec-nav__tab--general tec-nav__tab--has-subnav tec-nav__tab--subnav-active">
-				<a id="support-hub" class="tec-nav__link">Support Hub</a>
+			<li data-tab-target="tec-help-tab" class="tec-nav__tab tec-nav__tab--general tec-nav__tab--has-subnav tec-nav__tab--subnav-active">
+				<a class="tec-nav__link">Support Hub</a>
 
 			</li>
-			<li class="tec-nav__tab tec-nav__tab--general tec-nav__tab--has-subnav ">
-				<a id="resources" class="tec-nav__link" >Resources</a>
+			<li data-tab-target="tec-resources-tab" class="tec-nav__tab tec-nav__tab--general tec-nav__tab--has-subnav ">
+				<a class="tec-nav__link" >Resources</a>
 
 			</li>
 		</ul>
@@ -27,17 +27,54 @@
 
 
 
-	<div class="tribe-settings-form form">
+	<div id="tec-help-tab" class="tribe-settings-form form tec-tab-container">
 		<form id="tec-settings-form" class="tec-settings-form__display-calendar-tab-tab--active tec-settings-form__subnav-active" method="post">
 			<div class="tec-settings-form__header-block tec-settings-form__header-block--horizontal">
-				<h2 class="tec-settings-form__section-header">Support Hub</h2>
+				<h2 class="tec-settings-form__section-header">Resources</h2>
 				<p class="tec-settings-form__section-description">
-					todo
+					todo 1
 				</p>
 			</div>
 			<div class="tec-settings-form__content-section">
 				<h3 class="tec-settings-form__section-header tec-settings-form__section-header--sub">todo</h3>
+			</div>
+		</form>
+	</div>
 
-		</form>			</div>
+	<div id="tec-resources-tab" class="tribe-settings-form form tec-tab-container">
+		<form id="tec-settings-form" class="tec-settings-form__display-calendar-tab-tab--active tec-settings-form__subnav-active" method="post">
+			<div class="tec-settings-form__header-block tec-settings-form__header-block--horizontal">
+				<h2 class="tec-settings-form__section-header">Support Hub</h2>
+				<p class="tec-settings-form__section-description">
+					todo 2
+				</p>
+			</div>
+			<div class="tec-settings-form__content-section">
+				<h3 class="tec-settings-form__section-header tec-settings-form__section-header--sub">todo</h3>
+			</div>
+		</form>
+	</div>
 </div>
+
+
+<script>
+	jQuery( document ).ready( function($) {
+		let currentTab = $( 'li.tec-nav__tab.tec-nav__tab--subnav-active' );
+		let tabContainer = $( '#' + currentTab.data( 'tab-target' ) );
+		$( '.tec-tab-container' ).hide();
+		tabContainer.show();
+
+		$( 'li.tec-nav__tab' ).on( 'click', function() {
+			let tab = $( this );
+			let tabTarget = $( '#' + tab.data( 'tab-target' ) );
+
+			$( 'li.tec-nav__tab' ).removeClass( 'tec-nav__tab--subnav-active' );
+			tab.addClass( 'tec-nav__tab--subnav-active' );
+
+			tabContainer.hide();
+			tabTarget.show();
+			tabContainer = tabTarget;
+		} );
+	} );
+</script>
 
