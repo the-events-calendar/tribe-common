4201ac594d330d860b03e046a1181c8e6c950aae
Gustavo Bordoni <bordoni.dev@gmail.com>
2024-10-11 02:56:51 -0400

    Tabs, PHPStorm unlearned how to auto convert now...

--- src/admin-views/conditional_content/black-friday.php
+++ src/admin-views/conditional_content/black-friday.php
@@ -13,15 +13,15 @@ $year = date_i18n( 'Y' );
 ?>
 
 <a
-    href="<?php echo esc_url( $link ); ?>"
-    target="_blank"
-    rel="noopener nofollow"
-    style="display: block; margin: 25px 0; <?php echo $this->get( 'is_narrow' ) ? 'max-width: 1000px' : ''; ?>"
-    title="<?php echo sprintf( esc_attr_x( '%1$s Black Friday Sale for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Black Friday Ad', 'tribe-common' ), $year ); ?>"
+	href="<?php echo esc_url( $link ); ?>"
+	target="_blank"
+	rel="noopener nofollow"
+	style="display: block; margin: 25px 0; <?php echo $this->get( 'is_narrow' ) ? 'max-width: 1000px' : ''; ?>"
+	title="<?php echo sprintf( esc_attr_x( '%1$s Black Friday Sale for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Black Friday Ad', 'tribe-common' ), $year ); ?>"
 >
 	<img
-        style="display: block; width: 100%; height: auto;" <?php // This is intentionally inline, don't add classes here. ?>
-        src="<?php echo esc_url( $image_src ); ?>"
-        alt="<?php echo sprintf( esc_attr_x( '%1$s Black Friday Sale for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Black Friday Ad', 'tribe-common' ), $year ); ?>"
-    />
+		style="display: block; width: 100%; height: auto;" <?php // This is intentionally inline, don't add classes here. ?>
+		src="<?php echo esc_url( $image_src ); ?>"
+		alt="<?php echo sprintf( esc_attr_x( '%1$s Black Friday Sale for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Black Friday Ad', 'tribe-common' ), $year ); ?>"
+	/>
 </a>
