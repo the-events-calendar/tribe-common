6bef39f17ec6f4aefd8bfa85585acd237fac8ef3
Brian Krane <redscar@gmail.com>
2024-10-24 15:18:35 -0400

    Updated doc file comments.

--- src/admin-views/help-hub/shared-live-support.php
+++ src/admin-views/help-hub/shared-live-support.php
@@ -1,10 +1,18 @@
 <?php
+
 /**
  * The template that displays the support hub sidebar.
  *
- * @var Tribe__Main $main             The main common object.
- * @var bool        $is_opted_in      Whether the user has opted in to telemetry.
- * @var bool        $is_license_valid Whether the user has any valid licenses.
+ * @var Tribe__Main $main              The main common object.
+ * @var bool        $is_opted_in       Whether the user has opted in to telemetry.
+ * @var bool        $is_license_valid  Whether the user has any valid licenses.
+ * @var string      $zendesk_chat_key  The API key for the Zendesk chat integration.
+ * @var string      $docblock_chat_key The API key for the DocsBot chat integration.
+ * @var string      $opt_in_link       The link to the telemetry opt-in page.
+ * @var string      $notice            The admin notice HTML for the chatbot callout.
+ * @var string      $template_variant  The template variant, determining which template to display.
+ * @var string      $stars_icon_url    The URL for the stars icon image.
+ * @var string      $chat_icon_url     The URL for the chat bubble icon image.
  */
 
 $stars_icon_url = tribe_resource_url( 'images/icons/stars.svg', false, null, $main );
