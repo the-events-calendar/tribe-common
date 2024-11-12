340c01e939ade56edd6a5cf1a793cee8deecc91c
Camwyn <stephen@theeventscalendar.com>
2024-11-12 15:33:25 -0500

    Fix old TBDs

--- src/Tribe/Settings.php
+++ src/Tribe/Settings.php
@@ -304,7 +304,7 @@ class Tribe__Settings {
 	/**
 	 * Magic getter for deprecated properties.
 	 *
-	 * @since TBD
+	 * @since 6.3.1
 	 *
 	 * @param string $name The property name we are looking for.
 	 *
