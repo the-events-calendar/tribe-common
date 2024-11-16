d107085146380d0a32749eda0f9c1030bd07b2af
Gustavo Bordoni <bordoni.dev@gmail.com>
2024-11-16 01:07:41 -0500

    Re-introduce auto-size fields fix most of the CSS for date picker

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -3,6 +3,7 @@
  */
 import classnames from 'classnames';
 import propTypes from 'prop-types';
+import reactInputAutosize from 'react-18-input-autosize';
 import * as reactDayPicker from 'react-day-picker';
 import * as reactRedux from 'react-redux';
 import * as redux from 'redux';
@@ -12,6 +13,7 @@ import * as reselect from 'reselect';
 export {
 	classnames,
 	propTypes,
+	reactInputAutosize,
 	reactDayPicker,
 	reactRedux,
 	redux,
