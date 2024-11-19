eaa8970af6c09fc1d8020cdaefedc9b2e7ae2b8e
jesseeproductions <info@jesseeproductions.com>
2024-11-19 11:00:44 -0500

    EVA - Resolve issues with Faker being moved to vendor-prefixed

--- tests/_support/Traits/Create_Attendees.php
+++ tests/_support/Traits/Create_Attendees.php
@@ -18,6 +18,7 @@ use Tribe\Tickets_Plus\Test\Commerce\EDD\Order_Maker as EDD_Order_Maker;
 use Tribe\Tickets_Plus\Test\Commerce\WooCommerce\Ticket_Maker as Woo_Ticket_Maker;
 use Tribe\Tickets_Plus\Test\Commerce\WooCommerce\Order_Maker as Woo_Order_Maker;
 use Tribe\Tickets\Test\Commerce\Attendee_Maker as Attendee_Maker;
+use TEC\Common\Faker;
 use EDD_Payment;
 use EDD_Download;
 use WC_Order_Refund;
@@ -705,8 +706,8 @@ trait Create_Attendees {
 		$this->suspend_edd_emails();
 
 		$factory = $this->factory ?? $this->factory();
-		$faker   = \Faker\Factory::create();
-		$faker->addProvider( new \Faker\Provider\en_US\Address( $faker ) );
+		$faker   = Faker\Factory::create();
+		$faker->addProvider( new Faker\Provider\en_US\Address( $faker ) );
 
 		$create_user = $overrides['create_users'] ?? false;
 		$user_id     = $overrides['user_id'] ?? 0;
