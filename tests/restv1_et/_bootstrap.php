86e58579759fcd1aa60f4c4fbef13e833cd0fded
Dimitrios Pantazis <dpanta94@gmail.com>
2024-12-11 18:45:58 +0200

    Attempt to fix EVA tests

--- tests/restv1_et/_bootstrap.php
+++ tests/restv1_et/_bootstrap.php
@@ -1,1 +1,8 @@
 <?php
+
+use TEC\Tickets\Commerce\Provider as Commerce_Provider;
+
+putenv( 'TEC_TICKETS_COMMERCE=1' );
+putenv( 'TEC_DISABLE_LOGGING=1' );
+
+tribe_register_provider( Commerce_Provider::class );
