353537a33eb4c40d7c830d1f9d976a08d9a415c1
Luca Tumedei <luca@theaveragedev.com>
2024-12-30 13:04:33 +0100

    refactor(IAN) narrower handlers, namespace
    This updates the `data` attribute to use a less generic name and
    modifies the `click` handler logic to target IAN elements only.
    Additionally, ensure elements exist in the current context before
    targeting them.

--- src/admin-views/notifications/notification.php
+++ src/admin-views/notifications/notification.php
@@ -17,7 +17,7 @@
 ?>
 <div class="ian-sidebar__notification ian-sidebar__notification--<?php echo esc_attr( $type ); ?>" id="notification_<?php echo esc_attr( $id ); ?>" data-nonce="<?php echo esc_attr( wp_create_nonce( 'ian_nonce_' . $id ) ); ?>">
 	<?php if ( $dismissible ) : ?>
-	<div class="dashicons dashicons-dismiss" data-trigger="dismissIan" data-id="<?php echo esc_attr( $id ); ?>" data-slug="<?php echo esc_attr( $slug ); ?>" title="<?php esc_attr_e( 'Dismiss', 'tribe-common' ); ?>"></div>
+	<div class="dashicons dashicons-dismiss" data-tec-ian-trigger="dismissIan" data-id="<?php echo esc_attr( $id ); ?>" data-slug="<?php echo esc_attr( $slug ); ?>" title="<?php esc_attr_e( 'Dismiss', 'tribe-common' ); ?>"></div>
 	<?php endif; ?>
 	<div class="ian-sidebar__notification-title"><?php echo esc_html( $title ); ?></div>
 	<div class="ian-sidebar__notification-content"><?php echo wp_kses_post( $html ); ?></div>
@@ -30,7 +30,7 @@
 		</div>
 		<div class="ian-sidebar__notification-link--right">
 			<?php if ( ! $read ) : ?>
-				<a href="#" data-trigger="readIan" data-id="<?php echo esc_attr( $id ); ?>" data-slug="<?php echo esc_attr( $slug ); ?>"><?php esc_html_e( 'Mark as read', 'tribe-common' ); ?></a>
+				<a href="#" data-tec-ian-trigger="readIan" data-id="<?php echo esc_attr( $id ); ?>" data-slug="<?php echo esc_attr( $slug ); ?>"><?php esc_html_e( 'Mark as read', 'tribe-common' ); ?></a>
 			<?php endif; ?>
 		</div>
 	</div>
