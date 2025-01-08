4d9d9068805a58580c2f5a2d674a9c6b62c93721
Dimitrios Pantazis <dpanta94@gmail.com>
2025-01-08 13:35:16 +0200

    disable plugin,theme and core update checks

--- tests/end2end/_bootstrap.php
+++ tests/end2end/_bootstrap.php
@@ -1,1 +1,24 @@
 <?php
+
+add_action( 'plugins_loaded', function () {
+	remove_action( 'admin_init', '_maybe_update_core' );
+	remove_action( 'wp_version_check', 'wp_version_check' );
+
+	remove_action( 'load-plugins.php', 'wp_update_plugins' );
+	remove_action( 'load-update.php', 'wp_update_plugins' );
+	remove_action( 'load-update-core.php', 'wp_update_plugins' );
+	remove_action( 'admin_init', '_maybe_update_plugins' );
+	remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+
+	remove_action( 'load-themes.php', 'wp_update_themes' );
+	remove_action( 'load-update.php', 'wp_update_themes' );
+	remove_action( 'load-update-core.php', 'wp_update_themes' );
+	remove_action( 'admin_init', '_maybe_update_themes' );
+	remove_action( 'wp_update_themes', 'wp_update_themes' );
+
+	remove_action( 'update_option_WPLANG', 'wp_clean_update_cache', 10, 0 );
+
+	remove_action( 'wp_maybe_auto_update', 'wp_maybe_auto_update' );
+
+	remove_action( 'init', 'wp_schedule_update_checks' );
+} );
