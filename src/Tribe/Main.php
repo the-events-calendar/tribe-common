0924cf9f2bbb93d2281fe0d65121e120e3fb9aef
Dimitrios Pantazis <dpanta94@gmail.com>
2025-01-30 21:13:36 +0200

    adding test coverage

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -99,6 +99,10 @@ class Tribe__Main {
 		add_action( 'tribe_common_loaded', [ $this, 'tribe_common_app_store' ], 10 );
 		add_action( 'customize_controls_print_styles', [ $this, 'load_tec_variables' ], 10 );
 
+		add_action( 'current_screen', function () {
+			do_action( 'tribe_admin_headers_about_to_be_sent' );
+		}, PHP_INT_MAX );
+
 		if ( did_action( 'plugins_loaded' ) && ! doing_action( 'plugins_loaded' ) ) {
 			/*
 			 * This might happen in the context of a plugin activation.
