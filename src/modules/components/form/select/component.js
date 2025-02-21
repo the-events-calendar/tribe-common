ff8c8c70ffd35cf21b51ed45186633d31c44a353
Mike Cotton <codingmusician@gmail.com>
2025-02-21 13:53:53 -0600

    add map to options

--- src/modules/components/form/select/component.js
+++ src/modules/components/form/select/component.js
@@ -19,27 +19,28 @@ import './style.pcss';
 
 export default class Select extends PureComponent {
 	static propTypes = {
-		options: PropTypes.arrayOf( PropTypes.shape( {
-			label: PropTypes.string.isRequired,
-			value: PropTypes.any.isRequired,
-		} ) ),
+		options: PropTypes.shape( {
+			label: PropTypes.string,
+			value: PropTypes.any,
+			map: PropTypes.func,
+		} ),
 		onOptionClick: PropTypes.func.isRequired,
 		optionClassName: PropTypes.string,
 		isOpen: PropTypes.bool.isRequired,
 		value: PropTypes.any,
 		className: PropTypes.string,
-	}
+	};
 
 	static defaultProps = {
 		onOptionClick: noop,
 		isOpen: true,
 		optionClassName: '',
-	}
+	};
 
 	_onOptionClick = ( onClose, value, e ) => {
 		this.props.onOptionClick( value, e );
 		onClose();
-	}
+	};
 
 	get selected() {
 		return find( this.props.options, option => option.value === this.props.value );
@@ -66,7 +67,7 @@ export default class Select extends PureComponent {
 				{ option.label }
 			</button>
 		) )
-	)
+	);
 
 	renderToggle = ( { onToggle, isOpen } ) => (
 		<div className="tribe-common-form-select__toggle">
@@ -82,7 +83,7 @@ export default class Select extends PureComponent {
 				/>
 			</button>
 		</div>
-	)
+	);
 
 	renderContent = ( { onClose } ) => (
 		<ScrollTo>
