76fd5957571ac1307b7085daa3c1f82c17a0498f
Dimitrios Pantazis <dpanta94@gmail.com>
2025-03-11 14:31:31 +0200

    print title tag on embed header

--- src/functions/template-tags/html.php
+++ src/functions/template-tags/html.php
@@ -240,6 +240,7 @@ if ( ! function_exists( 'tec_embed_header' ) ) {
 		<!DOCTYPE html>
 		<html <?php language_attributes(); ?>>
 			<head>
+				<title><?php wp_title(); ?></title>
 				<meta charset="<?php bloginfo( 'charset' ); ?>">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
