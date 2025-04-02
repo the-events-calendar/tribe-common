0bfb6020c8413d8c906ad15cd7737f3068f89694
Camwyn <stephen@theeventscalendar.com>
2025-04-02 13:08:05 -0400

    version bump

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tribe Common
  * Description: An event settings framework for managing shared options.
- * Version: 6.5.3
+ * Version: 6.6.0
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: The Events Calendar
