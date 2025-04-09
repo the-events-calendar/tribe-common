7dc2b42f3fac753110c96d2a387af25cf89345e6
George Pattichis <gpattihis@gmail.com>
2025-04-09 19:12:35 +0300

    add parent classes to widget fields

--- src/admin-views/widgets/components/fields/dropdown.php
+++ src/admin-views/widgets/components/fields/dropdown.php
@@ -10,26 +10,28 @@
  *
  * @version 4.12.18
  *
- * @var string                      $label      Label for the dropdown.
- * @var string                      $value      Value for the dropdown.
- * @var string                      $id         ID of the dropdown.
- * @var string                      $name       Name attribute for the dropdown.
- * @var string                      $classes    Classes to add to the dropdown.
- * @var string                      $dependency The dependency attributes for the control wrapper.
- * @var array<array<string,string>> $options    An array of options in the format
- *                                              [
- *                                                  'value => string|int,
- *                                                  'text' => string
- *                                              ]
+ * @var string                      $label          Label for the dropdown.
+ * @var string                      $value          Value for the dropdown.
+ * @var string                      $id             ID of the dropdown.
+ * @var string                      $name           Name attribute for the dropdown.
+ * @var string                      $parent_classes Classes for the dropdown parent.
+ * @var string                      $classes        Classes to add to the dropdown.
+ * @var string                      $dependency     The dependency attributes for the control wrapper.
+ * @var array<array<string,string>> $options        An array of options in the format
+ *                                                  [
+ *                                                      'value => string|int,
+ *                                                      'text' => string
+ *                                                  ]
  */
 
 use Tribe__Utils__Array as Arr;
 
 $select_classes = array_merge( [ 'tribe-widget-form-control__input', ' widefat' ], Arr::list_to_array( $classes, ' ' ) );
 
+$parent_classes = array_merge( [ 'tribe-widget-form-control', 'tribe-widget-form-control--dropdown' ], Arr::list_to_array( $parent_classes, ' ' ) );
+
 ?>
-<div
-	class="tribe-widget-form-control tribe-widget-form-control--dropdown"
+<div <?php tribe_classes( $parent_classes ); ?>
 	<?php
 	// Not escaped - contains html (data-attr="value").
 	echo $dependency; // phpcs:ignore
