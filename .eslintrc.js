ac9e49f1da675b7b5b25cc37a6ba833c2d6e2d3f
Dimitrios Pantazis <dpanta94@gmail.com>
2025-04-14 18:38:36 +0300

    Update globals for linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,26 @@ module.exports = {
 	...eslintConfig,
 	overrides: [
 		...eslintConfig.overrides,
-	]
+	],
+	globals: {
+		...eslintConfig.globals,
+		wp: true,
+		jQuery: true,
+		React: true,
+		ajaxurl: true,
+		Clipboard: true,
+		ClipboardJS: true,
+		dayjs: true,
+		introJs: true,
+		moment: true,
+		TEC: true,
+		tribe: true,
+		tribe_l10n_datatables: true,
+		tribe_system_info: true,
+		TribeOnboarding: true,
+		TribeOnboardingHints: true,
+		TribeOnboardingTour: true,
+		tec_automator: true,
+		tribe_dropdowns: true
+	},
 };
