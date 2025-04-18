b936231dc692dac1249664db7e29711ad4d20f4c
Brian Krane <redscar@gmail.com>
2025-04-18 13:19:28 -0400

    Update snapshots and test leakage.

--- tests/eva_integration/Zapier/DashboardTest.php
+++ tests/eva_integration/Zapier/DashboardTest.php
@@ -28,22 +28,22 @@ class DashboardTest extends \Codeception\TestCase\WPTestCase {
 		$this->set_fn_return( 'wp_create_nonce', '123123' );
 		$dashboard = tribe( Dashboard::class );
 		tribe( Zapier_Tickets_Plus_Provider::class )->add_endpoints_to_dashboard();
-		$fields   = $dashboard->add_fields( [] );
-		add_filter( 'tec_event_automator_zapier_endpoints', function( $endpoints ) {
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\New_Events']['count']      = 1;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Attendees']['count']       = 1;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Checkin']['count']         = 1;
 
-			foreach ( $endpoints as $class => &$data ) {
-				if ( 0 === strpos( $class, 'TEC\Event_Automator\Zapier\REST\V1\Endpoints\\' ) ) {
-					$data['count'] = 1;
+		add_filter(
+			'tec_event_automator_zapier_endpoints',
+			function ( $endpoints ) {
+				foreach ( $endpoints as $class => &$data ) {
+					if ( 0 === strpos( $class, 'TEC\Event_Automator\Zapier\REST\V1\Endpoints\\' ) ) {
+						$data['count'] = 0;
+					}
 				}
+				unset( $data );
+
+				return $endpoints;
 			}
-			unset( $data );
+		);
 
-			return $endpoints;
-		});
-
+		$fields = $dashboard->add_fields( [] );
 		$this->assertMatchesJsonSnapshot( json_encode( $fields, JSON_PRETTY_PRINT ) );
 	}
 
@@ -54,21 +54,21 @@ class DashboardTest extends \Codeception\TestCase\WPTestCase {
 		$this->set_fn_return( 'wp_create_nonce', '123123' );
 		$dashboard = tribe( Dashboard::class );
 		tribe( Zapier_Pro_Provider::class )->add_endpoints_to_dashboard();
-		$fields   = $dashboard->add_fields( [] );
-		add_filter( 'tec_event_automator_zapier_endpoints', function( $endpoints ) {
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\New_Events']['count']      = 0;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Attendees']['count']       = 0;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Checkin']['count']         = 0;
-
-			foreach ( $endpoints as $class => &$data ) {
-				if ( 0 === strpos( $class, 'TEC\Event_Automator\Zapier\REST\V1\Endpoints\\' ) ) {
-					$data['count'] = 1;
+		add_filter(
+			'tec_event_automator_zapier_endpoints',
+			function ( $endpoints ) {
+				foreach ( $endpoints as $class => &$data ) {
+					if ( 0 === strpos( $class, 'TEC\Event_Automator\Zapier\REST\V1\Endpoints\\' ) ) {
+						$data['count'] = 0;
+					}
 				}
-			}
-			unset( $data );
+				unset( $data );
 
-			return $endpoints;
-		});
+				return $endpoints;
+			}
+		);
+
+		$fields = $dashboard->add_fields( [] );
 
 		$this->assertMatchesJsonSnapshot( json_encode( $fields, JSON_PRETTY_PRINT ) );
 	}
@@ -82,12 +82,9 @@ class DashboardTest extends \Codeception\TestCase\WPTestCase {
 		tribe( Zapier_Tickets_Plus_Provider::class )->add_endpoints_to_dashboard();
 		tribe( Zapier_Pro_Provider::class )->add_endpoints_to_dashboard();
 		add_filter( 'tec_event_automator_zapier_endpoints', function( $endpoints ) {
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\New_Events']['enabled'] = false;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Attendees']['enabled']  = false;
-			$endpoints['TEC\Event_Automator\Zapier\REST\V1\Endpoints\Checkin']['enabled']    = false;
 			foreach ( $endpoints as $class => &$data ) {
 				if ( 0 === strpos( $class, 'TEC\Event_Automator\Zapier\REST\V1\Endpoints\\' ) ) {
-					$data['count'] = 1;
+					$data['count'] = 0;
 				}
 			}
 			unset( $data );
