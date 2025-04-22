4541e0bba2881bd61125b91f015eb4c68280bb62
Camwyn <stephen@theeventscalendar.com>
2025-04-22 15:43:01 -0400

    Fix ubuntu version in phpstan tests

--- tribe-common.php
+++ tribe-common.php
@@ -32,5 +32,4 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // The main plugin class.
 require_once dirname( __FILE__ ) . '/src/Tribe/Main.php';
 
-
 Tribe__Main::instance();
