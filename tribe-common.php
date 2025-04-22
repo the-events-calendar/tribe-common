da7d3d38abc860a8e1465a44d90f9dfd10bf7a79
Camwyn <stephen@theeventscalendar.com>
2025-04-22 15:29:43 -0400

    php change to trigger tests

--- tribe-common.php
+++ tribe-common.php
@@ -32,4 +32,5 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 // The main plugin class.
 require_once dirname( __FILE__ ) . '/src/Tribe/Main.php';
 
+
 Tribe__Main::instance();
