01e966579e8620c3f85a8a6d2da72859b41f9649
Camwyn <stephen@theeventscalendar.com>
2025-04-24 17:21:32 -0400

    use tests_add_filter to disable onboarding wizard

--- tests/end2end/_bootstrap.php
+++ tests/end2end/_bootstrap.php
@@ -1,3 +1,3 @@
 <?php
-add_filter( 'tec_tickets_onboarding_is_active', '__return_false' );
-add_filter( 'tec_events_onboarding_is_active', '__return_false' );
+tests_add_filter( 'tec_tickets_onboarding_is_active', '__return_false' );
+tests_add_filter( 'tec_events_onboarding_is_active', '__return_false' );
