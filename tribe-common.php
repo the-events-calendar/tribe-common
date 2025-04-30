e13e809b14bb7a8e7d4a016682405c01ad097964
Camwyn <929375+Camwyn@users.noreply.github.com>
2025-04-30 17:56:49 +0000

    Version bump for release/T25.elektra
    This is an automated PR created by Camwyn.
    Generated by: https://github.com/the-events-calendar/tribe-common/actions/runs/14761056843

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tribe Common
  * Description: An event settings framework for managing shared options.
- * Version: 6.6.1
+ * Version: 6.6.2
  * Requires at least: 6.6
  * Requires PHP: 7.4
  * Author: The Events Calendar
