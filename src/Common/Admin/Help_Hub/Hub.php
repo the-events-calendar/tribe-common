ad59773c8a19ba1be1077e7777d46ed6bd4469b8
Brian Krane <redscar@gmail.com>
2025-05-15 11:30:51 -0400

    Reverted accidental commit.

--- src/Common/Admin/Help_Hub/Hub.php
+++ src/Common/Admin/Help_Hub/Hub.php
@@ -272,18 +272,15 @@ class Hub {
 	 * to customize or add additional classes via the `tec_help_hub_body_classes` filter.
 	 *
 	 * @since 6.3.2
-	 * @since TBD Removed type hinting from method.
 	 *
-	 * @param mixed $classes Value passed to the `admin_body_class` filter. Expected to be a string.
+	 * @param string $classes Space-separated string of classes for the body tag.
 	 *
 	 * @return string Filtered list of classes.
 	 */
-	public function add_help_page_body_class( $classes ): string {
+	public function add_help_page_body_class( string $classes ): string {
 		if ( ! self::is_current_page() ) {
 			return $classes;
 		}
-
-		$classes = (string) $classes;
 
 		// Default classes for Help Hub.
 		$default_classes = [ 'tribe-help', 'tec-help' ];
