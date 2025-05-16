64963baea202b49645811763a3cf8b9cb23df37a
Luca Tumedei <luca@theaveragedev.com>
2025-05-16 15:34:46 +0200

    refactor(Classy) plase Slots
    Thi sets up the Slot/Fill system to be extended from other plugins by
    simply using the filter. The code inline documentation outlines how to
    use the system and the theory behind.

--- src/resources/packages/classy/elements/Classy.tsx
+++ src/resources/packages/classy/elements/Classy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Slot, SlotFillProvider } from '@wordpress/components';
-import { doAction } from '@wordpress/hooks';
+import { applyFilters } from '@wordpress/hooks';
 import { _x } from '@wordpress/i18n';
 import {
 	EventDetails,
@@ -18,14 +18,85 @@ function ClassyApplication() {
 		<SlotFillProvider>
 			{
 				/**
-				 * Filters the rendered JSX of the Classy component.
+				 * Starts the rendering of the Classy application component.
 				 *
-				 * This component is wrapped within a `SlotFillProvider` to allow dynamic content insertion
-				 * via the `Slot/Fill` API. Use the `addFilter` hook to add Fills into the Classy application slots.
+				 * Extending plugins should hook on this filter to render their own components using the Slot/Fill API.
+				 *
+				 * Why is this a filter application instead of being an action? Because we're calling this inside `{}`
+				 * which means the returned values (React nodes) will be rendered here.
+				 * See notes on each Slot for more information and usage examples.
+				 *
+				 * @see https://developer.wordpress.org/block-editor/reference-guides/components/slot-fill/
+				 *
+				 * Incorrect usage of this filter is to render something outside a named Slot as it will render here.
+				 * Bad example:
+				 * ```
+				 * addFilter(
+				 * 	'tec.classy.render',
+				 * 	'tec.classy.my-plugin',
+				 * 	() => (
+				 * 		<Fragment>
+				 *          <p>HELLO FROM MY PLUGIN</p>
+				 * 		</Fragment>
+				 * 	)
+				 * );
+				 * ```
+				 * This will render the paragraph before any other field. That is likely not the desired behavior.
+				 *
+				 * Correct usage is to add to this filter and then render in a named Slot(s).
+				 *
+				 * Good example:
+				 * ```
+				 * addFilter(
+				 * 	'tec.classy.render',
+				 * 	'tec.classy.my-plugin',
+				 * 	(fields: React.ReactNode | null) => (
+				 * 		<Fragment>
+				 * 			{fields}
+				 * 			<Fill name='tec.classy.fields'>
+				 * 				<p>HELLO FROM MY PLUGIN</p>
+				 * 			</Fill>
+				 * 		</Fragment>
+				 * 	)
+				 * );
+				 * ```
+				 * In this example the rendered component (a paragraph) will be rendered among the Classy fields.
+				 *
+				 * Developers: do not add more filters, add more named Slots to control where things are rendered.
 				 *
 				 * @since TBD
+				 *
+				 * @param {React.ReactNode|null} The nodes previously rendered, starting with no nodes (null).
 				 */
-				doAction( 'classy.render' )
+				applyFilters( 'tec.classy.render', null )
+			}
+
+			{
+				/**
+				 * Renders before the Classy container.
+				 *
+				 * Extending plugins must hook on the `tec.classy.render` filter and render components in this Slot.
+				 *
+				 * Example:
+				 * ```
+				 * addFilter(
+				 * 	'tec.classy.render',
+				 * 	'tec.classy.my-plugin',
+				 * 	(fields: React.ReactNode | null) => (
+				 * 		<Fragment>
+				 * 			{fields}
+				 * 			<Fill name='tec.classy.before'>
+				 * 				<p>HELLO FROM MY PLUGIN</p>
+				 * 			</Fill>
+				 * 		</Fragment>
+				 * 	)
+				 * );
+				 * ```
+				 *
+				 * Note that, as in any filter, it's up to the extending plugin to manage priority in the filter
+				 * or whether previous nodes will be rendered or not.
+				 */
+				<Slot name="tec.classy.before" />
 			}
 
 			<div className="classy-container">
@@ -69,8 +140,118 @@ function ClassyApplication() {
 					) }
 				/>
 
-				<Slot name="classy.fields" />
+				{
+					/**
+					 * Renders in the Classy container, before the fields.
+					 *
+					 * Extending plugins must hook on the `tec.classy.render` filter and render components in this Slot.
+					 *
+					 * Example:
+					 * ```
+					 * addFilter(
+					 * 	'tec.classy.render',
+					 * 	'tec.classy.my-plugin',
+					 * 	(fields: React.ReactNode | null) => (
+					 * 		<Fragment>
+					 * 			{fields}
+					 * 			<Fill name='tec.classy.fields.before'>
+					 * 				<p>HELLO FROM MY PLUGIN</p>
+					 * 			</Fill>
+					 * 		</Fragment>
+					 * 	)
+					 * );
+					 * ```
+					 *
+					 * Note that, as in any filter, it's up to the extending plugin to manage priority in the filter
+					 * or whether previous nodes will be rendered or not.
+					 */
+					<Slot name="tec.classy.fields.before" />
+				}
+
+				{
+					/**
+					 * Renders in the Classy fields.
+					 *
+					 * Extending plugins must hook on the `tec.classy.render` filter and render components in this Slot.
+					 *
+					 * Example:
+					 * ```
+					 * addFilter(
+					 * 	'tec.classy.render',
+					 * 	'tec.classy.my-plugin',
+					 * 	(fields: React.ReactNode | null) => (
+					 * 		<Fragment>
+					 * 			{fields}
+					 * 			<Fill name='tec.classy.fields'>
+					 * 				<p>HELLO FROM MY PLUGIN</p>
+					 * 			</Fill>
+					 * 		</Fragment>
+					 * 	)
+					 * );
+					 * ```
+					 *
+					 * Note that, as in any filter, it's up to the extending plugin to manage priority in the filter
+					 * or whether previous nodes will be rendered or not.
+					 */
+					<Slot name="tec.classy.fields" />
+				}
+
+				{
+					/**
+					 * Renders in the Classy container, after the fields.
+					 *
+					 * Extending plugins must hook on the `tec.classy.render` filter and render components in this Slot.
+					 *
+					 * Example:
+					 * ```
+					 * addFilter(
+					 * 	'tec.classy.render',
+					 * 	'tec.classy.my-plugin',
+					 * 	(fields: React.ReactNode | null) => (
+					 * 		<Fragment>
+					 * 			{fields}
+					 * 			<Fill name='tec.classy.fields.after'>
+					 * 				<p>HELLO FROM MY PLUGIN</p>
+					 * 			</Fill>
+					 * 		</Fragment>
+					 * 	)
+					 * );
+					 * ```
+					 *
+					 * Note that, as in any filter, it's up to the extending plugin to manage priority in the filter
+					 * or whether previous nodes will be rendered or not.
+					 */
+					<Slot name="tec.classy.fields.after" />
+				}
 			</div>
+
+			{
+				/**
+				 * Renders outside of the Classy container, after the fields.
+				 *
+				 * Extending plugins must hook on the `tec.classy.render` filter and render components in this Slot.
+				 *
+				 * Example:
+				 * ```
+				 * addFilter(
+				 * 	'tec.classy.render',
+				 * 	'tec.classy.my-plugin',
+				 * 	(fields: React.ReactNode | null) => (
+				 * 		<Fragment>
+				 * 			{fields}
+				 * 			<Fill name='tec.classy.after'>
+				 * 				<p>HELLO FROM MY PLUGIN</p>
+				 * 			</Fill>
+				 * 		</Fragment>
+				 * 	)
+				 * );
+				 * ```
+				 *
+				 * Note that, as in any filter, it's up to the extending plugin to manage priority in the filter
+				 * or whether previous nodes will be rendered or not.
+				 */
+				<Slot name="tec.classy.after" />
+			}
 		</SlotFillProvider>
 	);
 }
