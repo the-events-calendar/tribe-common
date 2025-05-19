5079253a9f973148f7fefb33b2359674b635d3bb
Dimitrios Pantazis <dpanta94@gmail.com>
2025-05-19 20:09:27 +0300

    Update plugin JS object refs

--- src/modules/data/plugins/constants.js
+++ src/modules/data/plugins/constants.js
@@ -1,5 +1,5 @@
 export const EVENTS_PLUGIN = 'events';
-export const EVENTS_PRO_PLUGIN = 'events-pro';
+export const EVENTS_PRO_PLUGIN = 'eventsPro';
 export const EVENTS_VIRTUAL_PLUGIN = 'events-virtual';
 export const TICKETS = 'tickets';
-export const TICKETS_PLUS = 'tickets-plus';
+export const TICKETS_PLUS = 'ticketsPlus';
