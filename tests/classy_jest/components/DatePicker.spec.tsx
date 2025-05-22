0892e51cf4b4d77dd315731c8c74f2d98fe20717
Luca Tumedei <luca@theaveragedev.com>
2025-05-22 14:20:55 +0200

    test(classy_jest) wrap DatePicker coverage

--- tests/classy_jest/components/DatePicker.spec.tsx
+++ tests/classy_jest/components/DatePicker.spec.tsx
@@ -112,64 +112,75 @@ describe( 'DatePicker Component', () => {
 	} );
 
 	describe( 'date selection', () => {
-		it( 'handles picking new start date', () => {
-			const props = {
-				...defaultProps,
-				// The user is selecting the start date.
-				isSelectingDate: 'start',
-				// Show the popover, we start from the state where the user has clicked the date picker to pick a date.
-				showPopover: true,
-				onChange: jest.fn(),
-				onClick: jest.fn(),
-				onClose: jest.fn(),
-			} as DatePickerProps;
+		it.each( [
+			[ 'start', defaultProps.startDate, '21', '2023-12-21T10:00:00' ],
+			[ 'end', defaultProps.endDate, '25', '2023-12-25T13:00:00' ],
+		] )(
+			'handles picking new %s date',
+			( isSelectingDate: string, currentDate: Date, selectionText: string, expected: string ) => {
+				const props = {
+					...defaultProps,
+					// The user is selecting the end date.
+					isSelectingDate,
+					// Show the popover, we start from the state where the user has clicked the date picker to pick a date.
+					showPopover: true,
+					onChange: jest.fn(),
+					onClick: jest.fn(),
+					onClose: jest.fn(),
+					currentDate,
+				} as DatePickerProps;
 
-			const { getByText } = render( <DatePicker { ...props } /> );
+				const { getByText } = render( <DatePicker { ...props } /> );
 
-			// The user picks a new start date: 2023-12-21.
-			fireEvent.click( getByText( '21' ) );
+				// The user picks a new end date: 2023-12-25.
+				fireEvent.click( getByText( selectionText ) );
 
-			expect( props.onChange ).toHaveBeenCalledWith( 'start', '2023-12-21T10:00:00' );
-			expect( props.onClick ).not.toHaveBeenCalled();
-			expect( props.onClose ).not.toHaveBeenCalled();
-		} );
+				expect( props.onChange ).toHaveBeenCalledWith( isSelectingDate, expected );
+				expect( props.onClick ).not.toHaveBeenCalled();
+				expect( props.onClose ).not.toHaveBeenCalled();
+			}
+		);
 
-		it( 'handles closing the date selection modal', () => {
-			const baseElement = document.createElement( 'div' );
-			const props = {
-				...defaultProps,
-				anchor: baseElement,
-				// The user is selecting the start date.
-				isSelectingDate: 'start',
-				// Show the popover, we start from the state where the user has clicked the date picker to pick a date.
-				showPopover: true,
-				onChange: jest.fn(),
-				onClick: jest.fn(),
-				onClose: jest.fn(),
-			} as DatePickerProps;
+		it.each( [ 'start', 'end' ] )(
+			'handles closing the date selection modal while selecting %s',
+			( isSelectingDate: string ) => {
+				const baseElement = document.createElement( 'div' );
+				const props = {
+					...defaultProps,
+					anchor: baseElement,
+					// The user is selecting the start date.
+					isSelectingDate,
+					// Show the popover, we start from the state where the user has clicked the date picker to pick a date.
+					showPopover: true,
+					onChange: jest.fn(),
+					onClick: jest.fn(),
+					onClose: jest.fn(),
+				} as DatePickerProps;
 
-			const { container, asFragment } = render( <DatePicker { ...props } /> );
+				const { container, asFragment } = render( <DatePicker { ...props } /> );
 
-			const initialRender = asFragment();
+				const initialRender = asFragment();
 
-			// The user closes the modal by pressing Escape.
-			const popover = document.body.querySelector( '.classy-component__popover--calendar' );
+				// Select the popover element from the document, it will not be attached to the component.
+				const popover = document.body.querySelector( '.classy-component__popover--calendar' );
 
-			const keyDownEscape = new KeyboardEvent( 'keydown', {
-				key: 'Escape',
-				code: 'Escape',
-				keyCode: 27, // While deprecated, this is the property the dialog WordPress logic is actually using.
-				bubbles: true,
-				cancelable: true,
-			} );
+				// The user closes the modal by pressing Escape.
+				const keyDownEscape = new KeyboardEvent( 'keydown', {
+					key: 'Escape',
+					code: 'Escape',
+					keyCode: 27, // While deprecated, this is the property the dialog WordPress logic is actually using.
+					bubbles: true,
+					cancelable: true,
+				} );
 
-			// The user presses Escape to close the modal.
-			// We're not using the fireEvent API as it will not dispatch correctly using the deprecated keyCode property.
-			popover.dispatchEvent( keyDownEscape );
+				// The user presses Escape to close the modal.
+				// We're not using the fireEvent API as it will not dispatch correctly using the deprecated keyCode property.
+				popover.dispatchEvent( keyDownEscape );
 
-			expect( props.onChange ).not.toHaveBeenCalled();
-			expect( props.onClick ).not.toHaveBeenCalled();
-			expect( props.onClose ).toHaveBeenCalledTimes( 1 );
-		} );
+				expect( props.onChange ).not.toHaveBeenCalled();
+				expect( props.onClick ).not.toHaveBeenCalled();
+				expect( props.onClose ).toHaveBeenCalledTimes( 1 );
+			}
+		);
 	} );
 } );
