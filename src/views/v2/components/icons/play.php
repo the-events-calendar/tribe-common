c387f8208b216fe227bfa890f2cc787aec67da13
rafsuntaskin <rafsun.aiub@gmail.com>
2025-06-25 04:33:00 +0600

    more svg updates

--- src/views/v2/components/icons/play.php
+++ src/views/v2/components/icons/play.php
@@ -9,7 +9,11 @@
  *
  * @link http://evnt.is/1aiy
  *
- * @version 4.12.14
+ * @since 4.12.14 Initial template.
+ *
+ * @since TBD Updated for accessibility changes.
+ *
+ * @version TBD
  *
  * @var array $classes Additional classes to add to the svg icon.
  */
@@ -20,4 +24,11 @@ if ( ! empty( $classes ) ) {
 	$svg_classes = array_merge( $svg_classes, $classes );
 }
 ?>
-<svg <?php tribe_classes( $svg_classes ); ?> xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 12"><path d="M10 6a1 1 0 01-.47.848l-8 5a.996.996 0 01-1.237-.14A.999.999 0 010 11V1A1 1 0 011.53.153l8 5A1 1 0 0110 6z" fill-rule="nonzero"/></svg>
+<svg
+	<?php tec_classes( $svg_classes ); ?>
+	viewBox="0 0 10 12"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<title><?php echo esc_html__( 'Play', 'tribe-common' ); ?></title>
+	<path d="M10 6a1 1 0 01-.47.848l-8 5a.996.996 0 01-1.237-.14A.999.999 0 010 11V1A1 1 0 011.53.153l8 5A1 1 0 0110 6z" fill-rule="nonzero"/>
+</svg>
--- src/views/v2/components/icons/video.php
+++ src/views/v2/components/icons/video.php
@@ -11,13 +11,24 @@
  *
  * @var array<string> $classes Additional classes to add to the svg icon.
  *
- * @version 4.12.14
+ * @since 4.12.14 Initial template.
+ *
+ * @since TBD Updated for accessibility changes.
+ *
+ * @version TBD
+ *
  */
-
 $svg_classes = [ 'tribe-common-c-svgicon', 'tribe-common-c-svgicon--video' ];
 
 if ( ! empty( $classes ) ) {
 	$svg_classes = array_merge( $svg_classes, $classes );
 }
 ?>
-<svg <?php tribe_classes( $svg_classes ); ?> xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 12"><path d="M11 4V1c0-.6-.4-1-1-1H1C.4 0 0 .4 0 1v10c0 .6.4 1 1 1h9c.6 0 1-.4 1-1V8l5 2V2l-5 2z" fill-rule="nonzero" class="tribe-common-c-svgicon__svg-fill"/></svg>
+<svg
+	<?php tec_classes( $svg_classes ); ?>
+	viewBox="0 0 16 12"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<title><?php echo esc_html__( 'Video', 'tribe-common' ); ?></title>
+	<path d="M11 4V1c0-.6-.4-1-1-1H1C.4 0 0 .4 0 1v10c0 .6.4 1 1 1h9c.6 0 1-.4 1-1V8l5 2V2l-5 2z" fill-rule="nonzero" class="tribe-common-c-svgicon__svg-fill"/>
+</svg>
