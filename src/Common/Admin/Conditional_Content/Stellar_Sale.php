2b5f38e83e631fd897e01bed9596922345e95610
Stephen Page <stephen@theeventscalendar.com>
2025-07-03 15:28:39 -0400

    Second phase of banner logic updates for the 2025 stellar sale.

--- src/Common/Admin/Conditional_Content/Stellar_Sale.php
+++ src/Common/Admin/Conditional_Content/Stellar_Sale.php
@@ -57,4 +57,85 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 	protected function get_link_url(): string {
 		return 'https://evnt.is/1bdv';
 	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function get_suite_creative_map(): array {
+		return [
+			'events'  => [
+				'events-pro/events-calendar-pro.php'                   => [
+					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-wide.png', false, null, \Tribe__Main::instance() ),
+					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-narrow.png', false, null, \Tribe__Main::instance() ),
+					'link_url'         => 'https://evnt.is/stellar-pro-2024',
+					'alt_text'         => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s - Get Events Calendar Pro at 30%% off!', 'Alt text for the Events Pro Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+				'events-filterbar/the-events-calendar-filter-view.php' => [
+					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-wide.png', false, null, \Tribe__Main::instance() ),
+					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-narrow.png', false, null, \Tribe__Main::instance() ),
+					'link_url'         => 'https://evnt.is/stellar-fbar-2024',
+					'alt_text'         => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s - Get Filter Bar at 30%% off!', 'Alt text for the Filter Bar Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+				'default'                                              => [
+					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_wide_banner_image(), false, null, \Tribe__Main::instance() ),
+					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_narrow_banner_image(), false, null, \Tribe__Main::instance() ),
+					'link_url'         => $this->get_link_url(),
+					'alt_text'         => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Default Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+			],
+			'tickets' => [
+				'event-tickets-plus/event-tickets-plus.php' => [
+					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-wide.png', false, null, \Tribe__Main::instance() ),
+					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-narrow.png', false, null, \Tribe__Main::instance() ),
+					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-sidebar.png', false, null, \Tribe__Main::instance() ),
+					'link_url'          => 'https://evnt.is/stellar-etp-2024',
+					'alt_text'          => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s - Get Event Tickets Plus at 30%% off!', 'Alt text for the Event Tickets Plus Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+				'event-tickets-plus/seating.php'            => [
+					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-top-wide.png', false, null, \Tribe__Main::instance() ),
+					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-top-narrow.png', false, null, \Tribe__Main::instance() ),
+					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-sidebar.png', false, null, \Tribe__Main::instance() ),
+					'link_url'          => 'https://evnt.is/stellar-etp-2024',
+					'alt_text'          => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s - Get Event Tickets Plus at 30%% off!', 'Alt text for the Event Tickets Plus Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+				'default'                                   => [
+					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_wide_banner_image(), false, null, \Tribe__Main::instance() ),
+					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_narrow_banner_image(), false, null, \Tribe__Main::instance() ),
+					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_sidebar_image(), false, null, \Tribe__Main::instance() ),
+					'link_url'          => $this->get_link_url(),
+					'alt_text'          => sprintf(
+						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
+						_x( '%1$s %2$s for The Events Calendar plugins, add-ons and bundles.', 'Alt text for the Default Sale Ad', 'tribe-common' ),
+						date_i18n( 'Y' ),
+						$this->get_sale_name()
+					),
+				],
+			],
+		];
+	}
 }
