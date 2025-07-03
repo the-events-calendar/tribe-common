4706ad8a2c174e9866dc9d9877935e04190c8236
Stephen Page <stephen@theeventscalendar.com>
2025-07-03 16:03:41 -0400

    Fix assets for seating banner.

--- src/Common/Admin/Conditional_Content/Stellar_Sale.php
+++ src/Common/Admin/Conditional_Content/Stellar_Sale.php
@@ -67,7 +67,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 				'events-pro/events-calendar-pro.php'                   => [
 					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-narrow.png', false, null, \Tribe__Main::instance() ),
-					'link_url'         => 'https://evnt.is/stellar-pro-2024',
+					'link_url'         => 'https://evnt.is/stellar-pro-2025',
 					'alt_text'         => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Events Calendar Pro at 30%% off!', 'Alt text for the Events Pro Sale Ad', 'tribe-common' ),
@@ -78,7 +78,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 				'events-filterbar/the-events-calendar-filter-view.php' => [
 					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-narrow.png', false, null, \Tribe__Main::instance() ),
-					'link_url'         => 'https://evnt.is/stellar-fbar-2024',
+					'link_url'         => 'https://evnt.is/stellar-fbar-2025',
 					'alt_text'         => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Filter Bar at 30%% off!', 'Alt text for the Filter Bar Sale Ad', 'tribe-common' ),
@@ -103,7 +103,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-narrow.png', false, null, \Tribe__Main::instance() ),
 					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-sidebar.png', false, null, \Tribe__Main::instance() ),
-					'link_url'          => 'https://evnt.is/stellar-etp-2024',
+					'link_url'          => 'https://evnt.is/stellar-etp-2025',
 					'alt_text'          => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Event Tickets Plus at 30%% off!', 'Alt text for the Event Tickets Plus Sale Ad', 'tribe-common' ),
@@ -112,13 +112,13 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 					),
 				],
 				'event-tickets-plus/seating.php'            => [
-					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-top-wide.png', false, null, \Tribe__Main::instance() ),
-					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-top-narrow.png', false, null, \Tribe__Main::instance() ),
-					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seat-sidebar.png', false, null, \Tribe__Main::instance() ),
-					'link_url'          => 'https://evnt.is/stellar-etp-2024',
+					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-top-wide.png', false, null, \Tribe__Main::instance() ),
+					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-top-narrow.png', false, null, \Tribe__Main::instance() ),
+					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-sidebar.png', false, null, \Tribe__Main::instance() ),
+					'link_url'          => 'https://evnt.is/stellar-seating-2025',
 					'alt_text'          => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
-						_x( '%1$s %2$s - Get Event Tickets Plus at 30%% off!', 'Alt text for the Event Tickets Plus Sale Ad', 'tribe-common' ),
+						_x( '%1$s %2$s - Get Event Seating at 30%% off!', 'Alt text for the Event Seating Sale Ad', 'tribe-common' ),
 						date_i18n( 'Y' ),
 						$this->get_sale_name()
 					),
