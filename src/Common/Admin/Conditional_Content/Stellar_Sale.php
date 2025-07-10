de6afecc840b6285bec4a8aed5503cf440bb58c5
Stephen Page <stephen@theeventscalendar.com>
2025-07-10 14:50:57 -0400

    Just use the default link url

--- src/Common/Admin/Conditional_Content/Stellar_Sale.php
+++ src/Common/Admin/Conditional_Content/Stellar_Sale.php
@@ -93,7 +93,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 				'events-calendar-pro/events-calendar-pro.php'                       => [
 					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/ecp-top-narrow.png', false, null, \Tribe__Main::instance() ),
-					'link_url'         => 'https://evnt.is/stellar-pro-2025',
+					'link_url'         => $this->get_link_url(),
 					'alt_text'         => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Events Calendar Pro at 30%% off!', 'Alt text for the Events Pro Sale Ad', 'tribe-common' ),
@@ -104,7 +104,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 				'the-events-calendar-filterbar/the-events-calendar-filter-view.php' => [
 					'image_url'        => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/fbar-top-narrow.png', false, null, \Tribe__Main::instance() ),
-					'link_url'         => 'https://evnt.is/stellar-fbar-2025',
+					'link_url'         => $this->get_link_url(),
 					'alt_text'         => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Filter Bar at 30%% off!', 'Alt text for the Filter Bar Sale Ad', 'tribe-common' ),
@@ -129,7 +129,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-top-narrow.png', false, null, \Tribe__Main::instance() ),
 					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/etp-sidebar.png', false, null, \Tribe__Main::instance() ),
-					'link_url'          => 'https://evnt.is/stellar-etp-2025',
+					'link_url'          => $this->get_link_url(),
 					'alt_text'          => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Event Tickets Plus at 30%% off!', 'Alt text for the Event Tickets Plus Sale Ad', 'tribe-common' ),
@@ -142,7 +142,7 @@ class Stellar_Sale extends Promotional_Content_Abstract {
 					'image_url'         => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-top-wide.png', false, null, \Tribe__Main::instance() ),
 					'narrow_image_url'  => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-top-narrow.png', false, null, \Tribe__Main::instance() ),
 					'sidebar_image_url' => tribe_resource_url( 'images/conditional-content/' . $this->get_slug() . '/seating-sidebar.png', false, null, \Tribe__Main::instance() ),
-					'link_url'          => 'https://evnt.is/stellar-seating-2025',
+					'link_url'          => $this->get_link_url(),
 					'alt_text'          => sprintf(
 						/* translators: %1$s: Sale year (numeric), %2$s: Sale name */
 						_x( '%1$s %2$s - Get Event Seating at 30%% off!', 'Alt text for the Event Seating Sale Ad', 'tribe-common' ),
