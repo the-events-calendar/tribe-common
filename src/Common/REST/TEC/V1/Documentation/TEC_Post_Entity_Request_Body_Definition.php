8e2b7a0fcf321b61c7bff8a8eae8f0a5e1ddbe78
Dimitrios Pantazis <dpanta94@gmail.com>
2025-07-24 14:23:49 +0300

    Fix phpcs

--- src/Common/REST/TEC/V1/Documentation/TEC_Post_Entity_Request_Body_Definition.php
+++ src/Common/REST/TEC/V1/Documentation/TEC_Post_Entity_Request_Body_Definition.php
@@ -43,7 +43,7 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 			'description' => __( 'A TEC post object as expected by the REST API', 'tribe-common' ),
 			'type'        => 'object',
 			'properties'  => [
-				'date'               => [
+				'date'           => [
 					'type'        => 'string',
 					'format'      => 'date-time',
 					'description' => __( 'The date the entity was published, in the site\'s timezone. In RFC3339 format.', 'tribe-common' ),
@@ -51,7 +51,7 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					'pattern'     => '^\d{4}-\d{2}-\d{2}[Tt ]\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}(?::\d{2})?)?$',
 					'nullable'    => true,
 				],
-				'date_gmt'           => [
+				'date_gmt'       => [
 					'type'        => 'string',
 					'format'      => 'date-time',
 					'description' => __( 'The date the entity was published, as GMT. In RFC3339 format.', 'tribe-common' ),
@@ -59,12 +59,12 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					'pattern'     => '^\d{4}-\d{2}-\d{2}[Tt ]\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}(?::\d{2})?)?$',
 					'nullable'    => true,
 				],
-				'slug'               => [
+				'slug'           => [
 					'type'        => 'string',
 					'description' => __( 'An alphanumeric identifier for the entity unique to its type', 'tribe-common' ),
 					'example'     => 'my-awesome-event',
 				],
-				'status'             => [
+				'status'         => [
 					'type'        => 'string',
 					'description' => __( 'A named status for the entity.', 'tribe-common' ),
 					'enum'        => [
@@ -76,32 +76,32 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					],
 					'example'     => 'publish',
 				],
-				'title'              => [
+				'title'          => [
 					'type'        => 'string',
 					'description' => __( 'The title for the entity.', 'tribe-common' ),
 					'example'     => 'My Awesome Event',
 				],
-				'content'            => [
+				'content'        => [
 					'type'        => 'string',
 					'description' => __( 'The content for the entity.', 'tribe-common' ),
 					'example'     => '<p>This is the content of my event...</p>',
 				],
-				'author'             => [
+				'author'         => [
 					'type'        => 'integer',
 					'description' => __( 'The ID for the author of the entity.', 'tribe-common' ),
 					'example'     => 1,
 				],
-				'excerpt'            => [
+				'excerpt'        => [
 					'type'        => 'string',
 					'description' => __( 'The excerpt for the entity.', 'tribe-common' ),
 					'example'     => '<p>This is the excerpt...</p>',
 				],
-				'featured_media'     => [
+				'featured_media' => [
 					'type'        => 'integer',
 					'description' => __( 'The ID of the featured media for the entity.', 'tribe-common' ),
 					'example'     => 123,
 				],
-				'comment_status'     => [
+				'comment_status' => [
 					'type'        => 'string',
 					'description' => __( 'Whether or not comments are open on the entity.', 'tribe-common' ),
 					'enum'        => [
@@ -110,7 +110,7 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					],
 					'example'     => 'open',
 				],
-				'ping_status'        => [
+				'ping_status'    => [
 					'type'        => 'string',
 					'description' => __( 'Whether or not the entity can be pinged', 'tribe-common' ),
 					'enum'        => [
@@ -119,7 +119,7 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					],
 					'example'     => 'open',
 				],
-				'format'             => [
+				'format'         => [
 					'type'        => 'string',
 					'description' => __( 'The format for the entity.', 'tribe-common' ),
 					'enum'        => [
@@ -136,17 +136,17 @@ class TEC_Post_Entity_Request_Body_Definition extends Definition {
 					],
 					'example'     => 'standard',
 				],
-				'sticky'             => [
+				'sticky'         => [
 					'type'        => 'boolean',
 					'description' => __( 'Whether or not the entity should be treated as sticky', 'tribe-common' ),
 					'example'     => false,
 				],
-				'template'           => [
+				'template'       => [
 					'type'        => 'string',
 					'description' => __( 'The theme file to use to display the entity.', 'tribe-common' ),
 					'example'     => '',
 				],
-				'tags'               => [
+				'tags'           => [
 					'type'        => 'array',
 					'description' => __( 'The terms assigned to the entity in the post_tag taxonomy', 'tribe-common' ),
 					'items'       => [
