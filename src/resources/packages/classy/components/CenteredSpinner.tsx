3a2248df55f1c0553903c24d7dee686a0362aefd
Luca Tumedei <luca@theaveragedev.com>
2025-08-12 15:37:44 +0200

    feat(Classy) support class name in CenteredSpinner

--- src/resources/packages/classy/components/CenteredSpinner.tsx
+++ src/resources/packages/classy/components/CenteredSpinner.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import { Spinner } from '@wordpress/components';
 
+interface CenteredSpinnerProps {
+	className?: string;
+}
+
 /**
  * A component that displays a centered spinner wrapping the default WordPress spinner
  * in an element styled to center in the parent.
  *
  * @since TBD
  *
+ * @param {CenteredSpinnerProps} props The component props.
+ * @param {string}               props.className The class name to apply to the spinner.
+ *
  * @returns JSX.Element The centered spinner component.
  */
-export function CenteredSpinner() {
+export function CenteredSpinner( { className }: CenteredSpinnerProps ) {
+	const fullClassName = className ? `classy-component__spinner ${ className }` : 'classy-component__spinner';
 	return (
-		<div className="classy-component__spinner">
+		<div className={ fullClassName }>
 			<Spinner />
 		</div>
 	);
