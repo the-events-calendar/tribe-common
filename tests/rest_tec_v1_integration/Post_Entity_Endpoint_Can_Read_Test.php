08498c2d3ae4454c02ab926727729d5739f9c3e6
Gustavo Bordoni <bordoni.dev@gmail.com>
2025-08-13 12:05:51 -0400

    test: update password-protected post test expectations
    - Modified test to allow reading password-protected posts
    - Changed test name to reflect new behavior
    - Added verification that content is marked as protected
    - Added WordPress REST controller test for protected flag validation

--- tests/rest_tec_v1_integration/Post_Entity_Endpoint_Can_Read_Test.php
+++ tests/rest_tec_v1_integration/Post_Entity_Endpoint_Can_Read_Test.php
@@ -10,6 +10,7 @@ namespace TEC\Common\Tests\REST\TEC\V1;
 use Codeception\TestCase\WPRestApiTestCase;
 use TEC\Common\Tests\REST\V1\Fake_Post_Endpoint;
 use WP_REST_Request;
+use WP_REST_Posts_Controller;
 
 class Post_Entity_Endpoint_Can_Read_Test extends WPRestApiTestCase {
 
@@ -58,7 +59,7 @@ class Post_Entity_Endpoint_Can_Read_Test extends WPRestApiTestCase {
 		$this->assertFalse( $endpoint->can_read( $request ) );
 	}
 
-	public function test_cannot_read_password_protected_without_password(): void {
+	public function test_can_read_password_protected_without_password_and_sets_protected_flag(): void {
         $post_id = wp_insert_post(
             [
                 'post_type'     => 'tec_fake_post',
@@ -72,7 +73,18 @@ class Post_Entity_Endpoint_Can_Read_Test extends WPRestApiTestCase {
 		$request  = new WP_REST_Request( 'GET', '/tec/v1/fake-posts/' . $post_id );
 		$request->set_param( 'id', $post_id );
 
-		$this->assertFalse( $endpoint->can_read( $request ) );
+		// Password-protected published posts should be readable (content will be empty unless password provided).
+		$this->assertTrue( $endpoint->can_read( $request ) );
+
+		// Verify WordPress REST response marks content as protected.
+		$wp_controller = new WP_REST_Posts_Controller( 'tec_fake_post' );
+		$wp_request    = new WP_REST_Request( 'GET', '/wp/v2/tec_fake_post/' . $post_id );
+		$wp_request->set_param( 'context', 'view' );
+		$data       = $wp_controller->prepare_item_for_response( get_post( $post_id ), $wp_request );
+		$prepared   = $wp_controller->prepare_response_for_collection( $data );
+		$this->assertArrayHasKey( 'content', $prepared );
+		$this->assertArrayHasKey( 'protected', $prepared['content'] );
+		$this->assertTrue( (bool) $prepared['content']['protected'] );
 	}
 
 	public function test_can_read_password_protected_with_password(): void {
