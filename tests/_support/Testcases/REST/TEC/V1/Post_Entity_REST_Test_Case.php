7d705fcb3b92c77be711d42c8d81e144fe909c7e
Dimitrios Pantazis <dpanta94@gmail.com>
2025-08-13 14:27:31 +0300

    Move update special cases into abstract

--- tests/_support/Testcases/REST/TEC/V1/Post_Entity_REST_Test_Case.php
+++ tests/_support/Testcases/REST/TEC/V1/Post_Entity_REST_Test_Case.php
@@ -19,6 +19,7 @@ use TEC\Common\REST\TEC\V1\Parameter_Types\Integer;
 use TEC\Common\REST\TEC\V1\Parameter_Types\Text;
 use TEC\Common\REST\TEC\V1\Parameter_Types\Array_Of_Type;
 use TEC\Common\REST\TEC\V1\Exceptions\InvalidRestArgumentException;
+use Tribe__Repository as Base_Repo;
 
 /**
  * Class Post_Entity_REST_Test_Case
@@ -292,6 +293,44 @@ abstract class Post_Entity_REST_Test_Case extends REST_Test_Case {
 
 			$entity_ids[] = $new_entity_id;
 		}
+	}
+
+	public function test_update_handles_save_failure() {
+		if ( ! $this->is_updatable() ) {
+			return;
+		}
+
+		$this->set_class_fn_return( Base_Repo::class, 'save', false );
+
+		$request_body = $this->endpoint->update_schema()->get_request_body()->to_array()['content']['application/json'];
+
+		$example = $request_body['example'];
+		unset( $example['id'] );
+
+		$orm = $this->endpoint->get_orm();
+
+		wp_set_current_user( 1 );
+		$entity_id = $orm->set_args( $example )->create()->ID;
+		$this->assert_endpoint( sprintf( $this->endpoint->get_base_path(), $entity_id ), 'PUT', 500, [ 'title' => 'Updated Title' ] );
+	}
+
+	public function test_update_handles_entity_not_found_after_save() {
+		if ( ! $this->is_updatable() ) {
+			return;
+		}
+
+		$this->set_class_fn_return( Base_Repo::class, 'first', null );
+
+		$request_body = $this->endpoint->update_schema()->get_request_body()->to_array()['content']['application/json'];
+
+		$example = $request_body['example'];
+		unset( $example['id'] );
+
+		$orm = $this->endpoint->get_orm();
+
+		wp_set_current_user( 1 );
+		$entity_id = $orm->set_args( $example )->create()->ID;
+		$this->assert_endpoint( sprintf( $this->endpoint->get_base_path(), $entity_id ), 'PUT', 500, [ 'title' => 'Updated Title' ] );
 	}
 
 	private function modify_value( array $data ) {
