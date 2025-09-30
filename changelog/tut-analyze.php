92b425c3ec924595474922448d1928b91d287ef5
Brian Krane <redscar@gmail.com>
2025-09-30 11:14:10 -0400

    Add changelog entry for tut-analyze.

---
+++ changelog/tut-analyze.php
@@ -0,0 +1,4 @@
+Significance: minor
+Type: tweak
+
+Added filters: `tec_help_hub_register_tabs`, `tec_trustedlogin_config`, `tec_trustedlogin_page_url`, `tec_trustedlogin_api_key`, `tec_trustedlogin_namespace`, `tec_trustedlogin_title`, `tec_trustedlogin_logo_url`, `tec_trustedlogin_support_email`, `tec_trustedlogin_support_url`, `tec_trustedlogin_website_url`, `tec_trustedlogin_menu_slug`, `tec_trustedlogin_role`, `tec_trustedlogin_decay`, `tec_trustedlogin_clone_role`
