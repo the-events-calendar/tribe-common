5abecfbf54af088f745515409ae29a7e044100aa
Jeremy Pry <jeremy.pry@gmail.com>
2025-10-27 19:03:36 -0500

    Update Classy Root component to not require a title

--- src/resources/packages/classy/components/ClassyModal/Root.tsx
+++ src/resources/packages/classy/components/ClassyModal/Root.tsx
@@ -4,22 +4,24 @@ type RootProps = {
 	/**
 	 * The child elements to be rendered within the modal.
 	 */
-	children: React.ReactNode;
+	children?: React.ReactNode;
 
 	/**
-	 * An optional icon to be displayed in the modal header. If not provided, no icon will be shown.
+	 * An optional icon to be displayed in the modal header. If not provided, no icon will be shown. Wil
+	 * only be displayed if a title is also provided.
 	 */
 	headerIcon?: React.ReactNode;
 
 	/**
 	 * The type of the modal, used to apply specific class names for styling.
 	 */
-	type: string;
+	type?: string;
 
 	/**
-	 * The title text to be displayed in the modal header.
+	 * The title text to be displayed in the modal header. If not provided, the header (including
+	 * the icon) will not be rendered.
 	 */
-	title: string;
+	title?: string;
 };
 
 /**
@@ -32,15 +34,17 @@ type RootProps = {
  * @param {RootProps} props The component props.
  */
 export default function Root( props: RootProps ) {
-	const { children = null, headerIcon = null, type, title } = props;
+	const { children = null, headerIcon = null, title, type } = props;
+	const headerClass = `classy-modal__header${ type ? ` classy-modal__header--${ type }` : '' }`;
 
 	return (
 		<div className="classy-root">
-			<header className={ `classy-modal__header classy-modal__header--${ type }` }>
-				{ headerIcon }
-				<h4 className="classy-modal__header-title">{ title }</h4>
-			</header>
-
+			{ title && (
+				<header className={ headerClass }>
+					{ headerIcon }
+					<h4 className="classy-modal__header-title">{ title }</h4>
+				</header>
+			) }
 			{ children }
 		</div>
 	);
