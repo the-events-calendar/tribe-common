c675a416f4848523cb0cd2ef397c864937f33e2d
Luca Tumedei <luca@theaveragedev.com>
2025-10-31 17:47:15 +0100

    refactor(Classy) style combobox labels correctly
    Remove the need for the wrapper component around them.

--- src/resources/packages/classy/components/LabeledInput/InputLabel.tsx
+++ src/resources/packages/classy/components/LabeledInput/InputLabel.tsx
@@ -2,14 +2,7 @@ import * as React from 'react';
 
 export function InputLabel( { label }: { label: string } ) {
 	return (
-		<span
-			className="classy-component__input-label"
-			aria-hidden={ true }
-			style={ {
-				fontSize: 'var(--classy-font-size-15)',
-				fontWeight: 'var(--tec-font-weight-regular)',
-			} }
-		>
+		<span className="classy-component__input-label" aria-hidden={ true }>
 			{ label }
 		</span>
 	);
