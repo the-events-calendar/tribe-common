f20922389766ec8719f34096594503c3179c9fff
Luca Tumedei <luca@theaveragedev.com>
2025-12-23 16:19:42 +0100

    fix: IN queries with spaces support
    This fixes an issue where the repository, using `WP_Query` under the
    hood, would build the incorrect `IN` or `NOT IN` query SQL when using
    `by`/`where` methods like `meta_in`/`meta_not_in` with values containing
    spaces.
    
    An exmaple is `->by( 'meta_in', 'First Name')` where the built SQL would
    be `AND meta_value IN ('First', 'Name')` resulting in a non-match for
    "First Name".
    
    This is due to the fact that, in the `WP_Meta_Query` class the `IN` and
    `NOT IN` queries will split the meta value ("First Name" in the example)
    at commas and spaces resulting in two meta values: "First" and "Name".
    The split guard is `! is_array( $meta_value )`, so the casting to array
    in the repository solves the issue as explicit use of an array would:
    ```
    ->by( 'meta_in', [ 'First Name' ] )
    ```
    The problem is that a huge portion of the code relies on the use of
    `meta_in` and similar and passes values indifferently as
    `string|string[]` leading to the issue when a `string` value contains a
    space (the guard does not apply).

--- tests/wpunit/Tribe/RepositoryTest.php
+++ tests/wpunit/Tribe/RepositoryTest.php
@@ -4,9 +4,10 @@ namespace Tribe;
 
 require_once codecept_data_dir( 'classes/Repository/Book_Repository.php' );
 
+use Codeception\TestCase\WPTestCase;
 use Tribe\Common\Tests\Repository\Book_Repository as Repository;
 
-class RepositoryTest extends \Codeception\TestCase\WPTestCase {
+class RepositoryTest extends WPTestCase {
 	/**
 	 * @test
 	 * it should be instantiatable
@@ -22,6 +23,60 @@ class RepositoryTest extends \Codeception\TestCase\WPTestCase {
 	 */
 	protected function repository() {
 		return new Repository();
+	}
+
+	public function setUp(): void {
+		parent::setUp();
+
+		register_taxonomy(
+			'audience',
+			[ 'book' ],
+			[
+				'hierarchical' => false,
+				'label'        => 'Audience',
+			]
+		);
+
+		// Set the user to one that will be able to create taxonomy terms.
+		wp_set_current_user( static::factory()->user->create( [ 'role' => 'administrator' ] ) );
+	}
+
+	protected function create_three_books(): array {
+		$book_1_id = static::factory()->post->create( [
+			'post_type'  => 'book',
+			'post_title' => 'Alice in Wonderland',
+			'meta_input' => [
+				'sourced_genre' => 'fiction',
+				'author_name'   => 'Lewis Carroll',
+			],
+			'tax_input'  => [
+				'audience' => [ 'for kids', 'for young adults', 'for adults' ]
+			],
+		] );
+		$book_2_id = static::factory()->post->create( [
+			'post_type'  => 'book',
+			'post_title' => 'The Hobbit',
+			'meta_input' => [
+				'sourced_genre' => 'fantasy',
+				'author_name'   => 'J.R.R. Tolkien',
+			],
+			'tax_input'  => [
+				'audience' => [ 'for kids', 'for young adults', 'for adults' ]
+			],
+		] );
+		$book_3_id = static::factory()->post->create( [
+			'post_type'  => 'book',
+			'post_title' => 'The Lord of the Rings',
+			'meta_input' => [
+				'sourced_genre' => 'fantasy',
+				'author_name'   => 'J.R.R. Tolkien',
+			],
+			'tax_input'  => [
+				'audience' => [ 'for young adults', 'for adults' ]
+			],
+		] );
+
+		return array( $book_1_id, $book_2_id, $book_3_id );
 	}
 
 	/**
@@ -50,7 +105,8 @@ class RepositoryTest extends \Codeception\TestCase\WPTestCase {
 
 		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->by( 'title', 'foo' ) );
 		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->where( 'title', 'foo' ) );
-		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->where_args( [ 'title' => 'foo' ] ) );
+		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class,
+			$repository->where_args( [ 'title' => 'foo' ] ) );
 		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->page( 2 ) );
 		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->per_page( 2 ) );
 		$this->assertInstanceOf( \Tribe__Repository__Read_Interface::class, $repository->offset( 2 ) );
@@ -72,28 +128,7 @@ class RepositoryTest extends \Codeception\TestCase\WPTestCase {
 	 * @test
 	 */
 	public function should_allow_mapping_on_the_query_results(): void {
-		// Create 3 books.
-		$book_1_id = static::factory()->post->create( [
-			'post_type'  => 'book',
-			'post_title' => 'Alice in Wonderland',
-			'meta_input' => [
-				'sourced_genre' => 'fiction'
-			]
-		] );
-		$book_2_id = static::factory()->post->create( [
-			'post_type'  => 'book',
-			'post_title' => 'The Hobbit',
-			'meta_input' => [
-				'sourced_genre' => 'fantasy'
-			]
-		] );
-		$book_3_id = static::factory()->post->create( [
-			'post_type'  => 'book',
-			'post_title' => 'The Lord of the Rings',
-			'meta_input' => [
-				'sourced_genre' => 'fantasy'
-			]
-		] );
+		[ , $book_2_id, $book_3_id ] = $this->create_three_books();
 
 		$this->assertEquals(
 			[ 'Alice in Wonderland', 'The Hobbit', 'The Lord of the Rings' ],
@@ -102,19 +137,127 @@ class RepositoryTest extends \Codeception\TestCase\WPTestCase {
 
 		$this->assertEquals(
 			[ 'The Lord of the Rings', 'The Hobbit' ],
-			$this->repository()
-			     ->where( 'meta_equals', 'sourced_genre', 'fantasy' )
-			     ->order_by( 'title', 'desc' )
-			     ->map( fn( $book ) => $book->post_title )
+			$this
+				->repository()
+				->where( 'meta_equals', 'sourced_genre', 'fantasy' )
+				->order_by( 'title', 'desc' )
+				->map( fn( $book ) => $book->post_title )
 		);
 
 		$this->assertEquals(
 			[ 'ID => ' . $book_3_id, 'ID => ' . $book_2_id ],
-			$this->repository()
-			     ->where( 'meta_equals', 'sourced_genre', 'fantasy' )
-			     ->fields( 'ids' )
-			     ->order_by( 'title', 'desc' )
-			     ->map( fn( $id ) => 'ID => ' . $id )
+			$this
+				->repository()
+				->where( 'meta_equals', 'sourced_genre', 'fantasy' )
+				->fields( 'ids' )
+				->order_by( 'title', 'desc' )
+				->map( fn( $id ) => 'ID => ' . $id )
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_build_meta_in_not_in_query_correctly(): void {
+		[ $book_1_id, $book_2_id, $book_3_id ] = $this->create_three_books();
+
+		$this->assertEquals(
+			[ $book_1_id ],
+			$this->repository()->where( 'meta_in', 'author_name', 'Lewis Carroll' )->get_ids(),
+			"meta_in should support single values with space"
+		);
+
+		$this->assertEquals(
+			[ $book_1_id ],
+			$this->repository()->where( 'meta_in', 'author_name', [ 'Lewis Carroll' ] )->get_ids(),
+			"meta_in should support single values with space"
+		);
+		$this->assertEquals(
+			[ $book_2_id, $book_3_id ],
+			$this->repository()->where( 'meta_not_in', 'author_name', 'Lewis Carroll' )->get_ids(),
+			"meta_not_in should support single values with space"
+		);
+
+		$this->assertEquals(
+			[ $book_2_id, $book_3_id ],
+			$this->repository()->where( 'meta_not_in', 'author_name', [ 'Lewis Carroll' ] )->get_ids(),
+			"meta_not_in should support single values with spaces in array"
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_build_term_id_in_not_in_query_correctly(): void {
+		[ $book_1_id, $book_2_id, $book_3_id ] = $this->create_three_books();
+
+		$for_kids_term_id = get_term_by( 'name', 'for kids', 'audience' )->term_id;
+
+		$this->assertEquals(
+			[ $book_1_id, $book_2_id ],
+			$this->repository()->where( 'term_id_in', 'audience', $for_kids_term_id )->get_ids(),
+			"term_id_in should support single values"
+		);
+		$this->assertEquals(
+			[ $book_3_id ],
+			$this->repository()->where( 'term_id_not_in', 'audience', $for_kids_term_id )->get_ids(),
+			"term_id_not_in should support single values"
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_build_term_name_in_not_in_query_correctly(): void {
+		[ $book_1_id, $book_2_id, $book_3_id ] = $this->create_three_books();
+
+		$this->assertEquals(
+			[ $book_1_id, $book_2_id ],
+			$this->repository()->where( 'term_name_in', 'audience', 'for kids' )->get_ids(),
+			"term_name_in should support single values with space"
+		);
+		$this->assertEquals(
+			[ $book_1_id, $book_2_id ],
+			$this->repository()->where( 'term_name_in', 'audience', [ 'for kids' ] )->get_ids(),
+			"term_name_in should support single values with space in array"
+		);
+		$this->assertEquals(
+			[ $book_3_id ],
+			$this->repository()->where( 'term_name_not_in', 'audience', 'for kids' )->get_ids(),
+			"term_name_not_in should support single values with space"
+		);
+		$this->assertEquals(
+			[ $book_3_id ],
+			$this->repository()->where( 'term_name_not_in', 'audience', [ 'for kids' ] )->get_ids(),
+			"term_name_not_in should support single values with space in array"
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_build_term_slug_in_not_in_query_correctly(): void {
+		[ $book_1_id, $book_2_id, $book_3_id ] = $this->create_three_books();
+
+		$this->assertEquals(
+			[ $book_1_id, $book_2_id ],
+			$this->repository()->where( 'term_slug_in', 'audience', 'for-kids' )->get_ids(),
+			"term_slug_in should support single values with space"
+		);
+		$this->assertEquals(
+			[ $book_1_id, $book_2_id ],
+			$this->repository()->where( 'term_slug_in', 'audience', [ 'for-kids' ] )->get_ids(),
+			"term_slug_in should support single values with space in array"
+		);
+		$this->assertEquals(
+			[ $book_3_id ],
+			$this->repository()->where( 'term_slug_not_in', 'audience', 'for-kids' )->get_ids(),
+			"term_slug_not_in should support single values with space"
+		);
+		$this->assertEquals(
+			[ $book_3_id ],
+			$this->repository()->where( 'term_slug_not_in', 'audience', [ 'for-kids' ] )->get_ids(),
+			"term_slug_not_in should support single values with space in array"
 		);
 	}
 }
