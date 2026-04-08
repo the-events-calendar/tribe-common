c2dbb66a6ae0d2845882714da9df42c2da2deb2a
Eric Defore <eric.defore@gmail.com>
2026-04-08 13:58:19 -0400

    SCON-355: Revert unused stellar-icon template

--- src/views/v2/components/icons/stellar-icon.php
+++ src/views/v2/components/icons/stellar-icon.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * View: Liquid Web Icon
+ * View: Stellar Icon
  *
  * Override this template in your own theme by creating a file at:
  * [your-theme]/tribe/events/v2/components/icons/stellar-icon.php
