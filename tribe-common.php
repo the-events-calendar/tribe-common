a1efe210e3b2253b3bf019c0bc5ab4e54be4c021
sdokus <95599199+sdokus@users.noreply.github.com>
2026-06-15 21:26:12 +0000

    Version bump for bucket/rsvp-merge
    This is an automated PR created by sdokus.
    Generated by: https://github.com/the-events-calendar/tribe-common/actions/runs/27577456844

--- tribe-common.php
+++ tribe-common.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tribe Common
  * Description: An event settings framework for managing shared options.
- * Version: 6.12.0
+ * Version: 6.13.0
  * Requires at least: 6.7
  * Requires PHP: 7.4
  * Author: The Events Calendar
