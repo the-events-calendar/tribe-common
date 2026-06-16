29c22f9563598fe250bb8b98746abfa28b0f4d6e
WayneRocha <dev.waynerocha@gmail.com>
2026-06-16 03:15:00 -0300

    Update package versions and enhance select components with emotion cache support

--- src/modules/elements/creatable-select/element.js
+++ src/modules/elements/creatable-select/element.js
@@ -1,11 +1,13 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { components } from 'react-select';
 import Select from 'react-select';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import { Dashicon } from '@wordpress/components';
 
 /**
@@ -31,14 +33,41 @@ const IndicatorSeparator = () => null;
  * - https://github.com/JedWatson/react-select/issues/2944
  */
 
-const CreatableSelect = ( { className, ...rest } ) => (
-	<Select
-		className={ classNames( 'tribe-editor__creatable-select', className ) }
-		classNamePrefix="tribe-editor__creatable-select"
-		components={ { DropdownIndicator, IndicatorSeparator } }
-		{ ...rest }
-	/>
-);
+const CreatableSelect = ( { className, ...rest } ) => {
+	// In the iframed block editor canvas (WP 6.x+/7), react-select's emotion styles are
+	// injected into the wrong document by default. Bind an emotion cache to the owner
+	// document's head so the styles land inside the iframe where the select renders.
+	const [ ownerDocument, setOwnerDocument ] = useState( null );
+
+	const refCallback = useCallback( ( node ) => {
+		if ( node ) {
+			setOwnerDocument( node.ownerDocument );
+		}
+	}, [] );
+
+	const cache = useMemo(
+		() =>
+			ownerDocument
+				? createCache( { key: 'tribe-editor-creatable-select', container: ownerDocument.head } )
+				: null,
+		[ ownerDocument ]
+	);
+
+	const select = (
+		<Select
+			className={ classNames( 'tribe-editor__creatable-select', className ) }
+			classNamePrefix="tribe-editor__creatable-select"
+			components={ { DropdownIndicator, IndicatorSeparator } }
+			{ ...rest }
+		/>
+	);
+
+	return (
+		<div ref={ refCallback } className="tribe-editor__creatable-select-wrapper">
+			{ cache ? <CacheProvider value={ cache }>{ select }</CacheProvider> : select }
+		</div>
+	);
+};
 
 CreatableSelect.propTypes = {
 	className: PropTypes.string,
--- src/modules/elements/select/element.js
+++ src/modules/elements/select/element.js
@@ -1,10 +1,12 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ReactSelect, { components } from 'react-select';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 import { Dashicon } from '@wordpress/components';
 
 /**
@@ -21,14 +23,41 @@ const DropdownIndicator = ( props ) =>
 
 const IndicatorSeparator = () => null;
 
-const Select = ( { className, ...rest } ) => (
-	<ReactSelect
-		className={ classNames( 'tribe-editor__select', className ) }
-		classNamePrefix="tribe-editor__select"
-		components={ { DropdownIndicator, IndicatorSeparator } }
-		{ ...rest }
-	/>
-);
+const Select = ( { className, ...rest } ) => {
+	// In the iframed block editor canvas (WP 6.x+/7), react-select's emotion styles are
+	// injected into the wrong document by default. Bind an emotion cache to the owner
+	// document's head so the styles land inside the iframe where the select renders.
+	const [ ownerDocument, setOwnerDocument ] = useState( null );
+
+	const refCallback = useCallback( ( node ) => {
+		if ( node ) {
+			setOwnerDocument( node.ownerDocument );
+		}
+	}, [] );
+
+	const cache = useMemo(
+		() =>
+			ownerDocument
+				? createCache( { key: 'tribe-editor-select', container: ownerDocument.head } )
+				: null,
+		[ ownerDocument ]
+	);
+
+	const select = (
+		<ReactSelect
+			className={ classNames( 'tribe-editor__select', className ) }
+			classNamePrefix="tribe-editor__select"
+			components={ { DropdownIndicator, IndicatorSeparator } }
+			{ ...rest }
+		/>
+	);
+
+	return (
+		<div ref={ refCallback } className="tribe-editor__select-wrapper">
+			{ cache ? <CacheProvider value={ cache }>{ select }</CacheProvider> : select }
+		</div>
+	);
+};
 
 Select.propTypes = {
 	className: PropTypes.string,
